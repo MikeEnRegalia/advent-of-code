773eb872ec13243748aa6045b68a0e855b44ca44
Michael Böiers <michael.boiers@ingka.com>
2021-09-21 12:12:32 +0200

    beautify

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -3,39 +3,36 @@ package day15
 import kotlin.math.max
 
 fun main() {
-    val ingredients = input()
-    println("max score: ${ingredients.score()}, limited to 500 calories: ${ingredients.score(500)}")
+    with(input()) {
+        println("max score: ${maxPoints()}, limited to 500 calories: ${maxPoints(calorieLimit = 500)}")
+    }
 }
 
-private fun List<Ingredient>.score(calorieLimit: Int? = null): Int {
-    var max = 0
+private fun List<Ingredient>.maxPoints(calorieLimit: Int? = null) =
+    candidates()
+        .map { it.score() }
+        .filter { calorieLimit == null || it.calories <= calorieLimit }
+        .maxOf { it.points }
 
+private fun List<Ingredient>.candidates() = sequence {
     for (a in 0..100) {
         for (b in 0..100) {
             for (c in 0..100) {
                 for (d in 0..100) {
-                    if (a + b + c + d > 100) continue
-                    mapOf(get(0) to a, get(1) to b, get(2) to c, get(3) to d)
-                        .score()
-                        .let { (score, calories) ->
-                            if (score > max && (calorieLimit == null || calories <= calorieLimit)) max = score
-                        }
+                    if (a + b + c + d <= 100)
+                        yield(mapOf(get(0) to a, get(1) to b, get(2) to c, get(3) to d))
                 }
             }
         }
     }
-    return max
 }
 
-
-private fun Map<Ingredient, Int>.score(): Pair<Int, Int> {
-    val capacity = entries.sumOf { it.key.capacity * it.value }
-    val durability = entries.sumOf { it.key.durability * it.value }
-    val flavor = entries.sumOf { it.key.flavor * it.value }
-    val texture = entries.sumOf { it.key.texture * it.value }
-    val calories = entries.sumOf { it.key.calories * it.value }
-    return Pair(max(capacity, 0) * max(durability, 0) * max(flavor, 0) * max(texture, 0), calories)
-}
+private fun Map<Ingredient, Int>.score() =
+    (0..3)
+        .map { n -> entries.sumOf { (i, amount) -> i.propertyValues[n] * amount } }
+        .map { max(it, 0) }
+        .reduce { a, b -> a * b }
+        .let { Score(points = it, calories = entries.sumOf { (i, amount) -> i.calories * amount }) }
 
 data class Ingredient(
     val name: String,
@@ -44,23 +41,31 @@ data class Ingredient(
     val flavor: Int,
     val texture: Int,
     val calories: Int
-)
+) {
+    val propertyValues = listOf(capacity, durability, flavor, texture)
+}
 
-fun input() = """Frosting: capacity 4, durability -2, flavor 0, texture 0, calories 5
+data class Score(val points: Int, val calories: Int)
+
+fun input() =
+    """Frosting: capacity 4, durability -2, flavor 0, texture 0, calories 5
 Candy: capacity 0, durability 5, flavor -1, texture 0, calories 8
 Butterscotch: capacity -1, durability 0, flavor 5, texture 0, calories 6
 Sugar: capacity 0, durability 0, flavor -2, texture 2, calories 1"""
-    .split("\n")
-    .map { it.split(": ").let { tokens -> Pair(tokens[0], tokens[1]) } }
-    .map { (name, rest) ->
-        with(rest.split(", ".toRegex()).associate { it.split(" ").let { p -> Pair(p[0], p[1].toInt()) } }) {
-            Ingredient(
-                name,
-                get("capacity")!!,
-                get("durability")!!,
-                get("flavor")!!,
-                get("texture")!!,
-                get("calories")!!
-            )
+        .split("\n")
+        .map { it.split(": ") }
+        .map { tokens ->
+            tokens[1]
+                .split(", ".toRegex())
+                .associate { it.split(" ").let { p -> Pair(p[0], p[1].toInt()) } }
+                .let { props ->
+                    Ingredient(
+                        tokens[0],
+                        props["capacity"]!!,
+                        props["durability"]!!,
+                        props["flavor"]!!,
+                        props["texture"]!!,
+                        props["calories"]!!
+                    )
+                }
         }
-    }
