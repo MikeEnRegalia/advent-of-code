eb5c3e8110537412098497a4d01c141dc5b224a0
Michael Böiers <michael.boiers@ingka.com>
2021-09-22 23:40:41 +0200

    day 17 part 1 works

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -1,18 +1,23 @@
 package day17
 
 fun main() {
-    combinations(input(), total = 150).also { println(it) }
+    val combos = mutableSetOf<String>()
+    combinations(input(), total = 150) { combos.add(it.joinToString("")) }.also { println(it) }
+    println(combos.count { combo -> combo.length == combos.sortedBy { it.length }[0].length })
 }
 
-fun combinations(list: List<Int>, total: Int): Int {
+fun combinations(list: List<Int>, total: Int, prefix: List<Int> = listOf(), f: (List<Int>) -> Unit): Int {
     val sum = list.sum()
-    if (sum < total) return 0
-    if (sum == total) return 1
-    if (list.isEmpty()) return 0
-
-    val first = list.first()
-    val remaining = list.drop(1)
-    return combinations(remaining, total - first) + combinations(remaining, total)
+    return when {
+        sum < total -> 0
+        sum == total -> 1.also { f(prefix.plus(list)) }
+        list.isEmpty() -> 0
+        else -> {
+            val first = list.first()
+            val remaining = list.drop(1)
+            combinations(remaining, total - first, prefix.plus(first), f) + combinations(remaining, total, prefix, f)
+        }
+    }
 }
 
 fun input() = """11
