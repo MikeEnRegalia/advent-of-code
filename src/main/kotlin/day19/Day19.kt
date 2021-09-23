16829025a3e37205d0f824435fe7a1cffa904565
Michael Böiers <michael.boiers@ingka.com>
2021-09-23 15:34:21 +0200

    day 19

---
+++ src/main/kotlin/day19/Day19.kt
@@ -0,0 +1,104 @@
+package day19
+
+fun main() {
+    val (molecule, replacements) = input()
+
+    replacements
+        .fold(mutableSetOf<String>()) { acc, (from, to) ->
+            acc.apply { addAll(molecule.replaceAll(from, to)) }
+        }
+        .size
+        .also { println(it) }
+
+    println(replacements.deconstructNaively(molecule))
+}
+
+private fun String.replaceAll(from: String, to: String): MutableSet<String> {
+    val result = mutableSetOf<String>()
+    var i = 0
+    while (true) {
+        val at = indexOf(from, i)
+        if (at == -1) break
+        result.add(substring(0, at) + to + substring(at + from.length))
+        i = at + 1
+    }
+    return result
+}
+
+fun List<Pair<String, String>>.deconstructNaively(molecule: String): Int? {
+    var dest = molecule
+    var steps = 0
+    while (dest != "e") {
+        println(dest)
+        val oldDest = dest
+        forEach { (from, to) -> dest.replaceAll(to, from).firstOrNull()?.let { dest = it; steps++ } }
+        if (oldDest == dest) return null
+    }
+    return steps
+}
+
+fun List<Pair<String, String>>.deconstruct(molecule: String): Int? {
+    //println(molecule)
+    if (molecule == "e") return 0
+
+    val afterOneStep = fold(mutableSetOf<String>()) { acc, (src, dest) ->
+        acc.apply { addAll(molecule.replaceAll(dest, src)) }
+    }
+
+    if (afterOneStep.contains("e")) return 1
+    if (afterOneStep.isEmpty()) return null
+    return afterOneStep
+        .mapNotNull { deconstruct(it) }
+        .minOfOrNull { it }
+        ?.plus(1)
+}
+
+fun input() = """Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg
+
+CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr"""
+    .run {
+        val parts = split("\n\n")
+        Pair(parts[1], parts[0].split("\n").map { it.split(" => ") }.map { Pair(it[0], it[1]) })
+    }
