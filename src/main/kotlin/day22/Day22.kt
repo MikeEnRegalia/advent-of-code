a3ac7adf9597a685c3591e9bb6bf0ead62b67c9f
Michael Böiers <michael.boiers@ingka.com>
2021-09-24 15:58:07 +0200

    refactor

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -3,18 +3,52 @@ package day22
 import kotlin.math.max
 import kotlin.math.min
 
-val spells = listOf(
-    Spell("m", cost = 53, duration = null, damage = 4),
-    Spell("d", cost = 73, duration = null, damage = 2, heal = 2),
-    Spell("s", cost = 113, duration = 6, armor = 7),
-    Spell("p", cost = 173, duration = 6, damage = 3),
-    Spell("r", cost = 229, duration = 5, recharge = 101)
-)
-
 fun main() {
     val wizard = Player(health = 50, mana = 500)
     val boss = Player(health = 51, damage = 9)
     println("${play(wizard, boss)}, hard: ${play(wizard, boss, hard = true)}")
+}
+
+fun play(
+    wizard: Player,
+    boss: Player,
+    history: List<Spell> = listOf(),
+    move: Int = 0,
+    tracker: Tracker = Tracker(),
+    hard: Boolean = false
+): Int? {
+    val wizardsTurn = move % 2 == 0
+
+    val wizardPre = if (hard && wizardsTurn) wizard.healed(-1) else wizard
+
+    ifGameOver(wizardPre, boss, tracker, history) { return it }
+
+    var (p1, p2) = wizardPre.applySpellEffects(boss)
+
+    ifGameOver(p1, p2, tracker, history) { return it }
+
+    return if (wizardsTurn) {
+        // player's turn (only casts spells)
+        spells
+            .filter { p1.spells.none { s -> it.name == s.name } }
+            .filter { it.cost <= p1.mana }
+            .filter { !tracker.isTooBig(history.sumOf { it.cost }) }
+            .mapNotNull { spell ->
+                p1.cast(spell, p2)
+                    .let { (w, b) ->
+                        val newHistory = history.plus(spell)
+                        ifGameOver(w, b, tracker, newHistory, spell.cost) { return it }
+                        play(w, b, newHistory, move + 1, tracker, hard)
+                    }
+                    ?.let { spell.cost + it }
+            }
+            .minOfOrNull { it }
+    } else {
+        // boss's turn (can only hit)
+        p1 = p2.hit(p1)
+        ifGameOver(p1, p2, tracker, history) { return it }
+        play(p1, p2, history, move + 1, tracker, hard)
+    }
 }
 
 class Tracker {
@@ -26,54 +60,24 @@ class Tracker {
     fun isTooBig(cost: Int) = cost > (minMana ?: cost)
 }
 
-fun play(
-    wizard: Player,
-    boss: Player,
-    history: List<Spell> = listOf(),
-    move: Int = 0,
-    tracker: Tracker = Tracker(),
-    hard: Boolean = false
-): Int? {
-    if (tracker.isTooBig(history.sumOf { it.cost })) return null
-
-    val wizardsTurn = move % 2 == 0
-
-    val wizardPre = if (hard && wizardsTurn) wizard.healed(-1) else wizard
-
-    check(wizardPre, boss, tracker, history) { return if (it) 0 else null }
-
-    var (p1, p2) = wizardPre.applySpellEffects(boss)
-
-    check(p1, p2, tracker, history) { return if (it) 0 else null }
-
-    return if (wizardsTurn) {
-        // player's turn (only casts spells)
-        spells
-            .filter { p1.spells.none { s -> it.name == s.name } }
-            .filter { it.cost <= p1.mana }
-            .mapNotNull { spell ->
-                p1.cast(spell, p2)
-                    .let { (w, b) ->
-                        val newHistory = history.plus(spell)
-                        check(w, b, tracker, newHistory) { return if (it) spell.cost else null }
-                        play(w, b, newHistory, move + 1, tracker, hard)
-                    }
-                    ?.let { spell.cost + it }
-            }
-            .minOfOrNull { it }
-    } else {
-        // boss's turn (can only hit)
-        p1 = p2.hit(p1)
-        check(p1, p2, tracker, history) { return if (it) 0 else null }
-        play(p1, p2, history, move + 1, tracker, hard)
+inline fun ifGameOver(
+    p1: Player,
+    p2: Player,
+    tracker: Tracker,
+    history: List<Spell>,
+    cost: Int = 0,
+    callback: (Int?) -> Unit
+) {
+    result(p1, p2)?.let { wizardWon ->
+        if (wizardWon) tracker.registerWinningMana(history.sumOf { it.cost })
+        callback(if (wizardWon) cost else null)
     }
 }
 
-inline fun check(p1: Player, p2: Player, tracker: Tracker, history: List<Spell>, callback: (Boolean) -> Unit) {
-    result(p1, p2)?.let { wizardWon ->
-        if (wizardWon) tracker.registerWinningMana(history.sumOf { it.cost })
-        callback(wizardWon)
-    }
+fun result(p1: Player, p2: Player) = when {
+    p1.mana <= 0 || p1.health <= 0 -> false
+    p2.health <= 0 -> true
+    else -> null
 }
 
 data class Spell(
@@ -87,6 +91,14 @@ data class Spell(
 ) {
     override fun toString() = name
 }
+
+val spells = listOf(
+    Spell("m", cost = 53, duration = null, damage = 4),
+    Spell("d", cost = 73, duration = null, damage = 2, heal = 2),
+    Spell("s", cost = 113, duration = 6, armor = 7),
+    Spell("p", cost = 173, duration = 6, damage = 3),
+    Spell("r", cost = 229, duration = 5, recharge = 101)
+)
 
 data class Player(
     val health: Int = 0,
@@ -136,10 +148,4 @@ data class Player(
             p1 to p2
         }
     }
-}
-
-fun result(p1: Player, p2: Player) = when {
-    p1.mana <= 0 || p1.health <= 0 -> false
-    p2.health <= 0 -> true
-    else -> null
 }
