5055f72fcaa8c0c36d1ce4b0b56e59c5b9a57d11
Michael Böiers <michael.boiers@ingka.com>
2021-09-25 17:01:02 +0200

    day 23

---
+++ src/main/kotlin/day23/Day23.kt
@@ -0,0 +1,98 @@
+package day23
+
+fun main() {
+    val program = input()
+
+    compute(mutableMapOf("a" to 0, "b" to 0), program)
+    compute(mutableMapOf("a" to 1, "b" to 0), program)
+}
+
+private fun compute(
+    r: MutableMap<String, Int>,
+    program: List<Pair<String, List<String>>>
+) {
+    fun get(reg: String) = r[reg] ?: throw IllegalArgumentException(reg)
+
+    var pos = 0
+    while (pos in program.indices) {
+        val (instruction, args) = program[pos]
+        when (instruction) {
+            "hlf" -> {
+                r[args[0]] = get(args[0]) / 2
+                pos++
+            }
+            "tpl" -> {
+                r[args[0]] = get(args[0]) * 3
+                pos++
+            }
+            "inc" -> {
+                r[args[0]] = get(args[0]) + 1
+                pos++
+            }
+            "jmp" -> {
+                pos += args[0].toInt()
+            }
+            "jie" -> {
+                pos += if (get(args[0]) % 2 == 0) args[1].toInt() else 1
+            }
+            "jio" -> {
+                pos += if (get(args[0]) == 1) args[1].toInt() else 1
+            }
+            else -> throw IllegalStateException()
+        }
+    }
+    println(r["b"])
+}
+
+fun input() = """jio a, +19
+inc a
+tpl a
+inc a
+tpl a
+inc a
+tpl a
+tpl a
+inc a
+inc a
+tpl a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+jmp +23
+tpl a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+tpl a
+inc a
+tpl a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+tpl a
+inc a
+jio a, +8
+inc b
+jie a, +4
+tpl a
+inc a
+jmp +2
+hlf a
+jmp -7"""
+    .split("\n")
+    .map { Pair(it.substring(0, 3), it.substring(4)) }
+    .map { Pair(it.first, it.second.split(", ")) }
