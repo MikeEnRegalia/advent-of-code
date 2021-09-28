67bc447ec8dd5234d0042b602d89361efd649994
Michael Böiers <michael.boiers@ingka.com>
2021-09-28 15:26:43 +0200

    refactor

--- src/main/kotlin/y2018/day03/Day03.kt
+++ src/main/kotlin/y2018/day03/Day03.kt
@@ -3,27 +3,37 @@ package y2018.day03
 private val regex = """^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$""".toRegex()
 
 fun main() {
-    val claims = generateSequence(::readLine).toList()
-        .map { row ->
-            with(regex.find(row)!!.groupValues.drop(1).map { it.toInt() }) {
-                Claim(get(0), get(1), get(2), get(3), get(4))
-            }
-        }
-
-    val allOverlapping = mutableSetOf<Claim>()
-    var sum = 0
-    for (x in 1..1000) {
-        for (y in 1..1000) {
-            val overlapping = claims.filter { it.contains(x, y) }
-            if (overlapping.count() > 1) {
-                ++sum
-                allOverlapping.addAll(overlapping)
-            }
+    val claims = generateSequence(::readLine).toList().map {
+        regex.find(it)!!.destructured.let { (number, x, y, width, height) ->
+            Claim(number.toInt(), x.toInt(), y.toInt(), width.toInt(), height.toInt())
         }
     }
-    println(sum)
 
+    val (allOverlapping, sum) = claims.findOverlaps()
+
+    println(sum)
     claims.filterNot { allOverlapping.contains(it) }.forEach { println(it.number) }
+}
+
+private fun List<Claim>.findOverlaps(): Pair<MutableSet<Claim>, Int> {
+    val overlapping = mutableSetOf<Claim>()
+    var sum = 0
+    coordinates().forEach { (x, y) ->
+        val claims = filter { it.contains(x, y) }
+        if (claims.count() > 1) {
+            ++sum
+            overlapping.addAll(claims)
+        }
+    }
+    return Pair(overlapping, sum)
+}
+
+private fun coordinates() = sequence {
+    for (x in 1..1000) {
+        for (y in 1..1000) {
+            yield(x to y)
+        }
+    }
 }
 
 data class Claim(val number: Int, val x: Int, val y: Int, val width: Int, val height: Int) {
