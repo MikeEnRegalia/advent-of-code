8fd473e7263ec49fc895a7019915abdd28957026
Michael Böiers <michael.boiers@ingka.com>
2021-09-28 14:35:43 +0200

    refactor

--- src/main/kotlin/y2018/day02/Day02.kt
+++ src/main/kotlin/y2018/day02/Day02.kt
@@ -2,16 +2,26 @@ package y2018.day02
 
 fun main() {
     val boxes = generateSequence(::readLine).toList()
-    val boxesLetterCounts = boxes.map { box -> box.groupingBy { it }.eachCount() }
 
-    val count1 = boxesLetterCounts.count { it.values.any { it == 2 } }
-    val count2 = boxesLetterCounts.count { it.values.any { it == 3 } }
-    (count1 * count2).also { println(it) }
+    with(boxes.map { it.groupByCount() }) {
+        val count1 = count { it.hasDuplicateValues(2) }
+        val count2 = count { it.hasDuplicateValues(3) }
+        (count1 * count2).also { println(it) }
+    }
 
     (1 until boxes.minOf { it.length })
-        .map { i -> boxes.map { it.substring(0, i) + it.substring(i+1) }}
-        .map { it.groupingBy { it }.eachCount() }
-        .map { it.filter { it.value == 2 }.keys }
+        .map { i -> boxes.map { it.withoutIndex(i) } }
+        .map { it.groupByCount(2) }
         .filter { it.isNotEmpty() }
-        .also { println(it) }
+        .flatten()
+        .forEach { println(it) }
 }
+
+private fun <K, V> Map<K, V>.filterKeysByValue(predicate: (V) -> Boolean) = filter { predicate(it.value) }.keys
+
+private fun String.withoutIndex(i: Int) = substring(0, i) + substring(i + 1)
+
+private fun CharSequence.groupByCount() = groupingBy { it }.eachCount()
+private fun <T> Iterable<T>.groupByCount(min: Int) = groupingBy { it }.eachCount().filterKeysByValue { it == min }
+
+private fun Map<Char, Int>.hasDuplicateValues(n: Int) = values.any { it == n }
