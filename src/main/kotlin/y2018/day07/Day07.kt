6dd2ed94a624cd5c1d95abde13e7e4853ee0e991
Michael Böiers <michael.boiers@ingka.com>
2021-10-04 17:07:10 +0200

    refactor

--- src/main/kotlin/y2018/day07/Day07.kt
+++ src/main/kotlin/y2018/day07/Day07.kt
@@ -2,21 +2,15 @@ package y2018.day07
 
 fun main() {
     val rules = generateSequence(::readLine)
-        .map {
-            """^Step (\w) must be finished before step (\w) can begin.$""".toRegex()
-                .find(it)!!.destructured.let { (before, step) -> before to step }
-        }
-        .toList()
-
-    val steps = rules.flatMap { it.toList() }.distinct()
+        .map { Regex("""^Step (\w) must be finished before step (\w) can begin.$""").find(it) }
+        .map { it!!.destructured.toList() }.toList()
 
     val done = mutableListOf<String>()
-    while (true) {
-        steps
-            .filterNot { done.contains(it) }
-            .filterNot { rules.filterNot { done.contains(it.first) }.map { it.second }.contains(it) }
-            .minOrNull()
-            ?.let { done.add(it) } ?: break
+    fun String.isOpen() = !done.contains(this)
+    fun String.isAvailable() = isOpen() && rules.none { (dep, step) -> dep.isOpen() && step == this }
+
+    with(rules.flatten().sorted()) {
+        while (true) firstOrNull { it.isAvailable() }?.let { done.add(it) } ?: break
     }
 
     done.joinToString("").also { println(it) }
