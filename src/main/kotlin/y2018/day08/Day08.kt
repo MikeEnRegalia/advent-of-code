3584bb885cfede8c3dbec1b11e7cfa508cda5000
Michael Böiers <michael.boiers@ingka.com>
2021-10-05 00:01:52 +0200

    2018 day 08 part 2

--- src/main/kotlin/y2018/day08/Day08.kt
+++ src/main/kotlin/y2018/day08/Day08.kt
@@ -6,6 +6,8 @@ fun main() {
     var sum = 0
     input.readNodes() { sum += it }
     println(sum)
+
+    println(input.sumNodes().value)
 }
 
 fun List<Int>.readNodes(pos: Int = 0, onMetaData: (Int) -> Unit): Int {
@@ -15,4 +17,24 @@ fun List<Int>.readNodes(pos: Int = 0, onMetaData: (Int) -> Unit): Int {
     for (i in 1..children) newPos = readNodes(newPos, onMetaData)
     for (i in 1..metadata) onMetaData(this[newPos++])
     return newPos
+}
+
+fun List<Int>.sumNodes(pos: Int = 0): IndexedValue<Int> {
+    val childrenCount = this[pos]
+    val metadata = this[pos + 1]
+    if (childrenCount == 0) return IndexedValue(pos + 2 + metadata, subList(pos + 2, pos + 2 + metadata).sum())
+
+    val children = mutableListOf<IndexedValue<Int>>()
+    repeat(childrenCount) {
+        val newPos = if (children.isEmpty()) pos + 2 else children.last().index
+        children.add(sumNodes(newPos))
+    }
+
+    var sum = 0
+    for (i in 0 until metadata) {
+        val childIndex = this[children.last().index + i]
+        if (childIndex in 1..children.size) sum += children[childIndex - 1].value
+    }
+
+    return IndexedValue(children.last().index + metadata, sum)
 }
