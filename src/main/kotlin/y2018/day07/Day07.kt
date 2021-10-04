09565e7e78257fb164fc70b9aae3a7a10f31a1b9
Michael Böiers <michael.boiers@ingka.com>
2021-10-04 17:48:27 +0200

    refactor

--- src/main/kotlin/y2018/day07/Day07.kt
+++ src/main/kotlin/y2018/day07/Day07.kt
@@ -9,33 +9,25 @@ fun main() {
     fun String.isOpen() = !done.contains(this)
     fun String.isAvailable() = isOpen() && rules.none { (dep, step) -> dep.isOpen() && step == this }
 
-    with(rules.flatten().sorted()) {
-        while (true) firstOrNull { it.isAvailable() }?.let { done.add(it) } ?: break
-    }
-
+    val steps = rules.flatten().distinct().sorted()
+    while (true) steps.firstOrNull { it.isAvailable() }?.let { done.add(it) } ?: break
     done.joinToString("").also { println(it) }
-
-    // part 2
     done.clear()
 
-    fun duration(step: String) = 61 + (step[0] - 'A')
-    val workers = 5
     var workingOn = listOf<Pair<String, Int>>()
-
     var second = 0
-    with(rules.flatten().distinct().sorted()) {
-        while (true) {
-            filter { step -> step.isAvailable() && workingOn.none { step == it.first }}
-                .take(workers - workingOn.size)
-                .map { Pair(it, duration(it)) }
-                .let { workingOn = workingOn.plus(it) }
-            workingOn = workingOn.mapNotNull { (step, remainingSeconds) ->
-                if (remainingSeconds == 1) null.also { done.add(step) }
-                else step to remainingSeconds - 1
-            }
-            second++
-            if (done.size == size) break
+    while (done.size < steps.size) {
+        workingOn = workingOn.run {
+            steps.filter { step -> step.isAvailable() && none { step == it.first } }
+                .take(5 - size)
+                .map { Pair(it, 61 + (it[0] - 'A')) }
+                .let { plus(it) }
+                .mapNotNull { (step, remainingSeconds) ->
+                    if (remainingSeconds == 1) null.also { done.add(step) }
+                    else step to remainingSeconds - 1
+                }
         }
+        second++
     }
     done.joinToString("").also { println("$it: $second") }
 }
