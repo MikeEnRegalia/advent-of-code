c1a18bf921946c2b11f64c4ca373d5a22488d29f
Michael Böiers <michael.boiers@ingka.com>
2021-10-06 08:24:01 +0200

    refactor

--- src/main/kotlin/y2018/day08/Day08.kt
+++ src/main/kotlin/y2018/day08/Day08.kt
@@ -6,13 +6,20 @@ fun main() = with(readLine()!!.split(" ").map { it.toInt() }) {
 }
 
 fun List<Int>.sumNodes(pos: Int = 0, simple: Boolean = true): IndexedValue<Int> =
-    mutableListOf(IndexedValue(pos + 2, 0)).let { nodes ->
-        repeat(this[pos]) { with(nodes) { add(sumNodes(last().index, simple)) } }
+    mutableListOf(IndexedValue(pos + 2, 0))
+        .also { nodes -> this[pos].times { nodes.add(sumNodes(nodes.last().index, simple)) } }
+        .let { nodes ->
+            (0 until this[pos + 1])
+                .map { this[nodes.last().index + it] }
+                .let { metadata ->
+                    IndexedValue(nodes.last().index + metadata.size, metadata.value(simple, nodes.drop(1)))
+                }
+        }
 
-        val metadata = (0 until this[pos + 1]).map { this[nodes.last().index + it] }
-        with(metadata) {
-            if (simple) nodes.sumOf { it.value } + sum()
-            else if (nodes.size == 1) sum()
-            else filter { it in 1 until nodes.size }.sumOf { nodes[it].value }
-        }.let { IndexedValue(nodes.last().index + metadata.size, it) }
-    }
+private fun List<Int>.value(simple: Boolean, nodes: List<IndexedValue<Int>>) = when {
+    simple -> nodes.sumOf { it.value } + sum()
+    nodes.isEmpty() -> sum()
+    else -> filter { it in 1..nodes.size }.sumOf { nodes[it - 1].value }
+}
+
+inline fun Int.times(f: (Int) -> Unit) = repeat(this, f)
