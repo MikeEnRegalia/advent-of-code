4651d92820f8adcac9516046d6ee2b07b682104e
Michael Böiers <michael.boiers@ingka.com>
2021-10-07 15:38:11 +0200

    refactor

--- src/main/kotlin/y2018/day09/Day09.kt
+++ src/main/kotlin/y2018/day09/Day09.kt
@@ -2,33 +2,41 @@ package y2018.day09
 
 import java.util.*
 
-fun day09(players: Int, marbles: Int): Long {
-    val circle = LinkedList(listOf(0))
-
-    fun MutableListIterator<Int>.move(delta: Int): MutableListIterator<Int> =
-        if (delta == 0) this else when {
-            delta < 0 -> if (hasPrevious()) apply { previous() } else circle.listIterator(circle.size - 1)
-            else -> if (hasNext()) apply { next() } else circle.listIterator().apply { next() }
-        }.move(if (delta > 0) delta - 1 else delta + 1)
-
+fun day09(players: Int, marbles: Int): Long = with(Circle()) {
     var currentPlayer = 0
     val scores = mutableMapOf<Int, Long>()
 
-    var i: MutableListIterator<Int> = circle.listIterator()
-
     for (marbleToPlace in 1 until marbles) {
         if (marbleToPlace % 23 > 0) {
-            i = i.move(2)
-            i.add(marbleToPlace)
-            i = i.move(-1)
+            move(2)
+            add(marbleToPlace)
+            move(-1)
         } else {
-            i = i.move(-7)
-            val other = i.next()
+            move(-7)
+            val other = next()
             scores.compute(currentPlayer) { _, old -> (old ?: 0) + marbleToPlace + other }
-            i.remove()
+            remove()
         }
         currentPlayer = (currentPlayer + 1) % players
     }
 
-    return scores.values.maxOf { it }
+    scores.values.maxOf { it }
+}
+
+private class Circle {
+    private val data = LinkedList(listOf(0))
+    private var i = data.listIterator()
+
+    fun move(delta: Int) {
+        when {
+            delta == 0 -> return
+            delta > 0 -> if (i.hasNext()) i.next() else i = data.listIterator(1)
+            else -> if (i.hasPrevious()) i.previous() else i = data.listIterator(data.size - 1)
+        }
+        move(if (delta > 0) delta - 1 else delta + 1)
+    }
+
+    fun add(marbleToPlace: Int) = i.add(marbleToPlace)
+    fun next(): Int = i.next()
+    fun remove() = i.remove()
 }
