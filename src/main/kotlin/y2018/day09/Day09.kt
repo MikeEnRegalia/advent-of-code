db3498388c93fe6760d732f0a6fa174de923196c
Michael Böiers <michael.boiers@ingka.com>
2021-10-07 16:04:40 +0200

    refactor

--- src/main/kotlin/y2018/day09/Day09.kt
+++ src/main/kotlin/y2018/day09/Day09.kt
@@ -8,10 +8,10 @@ fun day09(players: Int, marbles: Int): Long = with(Circle()) {
 
     for (marble in 1 until marbles) {
         if (marble % 23 == 0) {
-            move(-7)
+            backward(7)
             scores.upsert(player, 0) { it + marble + remove() }
         } else {
-            move(2)
+            forward(2)
             insert(marble)
         }
         player = (player + 1) % players
@@ -23,16 +23,21 @@ private class Circle {
     private val data = LinkedList(listOf(0))
     private var i = data.listIterator()
 
-    fun move(delta: Int) {
-        when {
-            delta == 0 -> return
-            delta > 0 -> if (i.hasNext()) i.next() else i = data.listIterator(1)
-            else -> if (i.hasPrevious()) i.previous() else i = data.listIterator(data.size - 1)
+    fun forward(delta: Int) {
+        if (delta > 0) {
+            if (i.hasNext()) i.next() else i = data.listIterator(1)
+            forward(delta - 1)
         }
-        move(if (delta > 0) delta - 1 else delta + 1)
     }
 
-    fun insert(marbleToPlace: Int) = i.add(marbleToPlace).also { move(-1) }
+    fun backward(delta: Int) {
+        if (delta > 0) {
+            if (i.hasPrevious()) i.previous() else i = data.listIterator(data.size - 1)
+            backward(delta - 1)
+        }
+    }
+
+    fun insert(marbleToPlace: Int) = i.add(marbleToPlace).also { backward(1) }
     fun remove(): Int = i.next().also { i.remove() }
 }
 
