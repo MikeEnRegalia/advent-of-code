e0953119ee6d5b781dd68ef648238bb629b149d7
Michael Böiers <michael.boiers@ingka.com>
2021-10-07 15:24:16 +0200

    refactor

--- src/main/kotlin/y2018/day09/Day09.kt
+++ src/main/kotlin/y2018/day09/Day09.kt
@@ -4,21 +4,28 @@ import java.util.*
 
 fun day09(players: Int, marbles: Int): Long {
     val circle = LinkedList(listOf(0))
+
+    fun MutableListIterator<Int>.move(delta: Int): MutableListIterator<Int> =
+        if (delta == 0) this else when {
+            delta < 0 -> if (hasPrevious()) apply { previous() } else circle.listIterator(circle.size - 1)
+            else -> if (hasNext()) apply { next() } else circle.listIterator().apply { next() }
+        }.move(if (delta > 0) delta - 1 else delta + 1)
+
     var currentPlayer = 0
     val scores = mutableMapOf<Int, Long>()
 
-    var i = circle.listIterator(circle.size)
+    var i: MutableListIterator<Int> = circle.listIterator()
 
     for (marbleToPlace in 1 until marbles) {
-        if (marbleToPlace % 23 == 0) {
-            repeat(8) { if (i.hasPrevious()) i.previous() else i = circle.listIterator(circle.size - 1) }
+        if (marbleToPlace % 23 > 0) {
+            i = i.move(2)
+            i.add(marbleToPlace)
+            i = i.move(-1)
+        } else {
+            i = i.move(-7)
             val other = i.next()
             scores.compute(currentPlayer) { _, old -> (old ?: 0) + marbleToPlace + other }
             i.remove()
-            i.next()
-        } else {
-            if (i.hasNext()) i.next() else i = circle.listIterator().apply { next() }
-            i.add(marbleToPlace)
         }
         currentPlayer = (currentPlayer + 1) % players
     }
