6b5a123eaf5128fbb9c8b8f65a503fb73a0706a6
Michael Böiers <michael.boiers@ingka.com>
2021-10-07 15:02:44 +0200

    refactor

--- src/main/kotlin/y2018/day09/Day09.kt
+++ src/main/kotlin/y2018/day09/Day09.kt
@@ -3,17 +3,19 @@ package y2018.day09
 import java.util.*
 
 fun day09(players: Int, marbles: Int): Long {
-    val circle = LinkedList<Int>().apply { add(0) }
+    val circle = LinkedList(listOf(0))
     var currentPlayer = 0
     val scores = mutableMapOf<Int, Long>()
 
-    var i = circle.listIterator().apply { next() }
+    var i = circle.listIterator(circle.size)
 
     for (marbleToPlace in 1 until marbles) {
         if (marbleToPlace % 23 == 0) {
             repeat(8) { if (i.hasPrevious()) i.previous() else i = circle.listIterator(circle.size - 1) }
-            val other = i.next().also { i.remove() }.also { i.next() }
+            val other = i.next()
             scores.compute(currentPlayer) { _, old -> (old ?: 0) + marbleToPlace + other }
+            i.remove()
+            i.next()
         } else {
             if (i.hasNext()) i.next() else i = circle.listIterator().apply { next() }
             i.add(marbleToPlace)
