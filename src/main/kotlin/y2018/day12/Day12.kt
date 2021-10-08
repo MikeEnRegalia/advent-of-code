2f71558d56cd80da42fdf5035bd4487130ec30a9
Michael Böiers <michael.boiers@ingka.com>
2021-10-08 16:26:33 +0200

    refactor

--- src/main/kotlin/y2018/day12/Day12.kt
+++ src/main/kotlin/y2018/day12/Day12.kt
@@ -1,22 +1,21 @@
 package y2018.day12
 
 typealias Pots = Iterable<Int>
-typealias Rules = List<Pair<Set<Int>, Boolean>>
+typealias Rules = Iterable<Pair<Set<Int>, Boolean>>
 
 fun Pots.simulateGrowth(rules: Rules, turns: Long): Long {
     var pots = this
     for (turnsLeft in turns downTo 1)
-        pots = pots.grow { rules.match(pots, it) }.also {
-            if (it.shape() == pots.shape()) return pots.score() + turnsLeft * (it.score() - pots.score())
+        pots = pots.widerRange().filter { it.growsWith(pots, rules) }.also { nextPots ->
+            if (nextPots.shape() == pots.shape()) return pots.score() + turnsLeft * (nextPots.score() - pots.score())
         }
     return pots.score()
 }
 
-private fun Pots.grow(f: (Int) -> Int?) = (minOf { it } - 2..maxOf { it } + 2).mapNotNull(f)
+private fun Pots.widerRange() = minOf { it } - 2..maxOf { it } + 2
+private fun Int.growsWith(pots: Pots, rules: Rules) =
+    rules.firstOrNull { (rule) -> rule.matches(pots, this) }?.second ?: false
 
-private fun Rules.match(pots: Pots, p: Int) =
-    firstOrNull { (rule) -> (-2..2).all { i -> (i in rule) == ((p + i) in pots) } }
-        ?.takeIf { it.second }?.let { p }
-
+private fun Set<Int>.matches(pots: Pots, p: Int) = (-2..2).all { i -> (i in this) == ((p + i) in pots) }
 private fun Pots.score() = sumOf { it }.toLong()
 private fun Pots.shape() = (minOf { it }..maxOf { it }).map { contains(it) }
