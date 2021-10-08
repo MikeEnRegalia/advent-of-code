626e14b992294ba449969f7d4a4ad9f52ed1678c
Michael Böiers <michael.boiers@ingka.com>
2021-10-08 14:36:20 +0200

    refactor

--- src/main/kotlin/y2018/day12/Day12.kt
+++ src/main/kotlin/y2018/day12/Day12.kt
@@ -4,25 +4,23 @@ typealias Pots = Map<Int, Boolean>
 
 fun Pots.simulateGrowth(rules: List<Pair<Pots, Boolean>>, generations: Long = 20): Long {
     var pots = this
-
-    for (generation in 0 until generations) {
-        val newPots = pots.toMutableMap()
-
-        for (potIndex in (pots.min() - 2)..(pots.max() + 2)) {
-            rules.find { (map) ->
-                map.entries.all { (ruleIndex, state) -> state == (pots[potIndex + ruleIndex] ?: false) }
-            }?.let { (_, grow) -> if (grow) newPots[potIndex] = true else newPots.remove(potIndex) }
-        }
-
-        if (newPots.signature() == pots.signature())
-            return pots.score() + (generations - generation) * (newPots.score() - pots.score())
-
-        pots = newPots
+    for (generationsLeft in (1..generations).reversed()) {
+        (pots.min() - 2..pots.max() + 2)
+            .mapNotNull { index ->
+                rules.find { it.first.entries.all { (d, state) -> state == (pots[index + d] ?: false) } }
+                    ?.let { index to it.second }
+            }
+            .fold(pots.toMutableMap()) { newPots, (i, grow) -> newPots.store(i, grow) }
+            .also {
+                if (it.signature() == pots.signature())
+                    return pots.score() + generationsLeft * (it.score() - pots.score())
+            }
+            .also { pots = it }
     }
-
     return pots.score().toLong()
 }
 
+private fun MutableMap<Int, Boolean>.store(i: Int, grow: Boolean) = apply { if (grow) this[i] = true else remove(i) }
 private fun Pots.score() = keys.sumOf { it }
 private fun Pots.min() = keys.minOf { it }
 private fun Pots.max() = keys.maxOf { it }
