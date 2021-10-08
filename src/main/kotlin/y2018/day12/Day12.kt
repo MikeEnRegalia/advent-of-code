aeb0656771551df4e311a0aa2e94dae30914d875
Michael Böiers <michael.boiers@ingka.com>
2021-10-08 16:06:33 +0200

    refactor

--- src/main/kotlin/y2018/day12/Day12.kt
+++ src/main/kotlin/y2018/day12/Day12.kt
@@ -12,12 +12,11 @@ fun Pots.simulateGrowth(rules: Rules, turns: Long): Long {
     return pots.score()
 }
 
-private fun Pots.grow(withRulesApplied: (Int) -> Int?) =
-    (minOf { it } - 2..maxOf { it } + 2).mapNotNull(withRulesApplied)
+private fun Pots.grow(f: (Int) -> Int?) = (minOf { it } - 2..maxOf { it } + 2).mapNotNull(f)
 
-private fun Rules.match(pots: Pots, index: Int) =
-    firstOrNull { (rule) -> (-2..2).all { i -> (i in rule) == ((index + i) in pots) } }
-        ?.takeIf { it.second }?.let { index }
+private fun Rules.match(pots: Pots, p: Int) =
+    firstOrNull { (rule) -> (-2..2).all { i -> (i in rule) == ((p + i) in pots) } }
+        ?.takeIf { it.second }?.let { p }
 
 private fun Pots.score() = sumOf { it }.toLong()
 private fun Pots.shape() = (minOf { it }..maxOf { it }).map { contains(it) }
