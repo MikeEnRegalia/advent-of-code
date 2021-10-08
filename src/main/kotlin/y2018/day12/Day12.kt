0965beee503847bf44a2db55eb119ce3cdfbd182
Michael Böiers <michael.boiers@ingka.com>
2021-10-08 16:03:48 +0200

    refactor

--- src/main/kotlin/y2018/day12/Day12.kt
+++ src/main/kotlin/y2018/day12/Day12.kt
@@ -1,23 +1,23 @@
 package y2018.day12
 
-fun Set<Int>.simulateGrowth(rules: List<Pair<Set<Int>, Boolean>>, turns: Long): Long {
-    var pots = this as Iterable<Int>
-    for (turnsLeft in turns downTo 1) {
-        pots.indexes()
-            .mapNotNull { rules.match(pots, it) }.toSet()
-            .also {
-                if (it.shape() == pots.shape()) return pots.score() + turnsLeft * (it.score() - pots.score())
-                else pots = it
-            }
-    }
+typealias Pots = Iterable<Int>
+typealias Rules = List<Pair<Set<Int>, Boolean>>
+
+fun Pots.simulateGrowth(rules: Rules, turns: Long): Long {
+    var pots = this
+    for (turnsLeft in turns downTo 1)
+        pots = pots.grow { rules.match(pots, it) }.also {
+            if (it.shape() == pots.shape()) return pots.score() + turnsLeft * (it.score() - pots.score())
+        }
     return pots.score()
 }
 
-private fun Iterable<Int>.indexes() = (minOf { it } - 2..maxOf { it } + 2).asSequence()
+private fun Pots.grow(withRulesApplied: (Int) -> Int?) =
+    (minOf { it } - 2..maxOf { it } + 2).mapNotNull(withRulesApplied)
 
-private fun List<Pair<Set<Int>, Boolean>>.match(pots: Iterable<Int>, index: Int) =
+private fun Rules.match(pots: Pots, index: Int) =
     firstOrNull { (rule) -> (-2..2).all { i -> (i in rule) == ((index + i) in pots) } }
         ?.takeIf { it.second }?.let { index }
 
-private fun Iterable<Int>.score() = sumOf { it }.toLong()
-private fun Iterable<Int>.shape() = (minOf { it }..maxOf { it }).map { contains(it) }
+private fun Pots.score() = sumOf { it }.toLong()
+private fun Pots.shape() = (minOf { it }..maxOf { it }).map { contains(it) }
