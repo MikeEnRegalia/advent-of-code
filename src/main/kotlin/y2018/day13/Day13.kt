cc97ccb1f5a0d20a5567012262ec5d06a1efa7c1
Michael Böiers <michael.boiers@ingka.com>
2021-10-09 12:53:56 +0200

    refactor

--- src/main/kotlin/y2018/day13/Day13.kt
+++ src/main/kotlin/y2018/day13/Day13.kt
@@ -5,43 +5,32 @@ import y2018.day13.Direction.*
 
 typealias Point = Pair<Int, Int>
 
-fun crash(input: List<String>): Pair<Point?, Point?> {
-    val movingCars = mutableListOf<Car>()
+fun crash(input: List<String>) = with(mutableListOf<Car>()) {
     val map = input.mapIndexed { y, r ->
-        r.mapIndexed { x, c -> c.underneathCar().also { c.Car(x, y)?.let { movingCars.add(it) } } }
+        r.mapIndexed { x, c -> c.underneathCar().also { c.Car(x, y)?.let { add(it) } } }
     }
 
     var firstCrashAt: Point? = null
-    while (movingCars.size > 1) {
-        movingCars.sortWith(compareBy({ it.pos.x }, { it.pos.y }))
-        movingCars.withIndex().forEach { (index, car) ->
-            with(car) {
-                copy(pos = pos.to(direction))
-                    .run {
-                        when (map[pos.y][pos.x]) {
-                            '+' -> atIntersection()
-                            '/' -> copy(direction = direction.turnAtSlashCorner())
-                            '\\' -> copy(direction = direction.turnAtBackslashCorner())
-                            else -> this
-                        }
-                    }.apply {
-                        movingCars[index] = this
-                        val crashed = movingCars.filter { it.pos == pos }
-                        if (crashed.count() > 1) {
-                            if (firstCrashAt == null) firstCrashAt = pos.x to pos.y
-                            movingCars.withIndex()
-                                .filter { (_, c) -> crashed.contains(c) }
-                                .forEach { (i, c) -> movingCars[i] = c.copy(crashed = true) }
-                        }
-                    }
+    while (size > 1) {
+        sortWith(compareBy({ it.pos.x }, { it.pos.y }))
+        withIndex().forEach { (index, car) ->
+            with(car.drive(map)) {
+                set(index, this)
+                val crashed = filter { it.pos == pos }
+                if (crashed.count() > 1) {
+                    if (firstCrashAt == null) firstCrashAt = pos.x to pos.y
+                    withIndex()
+                        .filter { (_, c) -> crashed.contains(c) }
+                        .forEach { (i, c) -> set(i, c.copy(crashed = true)) }
+                }
             }
         }
-        movingCars.removeIf { it.crashed }
+        removeIf { it.crashed }
     }
 
-    val lastCarPos = movingCars.firstNotNullOf { it }.pos.let { Point(it.x, it.y) }
+    val lastCarPos = firstNotNullOf { it }.pos.let { Point(it.x, it.y) }
 
-    return Pair(firstCrashAt, lastCarPos)
+    Pair(firstCrashAt, lastCarPos)
 }
 
 private fun Char.isCar() = this == '>' || this == '<' || this == '^' || this == 'v'
@@ -61,10 +50,19 @@ data class Pos(val x: Int, val y: Int) {
     }
 }
 
-fun Char.Car(x: Int, y: Int) = takeIf { it.isCar() }?.let { Car(Pos(x, y), it.asDirection(), TURN_LEFT) }
+internal fun Char.Car(x: Int, y: Int) = takeIf { it.isCar() }?.let { Car(Pos(x, y), it.asDirection(), TURN_LEFT) }
 
-data class Car(val pos: Pos, val direction: Direction, val nextAction: Action, val crashed: Boolean = false) {
-    fun atIntersection() = when (nextAction) {
+internal data class Car(val pos: Pos, val direction: Direction, val nextAction: Action, val crashed: Boolean = false) {
+    fun drive(map: List<List<Char>>) = copy(pos = pos.to(direction)).run {
+        when (map[pos.y][pos.x]) {
+            '+' -> atIntersection()
+            '/' -> copy(direction = direction.turnAtSlashCorner())
+            '\\' -> copy(direction = direction.turnAtBackslashCorner())
+            else -> this
+        }
+    }
+
+    private fun atIntersection() = when (nextAction) {
         TURN_LEFT -> copy(direction = direction.turnedLeft(), nextAction = FORWARD)
         FORWARD -> copy(nextAction = TURN_RIGHT)
         TURN_RIGHT -> copy(direction = direction.turnedRight(), nextAction = TURN_LEFT)
