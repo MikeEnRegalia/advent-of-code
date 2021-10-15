2a117ddc6ccd60b75e7fa13103362e01955a590a
Michael Böiers <michael.boiers@ingka.com>
2021-10-15 13:36:39 +0200

    refactor

--- src/main/kotlin/aoc2018/AoC2018Day16.kt
+++ src/main/kotlin/aoc2018/AoC2018Day16.kt
@@ -9,7 +9,7 @@ import aoc2018.AocDay16.toProgram
 fun String.day16ChronalClassificationPart1(): Int = toOpcodeCandidates().probe()
 fun String.day16ChronalClassificationPart2(): Int = toProgram().eval(toOpcodeCandidates().identifyOpcodes())
 
-internal typealias Opcode = (MutableList<Int>, Int, Int, Int) -> Unit
+internal typealias Opcode = (MutableList<Int>, Int, Int) -> Int
 
 internal object AocDay16 {
     fun String.toOpcodeCandidates() = split(Regex("\n\n\n\n"))[0]
@@ -23,25 +23,25 @@ internal object AocDay16 {
     data class OpCodeCandidate(val before: List<Int>, val cmd: List<Int>, val after: List<Int>)
 
     val opcodes = listOf<Opcode>(
-        { r, a, b, c -> r[c] = r[a] + r[b] },
-        { r, a, b, c -> r[c] = r[a] + b },
-        { r, a, b, c -> r[c] = r[a] * r[b] },
-        { r, a, b, c -> r[c] = r[a] * b },
-        { r, a, b, c -> r[c] = r[a].and(r[b]) },
-        { r, a, b, c -> r[c] = r[a].and(b) },
-        { r, a, b, c -> r[c] = r[a].or(r[b]) },
-        { r, a, b, c -> r[c] = r[a].or(b) },
-        { r, a, _, c -> r[c] = r[a] },
-        { r, a, _, c -> r[c] = a },
-        { r, a, b, c -> r[c] = if (a > r[b]) 1 else 0 },
-        { r, a, b, c -> r[c] = if (r[a] > b) 1 else 0 },
-        { r, a, b, c -> r[c] = if (r[a] > r[b]) 1 else 0 },
-        { r, a, b, c -> r[c] = if (a == r[b]) 1 else 0 },
-        { r, a, b, c -> r[c] = if (r[a] == b) 1 else 0 },
-        { r, a, b, c -> r[c] = if (r[a] == r[b]) 1 else 0 },
+        { r, a, b -> r[a] + r[b] },
+        { r, a, b -> r[a] + b },
+        { r, a, b -> r[a] * r[b] },
+        { r, a, b -> r[a] * b },
+        { r, a, b -> r[a].and(r[b]) },
+        { r, a, b -> r[a].and(b) },
+        { r, a, b -> r[a].or(r[b]) },
+        { r, a, b -> r[a].or(b) },
+        { r, a, _ -> r[a] },
+        { _, a, _ -> a },
+        { r, a, b -> if (a > r[b]) 1 else 0 },
+        { r, a, b -> if (r[a] > b) 1 else 0 },
+        { r, a, b -> if (r[a] > r[b]) 1 else 0 },
+        { r, a, b -> if (a == r[b]) 1 else 0 },
+        { r, a, b -> if (r[a] == b) 1 else 0 },
+        { r, a, b -> if (r[a] == r[b]) 1 else 0 },
     )
 
-    fun List<OpCodeCandidate>.probe() = count { c -> opcodes.count { c.resultOf(it) } >= 3 }
+    fun List<OpCodeCandidate>.probe() = count { c -> opcodes.count { c.isResultOf(it) } >= 3 }
 
     fun String.toProgram() = split(Regex("\n\n\n\n"))[1]
         .split("\n").map { row -> row.split(" ").map { it.toInt() } }
@@ -50,7 +50,7 @@ internal object AocDay16 {
     data class Instruction(val opcodeNumber: Int, val a: Int, val b: Int, val c: Int)
 
     fun List<Instruction>.eval(opcodes: Map<Int, Opcode>) =
-        mutableListOf(0, 0, 0, 0).let { r -> forEach { (n, a, b, c) -> (opcodes[n]!!)(r, a, b, c) }; r[0] }
+        mutableListOf(0, 0, 0, 0).let { r -> forEach { (n, a, b, c) -> r[c] = (opcodes[n]!!)(r, a, b) }; r[0] }
 
     internal fun List<OpCodeCandidate>.identifyOpcodes(): Map<Int, Opcode> {
         val allNumbers = map { it.cmd[0] }.distinct().toSet()
@@ -63,8 +63,10 @@ internal object AocDay16 {
     }
 
     private fun Collection<Opcode>.match(candidatesSameN: List<OpCodeCandidate>) =
-        filter { opcode -> candidatesSameN.all { it.resultOf(opcode) } }.takeIf { it.size == 1 }?.first()
+        filter { opcode -> candidatesSameN.all { it.isResultOf(opcode) } }.takeIf { it.size == 1 }?.first()
 
-    private fun OpCodeCandidate.resultOf(opcode: Opcode) =
-        before.toMutableList().also { opcode(it, cmd[1], cmd[2], cmd[3]) } == after
+    private fun OpCodeCandidate.isResultOf(opcode: Opcode) =
+        before.toMutableList().also {
+            it[cmd[3]] = opcode(it, cmd[1], cmd[2])
+        } == after
 }
