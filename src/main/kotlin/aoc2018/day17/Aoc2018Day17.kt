b39f7ff4b3f3cc581a67986097d3ef1afa74ea94
Michael Böiers <michael.boiers@ingka.com>
2021-10-16 23:03:12 +0200

    2018 day 17 part 1

--- src/main/kotlin/aoc2018/day17/Aoc2018Day17.kt
+++ src/main/kotlin/aoc2018/day17/Aoc2018Day17.kt
@@ -1,41 +1,75 @@
 package aoc2018.day17
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun day17ReservoirResearch(input: String): Int {
     val clay = input.loadMap()
     val flowingWater = mutableSetOf<Pos>()
     val stableWater = mutableSetOf<Pos>()
 
     val maxY = clay.maxOf { it.y }
-    val minX = clay.minOf { it.x }
-    val maxX = clay.maxOf { it.x }
 
     fun Pos.isClay() = this in clay
     fun Pos.isStableWater() = this in stableWater
+    fun Pos.isFlowingWater() = this in flowingWater
     fun Pos.supportsWater() = isClay() || isStableWater()
 
     fun Pos.boundsIfBasin(): Pair<Int, Int>? {
         var right = x
         while (true) {
-            if (right > maxX || !Pos(right, y + 1).supportsWater()) return null
+            if (!Pos(right, y + 1).supportsWater()) return null
             if (Pos(right + 1, y).isClay()) break
             right++
         }
 
         var left = x
         while (true) {
-            if (left < minX || !Pos(left, y + 1).supportsWater()) return null
+            if (!Pos(left, y + 1).supportsWater()) return null
             if (Pos(left - 1, y).isClay()) break
             left--
         }
 
-        return (left to right).also { println(it) }
+        return left to right
+    }
+
+    fun Pos.cliffs(): List<Pos> {
+        var right = x
+        var rightIsCliff = true
+        while (true) {
+            flowingWater.add(Pos(right, y))
+            if (!Pos(right, y + 1).supportsWater()) break
+            if (Pos(right + 1, y).isClay()) {
+                rightIsCliff = false
+                break
+            }
+            right++
+        }
+
+        var left = x
+        var leftIsCliff = true
+        while (true) {
+            flowingWater.add(Pos(left, y))
+            if (!Pos(left, y + 1).supportsWater()) break
+            if (Pos(left - 1, y).isClay()) {
+                leftIsCliff = false
+                break
+            }
+            left--
+        }
+
+        return listOfNotNull(
+            if (leftIsCliff) Pos(left, y) else null,
+            if (rightIsCliff) Pos(right, y) else null
+        )
     }
 
     fun fromSpring(spring: Pos): List<Pos> {
+        if (spring.below().isFlowingWater()) return listOf()
         var p = spring
         while (true) {
             p = p.below()
-            if (p.y >= maxY) return listOf()
+            if (p.y > maxY) return listOf()
             if (p.supportsWater()) {
                 p = p.above()
                 break
@@ -43,11 +77,14 @@ fun day17ReservoirResearch(input: String): Int {
             flowingWater.add(p)
         }
 
-        val (left, right) = p.boundsIfBasin() ?: return listOf()
+        while (true) {
+            val (left, right) = p.boundsIfBasin() ?: return p.cliffs()
 
-        for (x in left..right) stableWater.add(Pos(x, p.y))
-
-        return listOf()
+            for (x in left..right) {
+                stableWater.add(Pos(x, p.y))
+            }
+            p = p.above()
+        }
     }
 
     var springs = listOf(Pos(500, 0))
@@ -56,9 +93,6 @@ fun day17ReservoirResearch(input: String): Int {
     }
 
     val water = flowingWater.union(stableWater).toMutableSet()
-
-    println("----------------------")
-    println(clay.render(flowingWater, stableWater))
 
     water.removeIf { w -> w.y !in clay.minOf { it.y }..clay.maxOf { it.y } }
 
@@ -70,9 +104,10 @@ internal data class Pos(val x: Int, val y: Int) {
     fun above() = copy(y = y - 1)
 }
 
+@Suppress("unused")
 internal fun Set<Pos>.render(flowingWater: Set<Pos>, stableWater: Set<Pos>, spring: Pos = Pos(500, 0)): String {
-    val from = Pos(minOf { it.x }, 0)
-    val to = Pos(maxOf { it.x }, maxOf { it.y })
+    val from = Pos(min(minOf { it.x }, flowingWater.minOf { it.x }), 0)
+    val to = Pos(max(maxOf { it.x }, flowingWater.maxOf { it.x }), maxOf { it.y })
 
     return (from.y..to.y).joinToString("\n") { y ->
         (from.x..to.x).joinToString("") { x ->
