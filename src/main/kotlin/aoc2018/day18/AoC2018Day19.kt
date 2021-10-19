f102d7440142274ea4441a455a55307cfe8774f9
Michael Böiers <michael.boiers@ingka.com>
2021-10-19 15:19:35 +0200

    2018 day 19 part 1

---
+++ src/main/kotlin/aoc2018/day18/AoC2018Day19.kt
@@ -0,0 +1,41 @@
+package aoc2018.day18
+
+
+fun day19(input: String, r0: Int = 0): Int {
+    val ipRegister = input.substring("#ip ".length, input.indexOf("\n")).toInt()
+    val program = input.substring(input.indexOf("\n") + 1).split("\n")
+        .map { it.split(" ") }
+        .map { it[0].opcode() to it.drop(1).map { it.toInt() } }
+
+    var ip = 0
+    val r = mutableListOf(r0, 0, 0, 0, 0, 0)
+    while (ip in program.indices) {
+        val (opcode, data) = program[ip]
+        r[ipRegister] = ip
+        r[data[2]] = r.opcode(data[0], data[1])
+        ip = r[ipRegister] + 1
+    }
+    return r[0]
+}
+
+internal typealias Opcode = MutableList<Int>.(Int, Int) -> Int
+
+internal fun String.opcode(): Opcode = when (this) {
+    "addr" -> { a, b -> this[a] + this[b] }
+    "addi" -> { a, b -> this[a] + b }
+    "mulr" -> { a, b -> this[a] * this[b] }
+    "muli" -> { a, b -> this[a] * b }
+    "banr" -> { a, b -> this[a].and(this[b]) }
+    "bani" -> { a, b -> this[a].and(b) }
+    "borr" -> { a, b -> this[a].or(this[b]) }
+    "bori" -> { a, b -> this[a].or(b) }
+    "setr" -> { a, _ -> this[a] }
+    "seti" -> { a, _ -> a }
+    "gtir" -> { a, b -> if (a > this[b]) 1 else 0 }
+    "gtri" -> { a, b -> if (this[a] > b) 1 else 0 }
+    "gtrr" -> { a, b -> if (this[a] > this[b]) 1 else 0 }
+    "eqir" -> { a, b -> if (a == this[b]) 1 else 0 }
+    "eqri" -> { a, b -> if (this[a] == b) 1 else 0 }
+    "eqrr" -> { a, b -> if (this[a] == this[b]) 1 else 0 }
+    else -> throw IllegalStateException(this)
+}
---
+++ src/test/kotlin/aoc2018/day18/AoC2018Day19KtTest.kt
@@ -0,0 +1,66 @@
+package aoc2018.day18
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+private const val INPUT = """#ip 5
+addi 5 16 5
+seti 1 8 3
+seti 1 1 1
+mulr 3 1 4
+eqrr 4 2 4
+addr 4 5 5
+addi 5 1 5
+addr 3 0 0
+addi 1 1 1
+gtrr 1 2 4
+addr 5 4 5
+seti 2 7 5
+addi 3 1 3
+gtrr 3 2 4
+addr 4 5 5
+seti 1 5 5
+mulr 5 5 5
+addi 2 2 2
+mulr 2 2 2
+mulr 5 2 2
+muli 2 11 2
+addi 4 8 4
+mulr 4 5 4
+addi 4 20 4
+addr 2 4 2
+addr 5 0 5
+seti 0 4 5
+setr 5 8 4
+mulr 4 5 4
+addr 5 4 4
+mulr 5 4 4
+muli 4 14 4
+mulr 4 5 4
+addr 2 4 2
+seti 0 7 0
+seti 0 9 5"""
+
+internal class AoC2018Day19KtTest {
+    @Test
+    fun test() = day19(
+        """#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5"""
+    ) shouldBe 6
+
+    @Test
+    fun part1() {
+        day19(INPUT) shouldBe 2640
+    }
+
+    @Test
+    fun part2() {
+        day19(INPUT, 1) shouldBe 2640
+    }
+}
