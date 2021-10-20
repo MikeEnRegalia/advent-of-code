e06dd6755440407d96059a2b5894ef4bf72c8056
Michael Böiers <michael.boiers@ingka.com>
2021-10-20 14:32:01 +0200

    refactor

--- src/main/kotlin/aoc2018/day22/AoC2018Day22.kt
+++ src/main/kotlin/aoc2018/day22/AoC2018Day22.kt
@@ -1,22 +1,22 @@
 package aoc2018.day22
 
-fun day22(depth: Int, targetX: Int, targetY: Int): Int = with(Problem(depth, targetX, targetY)) {
+fun day22(depth: Int, targetX: Int, targetY: Int): Int = with(Cave(depth, targetX, targetY)) {
     zeroToTarget().sumOf { (x, y) -> erosionLevel(x, y) % 3 }
 }
 
-data class Problem(val depth: Int, val targetX: Int, val targetY: Int) {
+internal data class Cave(val depth: Int, val targetX: Int, val targetY: Int) {
     val erosionCache: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
-
-    fun zeroToTarget() = (0..targetX).flatMap { x -> (0..targetY).map { y -> x to y } }
-
-    private fun geologicalIndex(x: Int, y: Int): Int = when {
-        x == 0 && y == 0 || x == targetX && y == targetY -> 0
-        x == 0 -> y * 48271
-        y == 0 -> x * 16807
-        else -> erosionLevel(x - 1, y) * erosionLevel(x, y - 1)
-    }
-
-    fun erosionLevel(x: Int, y: Int) = erosionCache.computeIfAbsent(x to y) { calculateErosionLevel(x, y) }
-
-    private fun calculateErosionLevel(x: Int, y: Int) = (geologicalIndex(x, y) + depth) % 20183
 }
+
+internal fun Cave.calculateErosionLevel(x: Int, y: Int) = (geologicalIndex(x, y) + depth) % 20183
+
+internal fun Cave.erosionLevel(x: Int, y: Int) = erosionCache.computeIfAbsent(x to y) { calculateErosionLevel(x, y) }
+
+internal fun Cave.geologicalIndex(x: Int, y: Int): Int = when {
+    x == 0 && y == 0 || x == targetX && y == targetY -> 0
+    x == 0 -> y * 48271
+    y == 0 -> x * 16807
+    else -> erosionLevel(x - 1, y) * erosionLevel(x, y - 1)
+}
+
+internal fun Cave.zeroToTarget() = (0..targetX).flatMap { x -> (0..targetY).map { y -> x to y } }
