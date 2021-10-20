6273c6351906277a1051df346803170d30765049
Michael Böiers <michael.boiers@ingka.com>
2021-10-20 11:51:50 +0200

    refactor

--- src/main/kotlin/aoc2018/day22/AoC2018Day22.kt
+++ src/main/kotlin/aoc2018/day22/AoC2018Day22.kt
@@ -1,29 +1,21 @@
 package aoc2018.day22
 
-typealias Cache = MutableMap<Pair<Int, Int>, Int>
-
-fun day22(depth: Int, targetX: Int, targetY: Int): Int {
-    val cache: Cache = mutableMapOf()
-    return (0..targetX).flatMap { x ->
-        (0..targetY).map { y -> erosionLevel(depth, x, y, targetX, targetY, cache) % 3 }
-    }.sum()
+fun day22(depth: Int, targetX: Int, targetY: Int): Int = with(Problem(depth, targetX, targetY)) {
+    (0..targetX)
+        .flatMap { x -> (0..targetY).map { y -> erosionLevel(x, y) % 3 } }
+        .sum()
 }
 
-internal fun geologicalIndex(depth: Int, x: Int, y: Int, targetX: Int, targetY: Int, cache: Cache): Int = when {
-    x == 0 && y == 0 || x == targetX && y == targetY -> 0
-    x == 0 -> y * 48271
-    y == 0 -> x * 16807
-    else -> erosionLevel(depth, x - 1, y, targetX, targetY, cache) * erosionLevel(
-        depth,
-        x,
-        y - 1,
-        targetX,
-        targetY,
-        cache
-    )
-}
+data class Problem(val depth: Int, val targetX: Int, val targetY: Int) {
+    val erosionLevels: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
 
-internal fun erosionLevel(depth: Int, x: Int, y: Int, targetX: Int, targetY: Int, cache: Cache) =
-    cache[x to y] ?: ((geologicalIndex(depth, x, y, targetX, targetY, cache) + depth) % 20183).also {
-        cache[x to y] = it
+    fun geologicalIndex(x: Int, y: Int): Int = when {
+        x == 0 && y == 0 || x == targetX && y == targetY -> 0
+        x == 0 -> y * 48271
+        y == 0 -> x * 16807
+        else -> erosionLevel(x - 1, y) * erosionLevel(x, y - 1)
     }
+
+    fun erosionLevel(x: Int, y: Int) =
+        erosionLevels[x to y] ?: ((geologicalIndex(x, y) + depth) % 20183).also { erosionLevels[x to y] = it }
+}
