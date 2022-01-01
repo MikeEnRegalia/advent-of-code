f6c28eb369947188e208f53572402d120487f7e1
Michael Böiers <michael.boiers@ingka.com>
2022-01-01 13:45:59 +0100

    2020 day 18

--- src/main/kotlin/aoc2020/Day18.kt
+++ src/main/kotlin/aoc2020/Day18.kt
@@ -3,49 +3,45 @@ package aoc2020
 import aoc2020.AoC2020Day18.Parser
 
 fun main() = with(generateSequence(::readLine).toList()) {
-    sequenceOf(false, true).forEach { proper -> sumOf { Parser(it, proper).expression() }.also { println(it) } }
+    sequenceOf(false, true).forEach { part2 -> sumOf { Parser(it, part2).expression() }.also(::println) }
 }
 
 object AoC2020Day18 {
-    class Parser(val line: String, val proper: Boolean, var pos: Int = 0) {
-
-        fun expression(parentheses: Boolean = false): Long {
-            if (parentheses) read()
+    class Parser(val line: String, val part2: Boolean, var pos: Int = 0) {
+        fun expression(): Long {
             val r = mutableListOf<Long>()
-            while (pos != line.length) {
-                val c = peek() ?: break
-                when {
-                    c.isDigit() -> r += literal()
-                    c == '(' -> r += expression(true)
-                    c == '+' -> {
-                        read()
-                        if (peek() == ' ') read()
-                        r[r.size - 1] =
-                            r.last() + if (peek()!!.isDigit()) literal() else expression(true)
-                    }
-                    c == '*' -> {
-                        read()
-                        if (peek() == ' ') read()
-                        if (proper) {
-                            r += if (peek()!!.isDigit()) literal() else expression(true)
-                        } else {
-                            r[r.size - 1] = r.last() * if (peek()!!.isDigit()) literal() else expression(true)
-                        }
-                    }
-                    c == ')' -> {
-                        if (parentheses) read()
-                        return r.reduce(Long::times)
-                    }
+            while (pos < line.length) when (val c = peek()) {
+                ' ' -> read(c)
+                '+' -> {
+                    read(c)
+                    read(' ')
+                    r[r.size - 1] = r.last() + term()
                 }
-                if (peek() == ' ') read()
+                '*' -> {
+                    read(c)
+                    read(' ')
+                    if (part2) r += term() else r[r.size - 1] = r.last() * term()
+                }
+                ')' -> break
+                else -> r += term()
             }
             return r.reduce(Long::times)
+        }
+
+        fun term() = if (peek() == '(') parentheses() else literal()
+
+        fun parentheses(): Long {
+            read('(')
+            val result = expression()
+            read(')')
+            return result
         }
 
         fun literal() = buildString { while (peek()?.isDigit() == true) append(read()) }.toLong()
 
         fun peek() = line.getOrNull(pos)
-
-        fun read(): Char = peek().also { pos++ }!!
+        fun read(c: Char? = null): Char = peek()
+            .also { if (c != null && it != c) throw IllegalStateException(it.toString()) }
+            .also { pos++ }!!
     }
 }
