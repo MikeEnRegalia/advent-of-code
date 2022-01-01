71db235f9cc1367e7b508a5aba5357f74f3f80dc
Michael Böiers <michael.boiers@ingka.com>
2022-01-01 14:18:49 +0100

    2020 day 18

--- src/main/kotlin/aoc2020/Day18.kt
+++ src/main/kotlin/aoc2020/Day18.kt
@@ -1,47 +1,43 @@
 package aoc2020
 
-import aoc2020.AoC2020Day18.Parser
-
 fun main() = with(generateSequence(::readLine).toList()) {
-    sequenceOf(false, true).forEach { part2 -> sumOf { Parser(it, part2).expression() }.also(::println) }
+    sequenceOf(false, true).forEach { part2 -> sumOf { AoC2020Day18(it, part2).expression() }.also(::println) }
 }
 
-object AoC2020Day18 {
-    class Parser(val line: String, val part2: Boolean, var pos: Int = 0) {
-        fun expression(): Long {
-            val r = mutableListOf<Long>()
-            while (pos < line.length) when (val c = peek()) {
-                ' ' -> read(c)
-                '+' -> {
-                    read(c)
-                    read(' ')
-                    r[r.size - 1] = r.last() + term()
-                }
-                '*' -> {
-                    read(c)
-                    read(' ')
-                    if (part2) r += term() else r[r.size - 1] = r.last() * term()
-                }
-                ')' -> break
-                else -> r += term()
+class AoC2020Day18(val line: String, val part2: Boolean, var pos: Int = 0) {
+    fun expression(): Long {
+        val r = mutableListOf<Long>()
+        while (pos < line.length) when (val c = peek()) {
+            ' ' -> read(c)
+            '+' -> {
+                read(c)
+                read(' ')
+                r[r.size - 1] = r.last() + term()
             }
-            return r.reduce(Long::times)
+            '*' -> {
+                read(c)
+                read(' ')
+                if (part2) r += term() else r[r.size - 1] = r.last() * term()
+            }
+            ')' -> break
+            else -> r += term()
         }
-
-        fun term() = if (peek() == '(') parentheses() else literal()
-
-        fun parentheses(): Long {
-            read('(')
-            val result = expression()
-            read(')')
-            return result
-        }
-
-        fun literal() = buildString { while (peek()?.isDigit() == true) append(read()) }.toLong()
-
-        fun peek() = line.getOrNull(pos)
-        fun read(c: Char? = null): Char = peek()
-            .also { if (c != null && it != c) throw IllegalStateException(it.toString()) }
-            .also { pos++ }!!
+        return r.reduce(Long::times)
     }
+
+    fun term() = if (peek() == '(') parentheses() else literal()
+
+    fun parentheses(): Long {
+        read('(')
+        val result = expression()
+        read(')')
+        return result
+    }
+
+    fun literal() = buildString { while (peek()?.isDigit() == true) append(read()) }.toLong()
+
+    fun peek() = line.getOrNull(pos)
+    fun read(c: Char? = null): Char = peek()
+        .also { if (c != null && it != c) throw IllegalStateException(it.toString()) }
+        .also { pos++ }!!
 }
