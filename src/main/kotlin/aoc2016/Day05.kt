453d44713c8c60dfd99c123c3325e914d32066e2
Michael Böiers <michael.boiers@ingka.com>
2022-01-04 15:37:02 +0100

    2016 day 5

--- src/main/kotlin/aoc2016/Day05.kt
+++ src/main/kotlin/aoc2016/Day05.kt
@@ -6,27 +6,24 @@ import java.security.MessageDigest
 fun main() {
     val input = "abbhdwsy"
 
-    fun seq(): Sequence<String> {
-        var x = 0
-        return generateSequence { "$input$x".md5().also { x++ } }.filter { it.startsWith("00000") }
-    }
+    fun seq() = generateSequence(0) { it + 1 }.map { "$input$it" }
+        .map(String::md5).filter { it.startsWith("00000") }
 
     val part1 = seq().map { it[5].toString() }.take(8).joinToString("")
     println(part1)
 
     val part2 = MutableList<String?>(8) { null }.run {
-        seq().forEach { hash ->
-            hash[5].digitToIntOrNull()?.takeIf { it in indices && get(it) == null }?.let { pos ->
-                set(pos, hash[6].toString())
+        seq().mapNotNull { hash -> hash[5].digitToIntOrNull()?.let { it to hash[6] } }
+            .filter { (pos) -> pos in indices && get(pos) == null }
+            .forEach { (pos, code) ->
+                set(pos, code.toString())
                 if (!contains(null)) return@run joinToString("")
             }
-        }
     }
     println(part2)
 }
 
 fun String.md5(): String {
     val md = MessageDigest.getInstance("MD5")
-    val bigInt = BigInteger(1, md.digest(toByteArray(Charsets.UTF_8)))
-    return String.format("%032x", bigInt)
+    return String.format("%032x", BigInteger(1, md.digest(toByteArray(Charsets.UTF_8))))
 }
