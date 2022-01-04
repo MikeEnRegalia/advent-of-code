d9692a5eca3a5f3dd53a92832a957a8419bee2af
Michael Böiers <michael.boiers@ingka.com>
2022-01-04 13:36:34 +0100

    refactor

--- src/main/kotlin/aoc2016/Day04.kt
+++ src/main/kotlin/aoc2016/Day04.kt
@@ -1,36 +1,33 @@
 package aoc2016
 
 fun main() {
-    val input = generateSequence(::readLine)
+    fun String.shift(n: Int = 1) = buildString {
+        for (c in this@shift) append(
+            when (c) {
+                '-', ' ' -> ' '
+                else -> 'a' + ((c - 'a') + n) % 26
+            }
+        )
+    }
+
+    data class Room(val name: String, val sectorId: Int, val checksum: String) {
+        val valid by lazy {
+            checksum == name.filterNot { it == '-' }
+                .groupingBy { it.toString() }.eachCount().entries
+                .sortedBy { it.key }.sortedByDescending { it.value }
+                .take(5).joinToString("") { it.key }
+        }
+        val decrypted by lazy { if (valid) name.shift(sectorId % 26) else null }
+    }
+
+    val rooms = generateSequence(::readLine)
         .mapNotNull { Regex("""^(.*)-(\d+)\[(.*)]$""").find(it)?.destructured?.toList() }
-        .map { (room, sectorId, checksum) -> Triple(room, sectorId.toInt(), checksum) }
+        .map { Room(it[0], it[1].toInt(), it[2]) }
+        .filter(Room::valid)
         .toList()
 
-    val real = input
-        .filter { (room, _, checksum) ->
-            room.filterNot { it == '-' }
-                .groupingBy { it.toString() }.eachCount().entries
-                .sortedWith(compareByDescending<Map.Entry<String, Int>> { it.value }.thenBy { it.key })
-                .take(5).joinToString("") { it.key } == checksum
-        }
-
-    println(real.sumOf { it.second })
-
-    real.map { (room, sectorId) ->
-        var newRoom = room
-        repeat(sectorId % 26) {
-            newRoom = newRoom.map {
-                when (it) {
-                    'z' -> 'a'
-                    '-' -> ' '
-                    ' ' -> '-'
-                    else -> it + 1
-                }
-            }.joinToString("")
-        }
-        newRoom to sectorId
+    with(rooms) {
+        sumOf(Room::sectorId).also(::println)
+        single { it.decrypted == "northpole object storage" }.sectorId.also(::println)
     }
-        .filter { it.second in 392..598 }
-        .sortedBy { it.first }
-        .forEach(::println)
 }
