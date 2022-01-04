cff4fd7cb315fe2fe505eb41f82a0f55d4af4fa1
Michael Böiers <michael.boiers@ingka.com>
2022-01-04 13:56:54 +0100

    refactor

--- src/main/kotlin/aoc2016/Day04.kt
+++ src/main/kotlin/aoc2016/Day04.kt
@@ -1,8 +1,6 @@
 package aoc2016
 
 fun main() {
-    fun String.shift(n: Int = 1) = map { c -> if ("- ".contains(c)) ' ' else 'a' + (c - 'a' + n) % 26 }
-
     data class Room(val name: String, val sectorId: Int, val checksum: String) {
         val valid by lazy {
             checksum == name.filterNot { it == '-' }
@@ -10,15 +8,16 @@ fun main() {
                 .sortedBy { it.key }.sortedByDescending { it.value }
                 .take(5).joinToString("") { it.key }
         }
-        val decrypted by lazy { name.takeIf { valid }?.shift(sectorId % 26)?.joinToString("") }
+        val decrypted by lazy {
+            if (!valid) null
+            else name.map { c -> if (c in "- ") ' ' else 'a' + (c - 'a' + sectorId) % 26 }.joinToString("")
+        }
     }
 
-    val rooms = generateSequence(::readLine)
+    generateSequence(::readLine)
         .map { Regex("""^(.*)-(\d+)\[(.*)]$""").find(it)!!.destructured }
         .map { (name, sectorId, checksum) -> Room(name, sectorId.toInt(), checksum) }
-
-    with(rooms.filter(Room::valid).toList()) {
-        sumOf(Room::sectorId).also(::println)
-        single { it.decrypted == "northpole object storage" }.sectorId.also(::println)
-    }
+        .filter(Room::valid).toList()
+        .run { sumOf(Room::sectorId) to single { it.decrypted == "northpole object storage" }.sectorId }
+        .also(::println)
 }
