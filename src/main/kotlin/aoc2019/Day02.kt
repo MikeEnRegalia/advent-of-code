041ceaed104352087c5fff128b7100ae3a0f4d2b
Michael Böiers <michael.boiers@ingka.com>
2022-01-05 21:37:00 +0100

    2019 day 5 intcode enhancement

--- src/main/kotlin/aoc2019/Day02.kt
+++ src/main/kotlin/aoc2019/Day02.kt
@@ -14,8 +14,12 @@ fun main() {
         if (19690720 == runIntCode(input.mod(noun, verb))) println(noun * 100 + verb)
 }
 
-fun runIntCode(input: List<Int>): Int? {
-    val code = input.mapIndexed { i, x -> i to x }.toMap().toMutableMap()
+fun runIntCode(
+    program: List<Int>,
+    input: () -> Int = { throw IllegalStateException() },
+    output: (Int) -> Unit = {}
+): Int? {
+    val code = program.mapIndexed { i, x -> i to x }.toMap().toMutableMap()
     var pos = 0
     while (true) {
         when (val opcode = code[pos]) {
@@ -24,6 +28,14 @@ fun runIntCode(input: List<Int>): Int? {
                 val b = code.getOrDefault(code[pos + 2]!!, 0)
                 code[code[pos + 3]!!] = if (opcode == 1) a.plus(b) else a.times(b)
                 pos += 4
+            }
+            3 -> {
+                code[code[pos + 1]!!] = input()
+                pos += 2
+            }
+            4 -> {
+                output(code[pos + 1]!!)
+                pos += 2
             }
             99 -> return code[0]
         }
