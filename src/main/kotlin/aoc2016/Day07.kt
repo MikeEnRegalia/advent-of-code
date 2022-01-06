7b5f61ef3c0de6b36f1388142a6eb1ed4213a999
Michael Böiers <michael.boiers@ingka.com>
2022-01-06 13:27:06 +0100

    2016 day 7

--- src/main/kotlin/aoc2016/Day07.kt
+++ src/main/kotlin/aoc2016/Day07.kt
@@ -1,7 +1,7 @@
 package aoc2016
 
 fun main() {
-    val lines = generateSequence { readLine() }
+    val lines = generateSequence { readLine() }.toList()
     val part1 = lines.count { line ->
         val abbas = (0..line.length - 4).filter {
             val abba = line.substring(it, it + 4)
@@ -10,6 +10,17 @@ fun main() {
         abbas.none { line.inBrackets(it) } && abbas.any { !line.inBrackets(it) }
     }
     println(part1)
+    val part2 = lines.count { line ->
+        val abas = (0..line.length - 3).filter {
+            val aba = line.substring(it, it + 3)
+            aba[0] != aba[1] && aba.substring(0, 2) == aba.substring(1).reversed()
+        }
+        abas.filter { !line.inBrackets(it) }.any { abaI ->
+            val correspondingAba = "${line[abaI + 1]}${line[abaI]}${line[abaI + 1]}"
+            abas.filter { line.inBrackets(it) }.any { cabaI -> line.substring(cabaI, cabaI + 3) == correspondingAba }
+        }
+    }
+    println(part2)
 }
 
 fun String.inBrackets(pos: Int) =
