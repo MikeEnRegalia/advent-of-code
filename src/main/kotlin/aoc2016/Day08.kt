72f7621d319ff57b06aab501ba5b55850b735996
Michael Böiers <michael.boiers@ingka.com>
2022-01-06 22:31:16 +0100

    refactor

--- src/main/kotlin/aoc2016/Day08.kt
+++ src/main/kotlin/aoc2016/Day08.kt
@@ -2,31 +2,31 @@ package aoc2016
 
 fun main() {
     val (width, height) = 50 to 6
-    val screen = mutableMapOf<Pair<Int, Int>, Boolean>()
+    val screen = mutableSetOf<Pair<Int, Int>>()
     for (line in generateSequence(::readLine)) when {
         line.startsWith("rect ") -> {
             val (a, b) = line.substringAfter("rect ").split("x").map(String::toInt)
-            for (x in 0 until a) for (y in 0 until b) screen[Pair(x, y)] = true
+            for (x in 0 until a) for (y in 0 until b) screen += x to y
         }
         line.startsWith("rotate row y=") -> {
             val (row, offset) = line.substringAfter("rotate row y=").split(Regex(" by ")).map(String::toInt)
-            with(screen.keys.filter { (_, y) -> y == row }) {
+            with(screen.filter { (_, y) -> y == row }) {
                 forEach(screen::remove)
-                forEach { (x, y) -> screen[Pair((x + offset) % width, y)] = true }
+                forEach { (x, y) -> screen += (x + offset) % width to y }
             }
         }
         line.startsWith("rotate column x=") -> {
             val (col, offset) = line.substringAfter("rotate column x=").split(Regex(" by ")).map(String::toInt)
-            with(screen.keys.filter { (x) -> x == col }) {
+            with(screen.filter { (x) -> x == col }) {
                 forEach(screen::remove)
-                forEach { (x, y) -> screen[Pair(x, (y + offset) % height)] = true }
+                forEach { (x, y) -> screen += x to (y + offset) % height }
             }
         }
     }
 
-    println(screen.count { it.value })
+    println(screen.count())
 
     (0 until height).joinToString("\n") { y ->
-        (0 until width).joinToString("") { x -> if (screen[Pair(x, y)] == true) "*" else " " }
+        (0 until width).joinToString("") { x -> if (x to y in screen) "*" else " " }
     }.also(::println)
 }
