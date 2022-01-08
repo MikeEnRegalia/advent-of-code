f3fb1e15bb4a08ec7946c2db10bca0578a379df0
Michael Böiers <michael.boiers@ingka.com>
2022-01-08 14:19:05 +0100

    refactor

--- src/main/kotlin/aoc2016/Day09.kt
+++ src/main/kotlin/aoc2016/Day09.kt
@@ -1,31 +1,31 @@
 package aoc2016
 
 fun main() {
-    fun String.decompress(rec: Boolean = false): List<Pair<Long, String>> {
-        val decompressed = mutableListOf<Pair<Long, String>>()
+    fun String.decompress(rec: Boolean = false): List<Pair<Long, Long>> {
+        val decompressed = mutableListOf<Pair<Long, Long>>()
         var pos = 0
         while (pos < length) {
             val next = indexOf('(', pos)
             if (next == -1) {
-                decompressed += 1L to substring(pos)
+                decompressed += 1L to (length.toLong() - pos)
                 pos = length
                 continue
             }
 
             val data = substring(pos, next)
-            decompressed += 1L to data
+            decompressed += 1L to data.length.toLong()
 
             val end = indexOf(')', next)
             val (l, x) = substring(next + 1, end).split("x").map(String::toInt)
             val block = substring(end + 1, end + 1 + l)
             if (rec) decompressed += block.decompress(rec = true).map { (x2, data) -> x.toLong() * x2 to data }
-            else decompressed += x.toLong() to block
+            else decompressed += x.toLong() to block.length.toLong()
             pos = end + 1 + l
         }
         return decompressed
     }
 
     with(generateSequence(::readLine).joinToString("")) {
-        sequenceOf(decompress(), decompress(true)).map { it.sumOf { (x, data) -> x * data.length } }.forEach(::println)
+        sequenceOf(decompress(), decompress(true)).map { it.sumOf { (x, d) -> x * d } }.forEach(::println)
     }
 }
