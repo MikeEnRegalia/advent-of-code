2ce91f9e3a8f9cbf2fc5249a8973be15e1f12dd3
Michael Böiers <michael.boiers@ingka.com>
2022-01-09 22:57:33 +0100

    refactor

--- src/main/kotlin/aoc2016/Day10.kt
+++ src/main/kotlin/aoc2016/Day10.kt
@@ -1,44 +1,43 @@
 package aoc2016
 
+fun <T> Regex.finD(s: String, f: (MatchResult.Destructured) -> T) = f(find(s)!!.destructured)
+
 fun main() {
     val bots = mutableMapOf<Int, Pair<Int?, Int?>>()
     val rules = mutableMapOf<Int, Pair<Pair<String, Int>, Pair<String, Int>>>()
 
     fun giveToBot(b: Int, v: Int) {
         val values = bots[b]
-        val x = values?.first ?: values?.second
-        bots[b] = if (x == null) v to null else if (x > v) v to x else x to v
+        if (values == null) {
+            bots[b] = v to null
+            return
+        }
+        val x = values.first!!
+        bots[b] = if (x > v) v to x else x to v
     }
 
     generateSequence(::readLine).forEach { line ->
         if (line.startsWith("value ")) {
-            val (v, b) = """^value (\d+) goes to bot (\d+)$""".toRegex()
-                .find(line)!!.destructured.toList().let { it[0].toInt() to it[1].toInt() }
+            val (b, v) = """^value (\d+) goes to bot (\d+)$""".toRegex()
+                .finD(line) { (v, b) -> b.toInt() to v.toInt() }
             giveToBot(b, v)
         } else {
             """^bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)$""".toRegex()
-                .find(line)!!.destructured.toList().let {
-                    rules[it[0].toInt()] =
-                        Pair(it[1], it[2].toInt()) to Pair(it[3], it[4].toInt())
-                }
+                .finD(line) { (b, ml, l, mh, h) -> rules[b.toInt()] = Pair(ml, l.toInt()) to Pair(mh, h.toInt()) }
         }
     }
-
     val outputs = mutableMapOf<Int, Int>()
 
-    while (true) {
-        val fullBots = bots.filterValues { it.first != null && it.second != null }
-        if (fullBots.isEmpty()) break
-        fullBots.forEach { (b, v) ->
+    while (bots.isNotEmpty()) {
+        bots.toMutableMap().forEach { (b, v) ->
             val (l, h) = v
-            if (l == 17 && h == 61) {
-                println(b)
-            }
-            val (ruleL, ruleH) = rules[b]!!
+            if (l == null || h == null) return@forEach
+            if (l == 17 && h == 61) println(b)
 
-            if (ruleL.first == "bot") giveToBot(ruleL.second, l!!) else outputs[ruleL.second] = l!!
-            if (ruleH.first == "bot") giveToBot(ruleH.second, h!!) else outputs[ruleH.second] = h!!
-            bots -= b
+            rules[b]!!.toList().zip(listOf(l, h)).forEach { (rule, v) ->
+                if (rule.first == "bot") giveToBot(rule.second, v) else outputs[rule.second] = v
+            }
+            bots.remove(b)
         }
     }
     println(outputs[0]!! * outputs[1]!! * outputs[2]!!)
