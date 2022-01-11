d7c95d44724f79d9854dd3efc1e8ebfb857cc5a3
Michael Böiers <michael.boiers@ingka.com>
2022-01-11 21:03:50 +0100

    refactor

--- python/day01.py
+++ python/day01.py
@@ -1,23 +1,30 @@
 import fileinput
+
 data = list()
 for line in fileinput.input():
     data.append(int(line))
 
-n = 0
-for x in data:
-    n += x
-part1 = n
 
-part2 = None
-H = set()
-n = 0
-while part2 is None:
+def part1():
+    n = 0
     for x in data:
         n += x
-        if H.__contains__(n):
-            part2 = n
-            break
-        H.add(n)
+    return n
 
-print(part1)
-print(part2)
+
+def part2():
+    twice = None
+    h = set()
+    n = 0
+    while twice is None:
+        for x in data:
+            n += x
+            if n in h:
+                twice = n
+                break
+            h.add(n)
+    return twice
+
+
+print(part1())
+print(part2())
