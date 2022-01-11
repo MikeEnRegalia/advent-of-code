4c9cdf35d8e68d88b7608e20d3f18216bd93d17b
Michael Böiers <michael.boiers@ingka.com>
2022-01-11 21:47:37 +0100

    refactor

--- python/day02.py
+++ python/day02.py
@@ -1,28 +1,25 @@
 import fileinput
 
-data = list()
-for line in fileinput.input():
-    data.append(line.removesuffix('\n'))
+data = map(lambda x: x.removesuffix('\n'), fileinput.input())
+
+
+def freq(line):
+    x = dict()
+    for c in line:
+        x[c] = 1 if c not in x else x[c] + 1
+    return set(x.values())
 
 
 def part1():
-    twice = 0
-    thrice = 0
-    for line in data:
-        x = {}
-        for c in line:
-            x[c] = 1 if c not in x else x[c] + 1
-        a = False
-        b = False
-        for c, v in x.items():
-            if v == 2:
-                a = True
-            if v == 3:
-                b = True
-        if a: twice += 1
-        if b: thrice += 1
-    return twice * thrice
+    sum_twice = 0
+    sum_thrice = 0
+    for f in map(lambda x: freq(x), data):
+        if 2 in f:
+            sum_twice += 1
+        if 3 in f:
+            sum_thrice += 1
+
+    return sum_twice * sum_thrice
 
 
 print(part1())
-
