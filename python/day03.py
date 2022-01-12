fe40a1e801884bdfa05493e7207ea2254c75eb66
Michael Böiers <michael.boiers@ingka.com>
2022-01-12 10:45:17 +0100

    refactor

--- python/day03.py
+++ python/day03.py
@@ -1,40 +1,28 @@
 import fileinput
 import itertools
 import re
-from dataclasses import dataclass
 from functools import reduce
 
 
-@dataclass
-class Rect:
-    n: int
-    offset: tuple[int, int]
-    size: tuple[int, int]
-
-    def points(self):
-        return itertools.product(range(self.offset[0], self.offset[0] + self.size[0]),
-                                 range(self.offset[1], self.offset[1] + self.size[1]))
-
-
 def rect(line):
-    g = re.match(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$", line).groups()
-    return Rect(int(g[0]), (int(g[1]), int(g[2])), (int(g[3]), int(g[4])))
+    g = list(map(int, re.match(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$", line).groups()))
+    return g[0], list(itertools.product(range(g[1], g[1] + g[3]), range(g[2], g[2] + g[4])))
 
 
-data = list(map(lambda _: rect(_), fileinput.input()))
+data = list(map(rect, fileinput.input()))
 
 M = dict()
-for r in data:
-    for (x, y) in r.points():
-        M[(x, y)] = M.setdefault((x, y), 0) + 1
+for (n, points) in data:
+    for p in points:
+        M[p] = M.setdefault(p, 0) + 1
 
-part1 = reduce(lambda acc, v: acc + (1 if v >= 2 else 0), M.values(), 0)
+part1 = reduce(lambda acc, v: acc + (1 if v > 1 else 0), M.values(), 0)
 print(part1)
 
-for r in data:
+for (n, points) in data:
     overlaps = False
-    for (x, y) in r.points():
-        if M[(x, y)] > 1:
+    for p in points:
+        if M[p] > 1:
             overlaps = True
     if not overlaps:
-        print(r.n)
+        print(n)
