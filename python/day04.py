3d193fd20d04d7c4797959b14541045f797187ba
Michael Böiers <michael.boiers@ingka.com>
2022-01-12 15:42:49 +0100

    2018 day 4 python part 1

---
+++ python/day04.py
@@ -0,0 +1,34 @@
+import fileinput
+import re
+from collections import Counter
+
+
+def guard(line):
+    return list(re.match(r"^\[\d+-\d+-\d+ (\d+):(\d+)] ((Guard #(\d+) begins shift)|(wakes up)|(falls asleep))$",
+                         line).groups())
+
+
+data = list(map(guard, sorted(fileinput.input())))
+
+guard_number = None
+asleep_since = None
+ASLEEP = dict()
+for g in data:
+    # TODO use match?
+    if g[4] is not None:
+        guard_number = int(g[4])
+        asleep_since = None
+        print(f"{guard_number} begins shift")
+    if g[5] is not None:
+        if asleep_since is not None:
+            for minute in range(asleep_since, int(g[1])):
+                ASLEEP.setdefault(guard_number, list()).append(minute)
+        asleep_since = None
+        print(f"{guard_number} wakes up")
+    if g[6] is not None:
+        asleep_since = int(g[1])
+        print(f"{guard_number} falls asleep")
+
+most_asleep = sorted(ASLEEP.keys(), key=lambda _: len(ASLEEP[_]), reverse=True)[0]
+most_asleep_minute = Counter(ASLEEP[most_asleep]).most_common(1)[0][0]
+print(most_asleep * most_asleep_minute)
