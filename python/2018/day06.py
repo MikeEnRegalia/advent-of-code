abea8026903c90f1fd7d39d14fd1d8bd73b65046
Michael Böiers <michael.boiers@ingka.com>
2022-01-13 13:01:27 +0100

    refactor

--- python/2018/day06.py
+++ python/2018/day06.py
@@ -13,12 +13,11 @@ class Point:
 C = list()
 X = list()
 Y = list()
-for line in fileinput.input():
-    tokens = line.split(",")
-    p = Point(int(tokens[0]), int(tokens[1]))
+for p in [Point(t[0], t[1]) for t in [list(map(int, line.split(","))) for line in fileinput.input()]]:
     C.append(p)
     X.append(p.x)
     Y.append(p.y)
+
 MIN = Point(min(X), min(Y))
 MAX = Point(max(X), max(Y))
 A = list(map(lambda _: Point(_[0], _[1]), itertools.product(range(MIN.x, MAX.x + 1), range(MIN.y, MAX.y + 1))))
@@ -28,29 +27,24 @@ def dist(a, c):
     return abs(c.x - a.x) + abs(c.y - a.y)
 
 
-def part1():
-    points = list()
-    for a in A:
-        best_c = best = None
-        for c in C:
-            d = dist(a, c)
-            if best is None or d < best:
-                best = d
-                infinite = c.x in (MIN.x, MAX.x) or c.y in (MIN.y, MAX.y)
-                best_c = None if infinite else c
-            elif d == best:
-                best_c = None
-        if best_c is not None:
-            points.append(best_c)
-
-    return Counter(points).most_common(1)[0][1]
+def belongs_to(a):
+    best_c = best = None
+    for c in C:
+        d = dist(a, c)
+        if best is None or d < best:
+            best = d
+            infinite = c.x in (MIN.x, MAX.x) or c.y in (MIN.y, MAX.y)
+            best_c = None if infinite else c
+        elif d == best:
+            best_c = None
+    return best_c
 
 
-print(part1())
+print(Counter(list(filter(lambda c: c is not None, [belongs_to(a) for a in A]))).most_common(1)[0][1])
 
 
 def sum_dist(a):
-    return sum(map(lambda c: dist(a, c), C))
+    return sum([dist(a, c) for c in C])
 
 
 print(len(list(filter(lambda s: s < 10000, map(sum_dist, A)))))
