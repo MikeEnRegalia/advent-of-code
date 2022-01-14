0b2cead815492e6384e5738e58806abaa889ef17
Michael Böiers <michael.boiers@ingka.com>
2022-01-14 13:24:44 +0100

    2018 day 24 part 1 still wrong

--- python/2018/day24.py
+++ python/2018/day24.py
@@ -11,7 +11,7 @@ class Group:
     hp_per_unit: int
     weak: list
     immune: list
-    attack: str
+    attack_type: str
     damage: int
     initiative: int
 
@@ -19,74 +19,76 @@ class Group:
         return self.units * self.damage
 
     def damage_to(self, other):
-        if self.attack in other.immune:
+        if self.attack_type in other.immune:
             return 0
-        if self.attack in other.weak:
+        if self.attack_type in other.weak:
             return self.effective_power() * 2
         return self.effective_power()
 
-    def hit_by(self, group):
-        (units_killed, _) = divmod(group.damage_to(self), self.hp_per_unit)
-        self.units = max(0, self.units - units_killed)
+    def attack(self, group):
+        damage_dealt = self.damage_to(group)
+        units_killed = min(group.units, damage_dealt // group.hp_per_unit)
+        group.units -= units_killed
 
 
-PATTERN = r"^(\d+) .* (\d+) hit points (\((weak to ([^;]+)(; )?)?(immune to (.+))?\) )?with .* (\d+) (\w+) .* (\d+)$"
+PATTERN = re.compile(r"^(\d+) .* (\d+) hit points "
+                     r"(\((.*)\) )?"
+                     r"with .* (\d+) (\w+) .* (\d+)$")
 
 
 def parse():
     army = None
     result = list()
-    id = 0
+    gid = 1
     for line in [line.strip() for line in fileinput.input()]:
         if ":" in line:
             army = 'immune system' if line == 'Immune System:' else 'infection'
             continue
         elif line.strip() != '':
-            (size, hp, _, _, weaknesses, _, _, immunities, damage, attack, initiative) = re.match(
-                PATTERN,
-                line).groups()
-            weak = list() if weaknesses is None else weaknesses.split(", ")
-            immune = list() if immunities is None else immunities.split(", ")
-            result.append(Group(id, army, int(size), int(hp), weak, immune, attack, int(damage), int(initiative)))
-            id += 1
+            (size, hp, _, w_i_r, damage, attack, initiative) = re.match(PATTERN, line).groups()
+            weak = immune = list()
+            for foo in w_i_r.split("; ") if w_i_r is not None else list():
+                if foo.startswith("weak to "):
+                    weak = foo[len("weak to "):].split(", ")
+                if foo.startswith("immune to "):
+                    immune = foo[len("immune to "):].split(", ")
+            result.append(Group(gid, army, int(size), int(hp), weak, immune, attack, int(damage), int(initiative)))
+            gid += 1
     return result
 
 
-P = parse()
-
-
-def highest_initiative_groups():
-    return sorted(P, key=lambda g: g.initiative, reverse=True)
-
-
-def target_selection_groups():
-    return sorted(highest_initiative_groups(), key=lambda g: g.effective_power(), reverse=True)
-
-
-def select_target(group, already_attacked):
-    targets = sorted([p for p in highest_initiative_groups() if p not in already_attacked and p.army != group.army],
-                     key=lambda g: group.damage_to(g), reverse=True)
-    return None if len(targets) == 0 else targets[0]
+G = parse()
 
 
 def fight():
-    while True:
-        units_before = sum([g.units for g in P])
+    def select_target(attacker, already_selected):
+        targets = sorted([target for target in G if target not in already_selected
+                          and target.army != attacker.army],
+                         key=lambda g: (-attacker.damage_to(g), -g.effective_power(), -g.initiative))
+        r = None if len(targets) == 0 else targets[0]
+        return r
 
-        attacks = dict()
-        for group in [g for g in target_selection_groups() if g.units > 0]:
-            target = select_target(group, attacks.values())
-            attacks[group.id] = target
+    def turns():
+        while True:
+            units_before = sum([g.units for g in G])
 
-        for group in [g for g in highest_initiative_groups() if g.units > 0]:
-            target = attacks[group.id]
-            if target is not None:
-                target.hit_by(group)
+            targets = dict()
+            for attacker in sorted(G, key=lambda g: (-g.effective_power(), -g.initiative)):
+                target = select_target(attacker, targets.values())
+                targets[attacker.id] = target
 
-        units_after = sum([g.units for g in P])
-        if units_before == units_after:
-            print(units_after)
-            break
+            for attacker in sorted(G, key=lambda g: -g.initiative):
+                target = targets[attacker.id]
+                if target is not None:
+                    attacker.attack(target)
+                    if target.units == 0:
+                        G.remove(target)
+
+            units_after = sum([g.units for g in G])
+            if units_before == units_after:
+                return
+    turns()
 
 
 fight()
+print(sum([g.units for g in G]))
