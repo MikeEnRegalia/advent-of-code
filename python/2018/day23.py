572e0ada0b3e0c279715ff7f3721353ee701fe9c
Michael Böiers <michael.boiers@ingka.com>
2022-01-17 11:27:46 +0100

    2018 day 23 (python)

--- python/2018/day23.py
+++ python/2018/day23.py
@@ -1,8 +1,8 @@
 import fileinput
+
 import re
 
-lines = list(fileinput.input())
-parsed = [re.match(r"pos=<([-]?\d+),([-]?\d+),([-]?\d+)>, r=(\d+)", line).groups() for line in lines]
+parsed = [re.match(r"pos=<([-]?\d+),([-]?\d+),([-]?\d+)>, r=(\d+)", line).groups() for line in fileinput.input()]
 bots = [list(map(int, g)) for g in parsed]
 
 
@@ -19,3 +19,61 @@ def part1():
 
 
 part1()
+
+bots_x = [b[0] for b in bots]
+bots_y = [b[1] for b in bots]
+bots_z = [b[2] for b in bots]
+
+
+def part2():
+    r = dict()
+    cache = set()
+
+    def drill(precision,
+              min_x=min(bots_x), max_x=max(bots_x),
+              min_y=min(bots_y), max_y=max(bots_y),
+              min_z=min(bots_z), max_z=max(bots_z)):
+        max_n = None
+        points = set()
+        for x in range(min_x // precision, max_x // precision + 1):
+            for y in range(min_y // precision, max_y // precision + 1):
+                for z in range(min_z // precision, max_z // precision + 1):
+                    n = 0
+                    for (bx, by, bz, br) in bots:
+                        d = abs(bx / precision - x) + abs(by / precision - y) + abs(bz / precision - z)
+                        if d <= (round((br / precision) + 1) if precision > 1 else br):
+                            n += 1
+                    if max_n == n:
+                        points.add((x, y, z))
+                    elif n > 0 and (max_n is None or max_n < n):
+                        max_n = n
+                        points.clear()
+                        points.add((x, y, z))
+
+        if precision == 1:
+            s = r.setdefault(max_n, set())
+            for p in points:
+                s.add(p)
+            return
+
+        for (x, y, z) in sorted(points, key=lambda m: abs(m[0]) + abs(m[1]) + abs(m[2])):
+            k = (precision, x, y, z)
+            if k not in cache:
+                cache.add(k)
+                (px2, py2, pz2) = ((x - 1) * precision, (y - 1) * precision, (z - 1) * precision)
+                (px3, py3, pz3) = ((x + 1) * precision, (y + 1) * precision, (z + 1) * precision)
+                drill(precision // 2, min_x=px2, max_x=px3, min_y=py2, max_y=py3, min_z=pz2, max_z=pz3)
+
+    seq = list()
+    seq.append(1)
+    while seq[len(seq) - 1] < min(max(bots_x), max(bots_y), max(bots_z)):
+        seq.append(seq[len(seq) - 1] * 2)
+
+    drill(seq[len(seq) - 1])
+
+    total_max_n = max(r.keys())
+    closest_points = sorted([abs(m[0]) + abs(m[1]) + abs(m[2]) for m in r[total_max_n]])
+    print(closest_points[0])
+
+
+part2()
