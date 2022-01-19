b2c05067a30ad3194471ea4180239ee2d4efa59c
Michael Böiers <michael.boiers@ingka.com>
2022-01-19 14:48:29 +0100

    2019 day 17 part 2 80%

--- python/2019/day17.py
+++ python/2019/day17.py
@@ -57,12 +57,18 @@ def run(state, i):
 ASCII = {int(q): int(n) for (q, n) in enumerate(fileinput.input().readline().split(","))}
 (_, o) = run((ASCII, 0, 0), None)
 grid = "".join([chr(i) for i in o]).strip().split("\n")
+pos = (0, 0)
+facing = '^'
 
 
 def part1():
+    global pos, facing
     r = 0
     for y in range(1, len(grid) - 1):
         for x in range(1, len(grid[y]) - 1):
+            if grid[y][x] in '<>v^':
+                pos = (x, y)
+                facing = grid[y][x]
             if grid[y][x] in '.X':
                 continue
             if grid[y - 1][x] in '.X' or grid[y + 1][x] in '.X':
@@ -74,3 +80,104 @@ def part1():
 
 
 part1()
+
+
+def part2():
+    global pos, facing
+    commands = list()
+
+    def turn_left():
+        global facing
+        dirs = "<v>^"
+        facing = dirs[(dirs.index(facing) + 1) % len(dirs)]
+        commands.append("L")
+
+    def turn_right():
+        global facing
+        dirs = ">v<^"
+        facing = dirs[(dirs.index(facing) + 1) % len(dirs)]
+        commands.append("R")
+
+    def at(x, y):
+        if x not in range(0, len(grid[0])):
+            return None
+        if y not in range(0, len(grid)):
+            return None
+        return grid[y][x]
+
+    def can_forward():
+        global pos
+        (x, y) = pos
+        if facing == '^':
+            return at(x, y - 1) == '#'
+        if facing == 'v':
+            return at(x, y + 1) == '#'
+        if facing == '<':
+            return at(x - 1, y) == '#'
+        if facing == '>':
+            return at(x + 1, y) == '#'
+        raise AssertionError
+
+    def can_left():
+        global pos
+        (x, y) = pos
+        if facing == '^':
+            return at(x - 1, y) == '#'
+        if facing == 'v':
+            return at(x + 1, y) == '#'
+        if facing == '<':
+            return at(x, y + 1) == '#'
+        if facing == '>':
+            return at(x, y - 1) == '#'
+        raise AssertionError
+
+    def can_right():
+        global pos
+        (x, y) = pos
+        if facing == '^':
+            return at(x + 1, y) == '#'
+        if facing == 'v':
+            return at(x - 1, y) == '#'
+        if facing == '<':
+            return at(x, y - 1) == '#'
+        if facing == '>':
+            return at(x, y + 1) == '#'
+        raise AssertionError
+
+    def forward():
+        global pos
+        (x, y) = pos
+        if facing == '^':
+            pos = (x, y - 1)
+        if facing == 'v':
+            pos = (x, y + 1)
+        if facing == '<':
+            pos = (x - 1, y)
+        if facing == '>':
+            pos = (x + 1, y)
+        if len(commands) == 0 or commands[-1] in ['L', 'R']:
+            commands.append(1)
+        else:
+            commands[-1] += 1
+
+    def print_grid():
+        for y in range(0, len(grid)):
+            for x in range(0, len(grid[0])):
+                print(facing if (x, y) == pos else grid[y][x], end='')
+            print()
+
+    while True:
+        if can_forward():
+            forward()
+        elif can_left():
+            turn_left()
+        elif can_right():
+            turn_right()
+        else:
+            break
+
+    print(len(commands))
+
+
+
+part2()
