4dd7785937e916440c97e0835c38205128391d74
Michael Böiers <michael.boiers@ingka.com>
2022-01-19 13:48:58 +0100

    refactor

--- python/2019/day17.py
+++ python/2019/day17.py
@@ -57,17 +57,20 @@ def run(state, i):
 ASCII = {int(q): int(n) for (q, n) in enumerate(fileinput.input().readline().split(","))}
 (_, o) = run((ASCII, 0, 0), None)
 grid = "".join([chr(i) for i in o]).strip().split("\n")
-for r in grid:
-    print(len(r))
 
-part1 = 0
-for y in range(1, len(grid) - 1):
-    for x in range(1, len(grid[y]) - 1):
-        if grid[y][x] in '.X':
-            continue
-        if grid[y - 1][x] in '.X' or grid[y + 1][x] in '.X':
-            continue
-        if grid[y][x - 1] in '.X' or  grid[y][x + 1] in '.X':
-            continue
-        part1 += x * y
-print(part1)
+
+def part1():
+    r = 0
+    for y in range(1, len(grid) - 1):
+        for x in range(1, len(grid[y]) - 1):
+            if grid[y][x] in '.X':
+                continue
+            if grid[y - 1][x] in '.X' or grid[y + 1][x] in '.X':
+                continue
+            if grid[y][x - 1] in '.X' or grid[y][x + 1] in '.X':
+                continue
+            r += x * y
+    print(r)
+
+
+part1()
