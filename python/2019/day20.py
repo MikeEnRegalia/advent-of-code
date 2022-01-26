e71ea1adb9ef5505cbceb2e64740a931efb2b627
Michael Böiers <michael.boiers@ingka.com>
2022-01-26 11:19:02 +0100

    2019 day 20 init

--- python/2019/day20.py
+++ python/2019/day20.py
@@ -1,18 +1,14 @@
 import fileinput
 import string
 
-MAZE = dict()
-for (y, line) in enumerate(fileinput.input()):
-    for (x, c) in enumerate(line):
-        if c not in " \n":
-            MAZE[(x, y)] = c
 
-print(MAZE)
-
-MAZE_MIN_X = min([k[0] for (k, v) in MAZE.items() if v == '#'])
-MAZE_MAX_X = max([k[0] for (k, v) in MAZE.items() if v == '#'])
-MAZE_MIN_Y = min([k[1] for (k, v) in MAZE.items() if v == '#'])
-MAZE_MAX_Y = max([k[1] for (k, v) in MAZE.items() if v == '#'])
+def maze():
+    r = dict()
+    for (y, line) in enumerate(fileinput.input()):
+        for (x, c) in enumerate(line):
+            if c not in " \n":
+                r[(x, y)] = c
+    return r
 
 
 def neighbors(p):
@@ -28,13 +24,29 @@ def letter_neighbors(p):
     return [(p, c) for (p, c) in neighbors(p) if c in string.ascii_uppercase]
 
 
-PORTALS = set()
-for (x, y) in MAZE.keys():
-    if x in [MAZE_MIN_X, MAZE_MAX_X] or y in [MAZE_MIN_Y, MAZE_MAX_Y]:
-        if MAZE[(x, y)] == '.':
-            (p, c1) = letter_neighbors((x, y))[0]
-            (p2, c2) = letter_neighbors(p)[0]
-            PORTALS.add(c1 + c2)
-            # follow (x, y) to find connected portals
+def follow(p):
+    v = set()
+    q = set()
+    d = {p: 0}
 
+
+def portals():
+    r = set()
+    for (x, y) in MAZE.keys():
+        if x in [MAZE_MIN_X, MAZE_MAX_X] or y in [MAZE_MIN_Y, MAZE_MAX_Y]:
+            if MAZE[(x, y)] == '.':
+                (p, c1) = letter_neighbors((x, y))[0]
+                (p2, c2) = letter_neighbors(p)[0]
+                r.add(c1 + c2)
+                # follow (x, y) to find connected portals
+    return r
+
+
+MAZE = maze()
+MAZE_MIN_X = min([k[0] for (k, v) in MAZE.items() if v == '#'])
+MAZE_MAX_X = max([k[0] for (k, v) in MAZE.items() if v == '#'])
+MAZE_MIN_Y = min([k[1] for (k, v) in MAZE.items() if v == '#'])
+MAZE_MAX_Y = max([k[1] for (k, v) in MAZE.items() if v == '#'])
+
+PORTALS = portals()
 print(PORTALS)
