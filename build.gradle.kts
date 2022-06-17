38481e57b05491c56b2b58846bd581d5d037b0b9
Michael Böiers <mikeenregalia@gmail.com>
2022-06-17 21:29:21 +0200

    some more fixes to the configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.google.code.gson:gson:2.9.0")
     testImplementation("io.kotest:kotest-assertions-core:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime")
 }
 
