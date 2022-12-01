1d30a00a66a752cb13bf639be25435ddabf4bace
Michael Böiers <mikeenregalia@gmail.com>
2022-12-01 09:33:40 +0100

    refactor

--- src/main/kotlin/aoc2022/day01.kt
+++ src/main/kotlin/aoc2022/day01.kt
@@ -1,9 +1,8 @@
 package aoc2022
 
-fun main() = day01(String(System.`in`.readAllBytes())).toList().forEach(::println)
+fun main() = day01(String(System.`in`.readAllBytes())).forEach(::println)
 
 fun day01(input: String) = input.split("\n\n")
-    .map { it.split("\n").map(String::toInt).sum() }
+    .map { it.split("\n").sumOf(String::toInt) }
     .sortedDescending()
-    .let { it.first() to it.take(3).sum() }
-
+    .let { listOf(it.first(), it.take(3).sum()) }
