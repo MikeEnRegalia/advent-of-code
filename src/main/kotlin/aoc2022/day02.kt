53472a5621095fd262f1de41fbe0622fea43e269
Michael Böiers <mikeenregalia@gmail.com>
2022-12-01 23:24:25 +0100

    prepare day 2

---
+++ src/main/kotlin/aoc2022/day02.kt
@@ -0,0 +1,8 @@
+package aoc2022
+
+fun main() = day02(String(System.`in`.readAllBytes())).forEach(::println)
+
+fun day02(input: String): List<Any?> {
+    return listOf(null, null)
+}
+
---
+++ src/test/kotlin/aoc2022/Day02KtTest.kt
@@ -0,0 +1,11 @@
+package aoc2022
+
+import org.junit.jupiter.api.Test
+
+class Day02KtTest {
+    @Test
+    fun test() {
+        val input = """"""
+        day02(input).forEach(::println)
+    }
+}
