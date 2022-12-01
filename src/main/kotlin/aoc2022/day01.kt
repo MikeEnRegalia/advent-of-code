bde2fda913b59cbc60d38870810a958109b63e17
Michael Böiers <mikeenregalia@gmail.com>
2022-12-01 08:44:06 +0100

    refactor

--- src/main/kotlin/aoc2022/day01.kt
+++ src/main/kotlin/aoc2022/day01.kt
@@ -1,8 +1,9 @@
 package aoc2022
 
-fun main() = day01(generateSequence(::readlnOrNull).toList()).toList().forEach(::println)
+fun main() = day01(String(System.`in`.readAllBytes())).toList().forEach(::println)
 
-fun day01(lines: List<String>): Pair<String?, String?> {
-    return null to null
-}
+fun day01(input: String) = input.split("\n\n")
+    .map { it.split("\n").map(String::toInt).sum() }
+    .sortedDescending()
+    .let { it.first() to it.take(3).sum() }
 
--- src/test/kotlin/aoc2022/day01.kt
+++ src/test/kotlin/aoc2022/day01.kt
@@ -3,29 +3,26 @@ package aoc2022
 import org.apache.commons.io.IOUtils
 import org.junit.jupiter.api.Test
 import strikt.api.expect
-import strikt.assertions.isNull
 
 class Day01Test {
 
     @Test
     fun `actual input`() {
-        val input = String(IOUtils.resourceToByteArray("/challenges/aoc2022-day01.txt")).split("\n")
+        val input = String(IOUtils.resourceToByteArray("/challenges/aoc2022-day01.txt"))
         test(input)
     }
 
     @Test
     fun `test input`() {
-        val input = listOf("")
+        val input = ""
         test(input)
     }
 
-    private fun test(input: List<String>) {
+    private fun test(input: String) {
         val (part1, part2) = day01(input)
         println("part1: $part1")
         println("part2: $part2")
         expect {
-            that(part1).isNull()
-            that(part2).isNull()
         }
     }
 }
