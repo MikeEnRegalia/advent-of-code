c634993babed349103928781b615195a830bc5bb
Michael Böiers <mikeenregalia@gmail.com>
2022-12-01 20:42:56 +0100

    add another efficient solution

--- src/main/kotlin/aoc2022/day01.kt
+++ src/main/kotlin/aoc2022/day01.kt
@@ -1,8 +1,24 @@
 package aoc2022
 
-fun main() = day01(String(System.`in`.readAllBytes())).forEach(::println)
+import java.util.*
 
-fun day01(input: String) = input.split("\n\n")
-    .map { it.split("\n").sumOf(String::toInt) }
-    .sortedDescending()
-    .let { listOf(it.first(), it.take(3).sum()) }
+fun main() = day01Fold(String(System.`in`.readAllBytes())).forEach(::println)
+
+fun day01(input: String) = input.split("\n\n").map { it.lines().sumOf(String::toInt) }.collectAnswers()
+private fun List<Int>.collectAnswers() = sortedDescending().run { listOf(first(), take(3).sum()) }
+
+fun day01Fold(input: String) = input.split("\n").fold(mutableListOf(0), ::addLine).collectAnswers()
+
+private fun addLine(acc: MutableList<Int>, line: String) = acc.apply {
+    if (line.isBlank()) this += 0 else this[lastIndex] = this[lastIndex] + line.toInt()
+}
+
+fun day01SortedSet(input: String): List<Int> {
+    class Food(val calories: Int)
+    val top = TreeSet(compareByDescending(Food::calories))
+    for (elf in input.split("\n\n")) {
+        top += Food(elf.lines().sumOf(String::toInt))
+        if (top.size > 3) top -= top.last()
+    }
+    return listOf(top.first().calories, top.sumOf(Food::calories))
+}
