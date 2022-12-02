c18f1a40b2611d9e622d8793faf0a807864719e0
Michael Böiers <mikeenregalia@gmail.com>
2022-12-02 13:35:31 +0100

    refactor

--- src/main/kotlin/aoc2022/day02.kt
+++ src/main/kotlin/aoc2022/day02.kt
@@ -2,55 +2,43 @@ package aoc2022
 
 import aoc2022.RPSMove.*
 import aoc2022.RPSResult.*
+import aoc2022.RPSMove.values as RPSMoves
+import aoc2022.RPSResult.values as RPSResults
 
 fun main() = day02Clean(String(System.`in`.readAllBytes())).forEach(::println)
 
 fun day02Clean(input: String) = with(input.lines().map { it.split(" ") }) {
+    fun playPart1(line: List<String>): Int {
+        val (opponentMove, myMove) = line.map { token -> RPSMoves().single { token in it.tokens } }
+        val result = myMove.playAgainst(opponentMove)
+        return myMove.score + result.score
+    }
+
+    fun playPart2(line: List<String>): Int {
+        val opponentMove = RPSMoves().single { line[0] in it.tokens }
+        val desiredResult = RPSResults().single { it.token == line[1] }
+        val myMove = opponentMove.whatToPlayToAchieve(desiredResult)
+        return myMove.score + desiredResult.score
+    }
     listOf(sumOf(::playPart1), sumOf(::playPart2))
 }
 
-private fun playPart1(line: List<String>): Int {
-    val (opponentMove, myMove) = line.map { it.toRPSMove() }
-    val result = myMove.playAgainst(opponentMove)
-    return myMove.score + result.score
-}
+private enum class RPSResult(val score: Int, val token: String) { Lose(0, "X"), Draw(3, "Y"), Win(6, "Z") }
 
-private fun playPart2(line: List<String>): Int {
-    val opponentMove = line[0].toRPSMove()
-    val desiredResult = line[1].toRPSResult()
-    val myMove = opponentMove.whatToPlayToAchieve(desiredResult)
-    return myMove.score + desiredResult.score
-}
+private enum class RPSMove(val score: Int, vararg val tokens: String) {
+    Rock(1, "A", "X"), Paper(2, "B", "Y"), Scissors(3, "C", "Z");
 
-private enum class RPSMove(val score: Int) { Rock(1), Paper(2), Scissors(3) }
+    fun playAgainst(move: RPSMove): RPSResult = when {
+        this == beats(move) -> Lose
+        this == move -> Draw
+        else -> Win
+    }
 
-private fun beats(move: RPSMove) = when (move) {
-    Paper -> Rock
-    Rock -> Scissors
-    Scissors -> Paper
-}
+    private fun beats(move: RPSMove) = when (move) {
+        Paper -> Rock
+        Rock -> Scissors
+        Scissors -> Paper
+    }
 
-private fun RPSMove.playAgainst(move: RPSMove): RPSResult = when {
-    this == beats(move) -> Lose
-    this == move -> Draw
-    else -> Win
-}
-
-private fun RPSMove.whatToPlayToAchieve(result: RPSResult) =
-    RPSMove.values().first { it.playAgainst(this) == result }
-
-private fun String.toRPSMove() = when (this) {
-    "A", "X" -> Rock
-    "B", "Y" -> Paper
-    "C", "Z" -> Scissors
-    else -> throw IllegalArgumentException(this)
-}
-
-private enum class RPSResult(val score: Int) { Win(6), Lose(0), Draw(3) }
-
-private fun String.toRPSResult() = when (this) {
-    "X" -> Lose
-    "Y" -> Draw
-    "Z" -> Win
-    else -> throw IllegalArgumentException(this)
+    fun whatToPlayToAchieve(result: RPSResult) = RPSMove.values().first { it.playAgainst(this) == result }
 }
