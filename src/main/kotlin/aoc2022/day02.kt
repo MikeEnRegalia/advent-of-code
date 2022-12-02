e21c51e13a4e00a346f746b02ca3191339de3bac
Michael Böiers <mikeenregalia@gmail.com>
2022-12-02 06:42:57 +0100

    refactor

--- src/main/kotlin/aoc2022/day02.kt
+++ src/main/kotlin/aoc2022/day02.kt
@@ -3,44 +3,41 @@ package aoc2022
 fun main() = day02(String(System.`in`.readAllBytes())).forEach(::println)
 
 fun day02(input: String): List<Any?> {
-    val R = "A"
-    val P = "B"
-    val S = "C"
-    val R1 = "X"
-    val P1 = "Y"
-    val S1 = "Z"
+    fun String.toRPS() = when (this) {
+        "A", "X" -> "R"
+        "B", "Y" -> "P"
+        "C", "Z" -> "S"
+        else -> throw IllegalArgumentException(this)
+    }
+
     val score = input.lines().map { it.split(" ") }.fold(0 to 0) { (part1, part2), (opp, you) ->
-        val roundPart1 = when (you) {
-            R1 -> 1
-            P1 -> 2
-            S1 -> 3
+        val roundPart1 = when (you.toRPS()) {
+            "R" -> 1
+            "P" -> 2
+            "S" -> 3
             else -> throw IllegalArgumentException(you)
-        } + when (you to opp) {
-            R1 to S -> 6
-            S1 to P -> 6
-            P1 to R -> 6
-            R1 to R -> 3
-            S1 to S -> 3
-            P1 to P -> 3
-            R1 to P -> 0
-            S1 to R -> 0
-            P1 to S -> 0
-            else -> 0
+        } + when (you.toRPS() to opp.toRPS()) {
+            "R" to "S", "S" to "P", "P" to "R" -> 6
+            "R" to "R", "S" to "S", "P" to "P" -> 3
+            "R" to "P", "S" to "R", "P" to "S" -> 0
+            else -> throw IllegalArgumentException(you)
         }
         val roundPart2 = when (you) {
-            "X" -> when (opp) {
-                R -> 3
-                S -> 2
+            "X" -> when (opp.toRPS()) {
+                "R" -> 3
+                "S" -> 2
                 else -> 1
             }
-            "Y" -> when (opp) {
-                R -> 1 + 3
-                P -> 2 + 3
+
+            "Y" -> when (opp.toRPS()) {
+                "R" -> 1 + 3
+                "P" -> 2 + 3
                 else -> 3 + 3
             }
-            else -> when (opp) {
-                R -> 2 + 6
-                S -> 1 + 6
+
+            else -> when (opp.toRPS()) {
+                "R" -> 2 + 6
+                "S" -> 1 + 6
                 else -> 3 + 6
             }
         }
