f3f4925ee541102caa46bee11b0e8b33d4a27abc
Michael Böiers <mikeenregalia@gmail.com>
2022-12-03 14:22:11 +0100

    refactor

--- src/main/kotlin/aoc2022/day03.kt
+++ src/main/kotlin/aoc2022/day03.kt
@@ -2,17 +2,12 @@ package aoc2022
 
 fun main() = day03(String(System.`in`.readAllBytes())).forEach(::println)
 
-fun day03(input: String): List<Any?> {
-    fun Char.score() = this - if (isLowerCase()) 'a' - 1 else 'A' - 27
-
-    return with(input.lines()) {
-        listOf(
-            sumOf { line ->
-                line.toSet().filter { c -> line.chunked(line.length / 2).all { c in it } }.sumOf(Char::score)
-            },
-            chunked(3).sumOf { group ->
-                group.flatMapTo(mutableSetOf(), String::toSet).single { c -> group.all { c in it } }.score()
-            }
-        )
-    }
+fun day03(input: String) = with(input.lines()) {
+    listOf(
+        sumOf { l -> l.toSet().filter { l.chunked(l.length / 2).contain(it) }.sumOf(Char::score) },
+        chunked(3).sumOf { g -> g.flatMapTo(mutableSetOf(), String::toSet).single(g::contain).score() }
+    )
 }
+
+private fun Char.score() = this - if (isLowerCase()) 'a' - 1 else 'A' - 27
+private fun List<CharSequence>.contain(c: Char) = all { c in it }
