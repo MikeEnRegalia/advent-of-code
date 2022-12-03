d02b412419f057400bf790d7e111b5e12135d2c4
Michael Böiers <mikeenregalia@gmail.com>
2022-12-03 14:11:54 +0100

    refactor

--- src/main/kotlin/aoc2022/day03.kt
+++ src/main/kotlin/aoc2022/day03.kt
@@ -5,14 +5,14 @@ fun main() = day03(String(System.`in`.readAllBytes())).forEach(::println)
 fun day03(input: String): List<Any?> {
     fun Char.score() = this - if (isLowerCase()) 'a' - 1 else 'A' - 27
 
-    with(input.lines()) {
-        val part1 = sumOf { line ->
-            val (a, b) = line.chunked(line.length / 2)
-            line.toSet().filter { it in a && it in b }.sumOf(Char::score)
-        }
-        val part2 = chunked(3).sumOf { group ->
-            group.flatMapTo(mutableSetOf(), String::toList).single { c -> group.all { c in it } }.score()
-        }
-        return listOf(part1, part2)
+    return with(input.lines()) {
+        listOf(
+            sumOf { line ->
+                line.toSet().filter { c -> line.chunked(line.length / 2).all { c in it } }.sumOf(Char::score)
+            },
+            chunked(3).sumOf { group ->
+                group.flatMapTo(mutableSetOf(), String::toSet).single { c -> group.all { c in it } }.score()
+            }
+        )
     }
 }
