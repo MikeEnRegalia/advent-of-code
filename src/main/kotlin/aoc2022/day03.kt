aca7c6462ceda4ed4b658903e7e1cce0899fd7a0
Michael Böiers <mikeenregalia@gmail.com>
2022-12-03 06:51:26 +0100

    day 3

--- src/main/kotlin/aoc2022/day03.kt
+++ src/main/kotlin/aoc2022/day03.kt
@@ -3,5 +3,21 @@ package aoc2022
 fun main() = day03(String(System.`in`.readAllBytes())).forEach(::println)
 
 fun day03(input: String): List<Any?> {
-    return listOf(null, null)
+    fun Char.score() = when (this) {
+        in 'a'..'z' -> this - 'a' + 1
+        else -> this - 'A' + 27
+    }
+
+    val part1 = input.lines().fold<String, MutableList<Char>>(mutableListOf()) { acc, line ->
+        val (a, b) = line.chunked(line.length / 2)
+        acc.apply {
+            acc.addAll(line.toSet().filter { it in a && it in b })
+        }
+    }.sumOf { it.score() }
+
+    val part2 = input.lines().chunked(3).sumOf { group ->
+        val inAll = group.flatMap { it.toList() }.toSet().filter { c -> group.all { c in it } }
+        inAll.single().score()
+    }
+    return listOf(part1, part2)
 }
