6d60b48b4b0ae93bd0820a95739823d4627d0a4d
Michael Böiers <mikeenregalia@gmail.com>
2022-12-04 07:07:09 +0100

    day 4

--- src/main/kotlin/aoc2022/day04.kt
+++ src/main/kotlin/aoc2022/day04.kt
@@ -2,6 +2,14 @@ package aoc2022
 
 fun main() = day04(String(System.`in`.readAllBytes())).forEach(::println)
 
-fun day04(input: String): List<Any?> {
-    return listOf(null, null)
-}
+fun day04(input: String) = input.lines()
+    .map { line ->
+        line.split(",").map { elf ->
+            elf.split("-").map(String::toInt).let { it[0]..it[1] }
+        }
+    }
+    .run {
+        listOf(
+            count { (a, b) -> a.all { it in b } || b.all { it in a } },
+            count { (elf1, elf2) -> elf1.any { it in elf2 } || elf2.any { it in elf1 } })
+    }
