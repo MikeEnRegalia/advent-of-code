5191433a7054f805be35f4ae978029d06b6e8207
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 21:07:11 +0100

    enhance template

--- src/main/kotlin/aoc2022/day00.kt
+++ src/main/kotlin/aoc2022/day00.kt
@@ -1,33 +1,17 @@
 package aoc2022
 
-import kotlin.math.min
+import com.google.gson.Gson
+import java.security.MessageDigest
 
 fun main() = day00(String(System.`in`.readAllBytes())).forEach(::println)
 
 fun day00(input: String): List<Any?> {
-    fun String.parseLine() = this
-    val lines = input.lines().map(String::parseLine)
 
     return listOf(null, null)
 }
 
-private fun <S> dijkstra(start: S, neighbors: (S) -> Iterable<S>): Map<S, Int> {
-    var s = start
-    val v = mutableSetOf<S>()
-    val u = mutableSetOf<S>()
-    val d = mutableMapOf(s to 0)
-    while (true) {
-        neighbors(s).filter { it !in v }.forEach { n ->
-            u += n
-            val distance = d.getValue(s) + 1
-            d.compute(n) { _, old -> min(distance, old ?: Int.MAX_VALUE) }
-        }
-        v += s
-        u -= s
-        s = u.randomOrNull() ?: break
-    }
-    return d
-}
-
-// TODO Json parsing
-// TODO MD5
+private fun String.fullyMatch(regex: Regex) = regex.matchEntire(this)!!.destructured.toList()
+private fun String.toJsonList() = Gson().fromJson(this, ArrayList::class.java)
+private fun String.md5() = toByteArray().md5().joinToString("") { it.toUByte().toHex() }
+private fun ByteArray.md5() = MessageDigest.getInstance("MD5").digest(this)
+private fun UByte.toHex() = toString(16).padStart(2, '0')
