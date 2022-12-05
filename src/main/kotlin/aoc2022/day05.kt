ecc9d6e9f42cddd10d363f395af21f93aa8a1b82
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 13:50:03 +0100

    refactor

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -5,22 +5,18 @@ fun main() = day05(String(System.`in`.readAllBytes())).forEach(::println)
 fun day05(input: String): List<Any?> {
     val (stacksInput, commandsInput) = input.split("\n\n").map { it.split("\n") }
 
-    val size = stacksInput.last().split(" ").mapNotNull(String::toIntOrNull).last()
-    val stacksContents = with(stacksInput) { take(size - 1).reversed() }
-    val initialStacks = List(size) { stack ->
-        buildList {
-            for (line in stacksContents) line.getOrNull(1 + (stack * 4))?.takeIf { it != ' ' }?.let { add(it) }
-        }
+    val size = stacksInput.last().trim().substringAfterLast(' ').toInt()
+    val initialStacks = with(stacksInput.reversed().drop(1)) {
+        List(size) { i -> mapNotNull { it.getOrNull(1 + (i * 4))?.takeUnless(Char::isWhitespace) } }
     }
-
     val commands = commandsInput.map {
         it.split(" ").mapNotNull(String::toIntOrNull).mapIndexed { i, x -> if (i == 0) x else x - 1 }
     }
 
     fun compute(mod: ArrayDeque<Char>.(Char) -> Unit): String {
-        val stacks = List(initialStacks.size) { initialStacks[it].toMutableList() }
+        val stacks = initialStacks.map { it.toMutableList() }
         for ((n, from, to) in commands)
-            stacks[to] += ArrayDeque<Char>().apply { repeat(n) { mod(stacks[from].removeLast()) } }
+            stacks[to] += (1..n).fold(ArrayDeque()) { d, _ -> d.apply { mod(stacks[from].removeLast()) } }
         return stacks.joinToString("") { it.last().toString() }
     }
 
