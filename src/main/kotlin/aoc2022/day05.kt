03a77b9c8d2008f3a6aeae859512d6ec10a708dc
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 06:31:26 +0100

    day 5

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -39,21 +39,3 @@ private fun loadStacks(stacksInput: List<String>): MutableList<MutableList<Char>
     }
     return stacks
 }
-
-private fun <S> dijkstra(start: S, neighbors: (S) -> Iterable<S>): Map<S, Int> {
-    var s = start
-    val v = mutableSetOf<S>()
-    val u = mutableSetOf<S>()
-    val d = mutableMapOf(s to 0)
-    while (true) {
-        neighbors(s).filter { it !in v }.forEach { n ->
-            u += n
-            val distance = d.getValue(s) + 1
-            d.compute(n) { _, old -> min(distance, old ?: Int.MAX_VALUE) }
-        }
-        v += s
-        u -= s
-        s = u.randomOrNull() ?: break
-    }
-    return d
-}
