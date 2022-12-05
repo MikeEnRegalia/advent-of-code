4ccb1b575cb0177c62a6b7f882f7dd969a818354
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 09:01:09 +0100

    refactor

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -5,30 +5,25 @@ fun main() = day05(String(System.`in`.readAllBytes())).forEach(::println)
 fun day05(input: String): List<Any?> {
     val (stacksInput, commandsInput) = input.split("\n\n").map { it.split("\n") }
 
-    fun loadStacks(stacksInput: List<String>): MutableList<MutableList<Char>> {
-        val stacks = MutableList((stacksInput.last().length + 2) / 4) { mutableListOf<Char>() }
-        stacksInput.take(stacksInput.size - 1).reversed().forEach { line ->
-            val chunked = line.chunked(4).map(String::trim)
-            chunked.forEachIndexed { i, token ->
-                if (token.isNotBlank()) stacks[i] += token[1]
-            }
-        }
-        return stacks
-    }
-
-    fun compute(part1: Boolean = false) = loadStacks(stacksInput).also { stacks ->
-        for (line in commandsInput) {
-            val (n, from, to) = line.split(" ").mapNotNull(String::toIntOrNull)
-            if (part1) {
-                repeat(n) { stacks[to - 1] += stacks[from - 1].removeLast() }
-            } else {
-                val newCrates = mutableListOf<Char>()
-                repeat(n) { newCrates += stacks[from - 1].removeLast() }
-                stacks[to - 1] += newCrates.reversed()
+    val size = stacksInput.last().split(" ").mapNotNull(String::toIntOrNull).last()
+    val stacksContents = with(stacksInput) { take(size - 1).reversed() }
+    val initialStacks = List(size) { stack ->
+        buildList {
+            for (line in stacksContents) {
+                line.getOrNull(1 + (stack * 4))?.takeIf { it != ' ' }?.let { add(it) }
             }
         }
     }
 
-    return listOf(compute(part1 = true), compute(part1 = false))
-        .map { it.joinToString("") { it.last().toString() } }
+    fun compute(mod: List<Char>.() -> List<Char> = { this }) =
+        List(initialStacks.size) { initialStacks[it].toMutableList() }.let { stacks ->
+            for (line in commandsInput) {
+                val (n, from, to) = line.split(" ").mapNotNull(String::toIntOrNull)
+                    .mapIndexed { i, x -> if (i == 0) x else x - 1 }
+                stacks[to] += buildList { repeat(n) { add(stacks[from].removeLast()) } }.mod()
+            }
+            stacks.joinToString("") { it.last().toString() }
+        }
+
+    return listOf(compute(), compute { reversed() })
 }
