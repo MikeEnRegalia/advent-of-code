81377674d8bad7c9bbe16d2ca0137cc4c42fcbf6
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 06:39:59 +0100

    refactor

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -5,6 +5,17 @@ fun main() = day05(String(System.`in`.readAllBytes())).forEach(::println)
 fun day05(input: String): List<Any?> {
     val (stacksInput, commandsInput) = input.split("\n\n").map { it.split("\n") }
 
+    fun loadStacks(stacksInput: List<String>): MutableList<MutableList<Char>> {
+        val stacks = MutableList((stacksInput.last().length + 2) / 4) { mutableListOf<Char>() }
+        stacksInput.take(stacksInput.size - 1).reversed().forEach { line ->
+            val chunked = line.chunked(4).map(String::trim)
+            chunked.forEachIndexed { i, token ->
+                if (token.isNotBlank()) stacks[i] += token[1]
+            }
+        }
+        return stacks
+    }
+
     fun compute(part1: Boolean = false) = loadStacks(stacksInput).also { stacks ->
         for (line in commandsInput) {
             val (n, from, to) = line.split(" ").mapNotNull(String::toIntOrNull)
@@ -20,15 +31,4 @@ fun day05(input: String): List<Any?> {
 
     return listOf(compute(part1 = true), compute(part1 = false))
         .map { it.joinToString("") { it.last().toString() } }
-}
-
-private fun loadStacks(stacksInput: List<String>): MutableList<MutableList<Char>> {
-    val stacks = MutableList((stacksInput.last().length + 2) / 4) { mutableListOf<Char>() }
-    stacksInput.take(stacksInput.size - 1).reversed().forEach { line ->
-        val chunked = line.chunked(4).map(String::trim)
-        chunked.forEachIndexed { i, token ->
-            if (token.isNotBlank()) stacks[i] += token[1]
-        }
-    }
-    return stacks
 }
