add014100565d43c358211607ca13340df31fb8c
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 06:36:14 +0100

    refactor

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -5,22 +5,17 @@ fun main() = day05(String(System.`in`.readAllBytes())).forEach(::println)
 fun day05(input: String): List<Any?> {
     val (stacksInput, commandsInput) = input.split("\n\n").map { it.split("\n") }
 
-    fun compute(part1: Boolean = false) = with(loadStacks(stacksInput)) {
-        commandsInput.forEach { line ->
+    fun compute(part1: Boolean = false) = loadStacks(stacksInput).also { stacks ->
+        for (line in commandsInput) {
             val (n, from, to) = line.split(" ").mapNotNull(String::toIntOrNull)
-            if (!part1) {
-                val newCrates = mutableListOf<Char>()
-                repeat(n) {
-                    newCrates += this[from - 1].removeLast()
-                }
-                this[to - 1] += newCrates.reversed()
+            if (part1) {
+                repeat(n) { stacks[to - 1] += stacks[from - 1].removeLast() }
             } else {
-                repeat(n) {
-                    this[to - 1] += this[from - 1].removeLast()
-                }
+                val newCrates = mutableListOf<Char>()
+                repeat(n) { newCrates += stacks[from - 1].removeLast() }
+                stacks[to - 1] += newCrates.reversed()
             }
         }
-        this
     }
 
     return listOf(compute(part1 = true), compute(part1 = false))
