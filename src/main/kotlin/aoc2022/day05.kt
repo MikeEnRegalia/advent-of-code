28896b7015aa6fdfc0bdac4460c2a8ae3bd2d5c1
Michael Böiers <mikeenregalia@gmail.com>
2022-12-05 13:55:23 +0100

    refactor

--- src/main/kotlin/aoc2022/day05.kt
+++ src/main/kotlin/aoc2022/day05.kt
@@ -6,9 +6,10 @@ fun day05(input: String): List<Any?> {
     val (stacksInput, commandsInput) = input.split("\n\n").map { it.split("\n") }
 
     val size = stacksInput.last().trim().substringAfterLast(' ').toInt()
-    val initialStacks = with(stacksInput.reversed().drop(1)) {
+    val initialStacks = with(stacksInput.dropLast(1).asReversed()) {
         List(size) { i -> mapNotNull { it.getOrNull(1 + (i * 4))?.takeUnless(Char::isWhitespace) } }
     }
+
     val commands = commandsInput.map {
         it.split(" ").mapNotNull(String::toIntOrNull).mapIndexed { i, x -> if (i == 0) x else x - 1 }
     }
