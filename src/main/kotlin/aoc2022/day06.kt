86c16c13e8a0980ae8c20a627129109136d458ae
Michael Böiers <mikeenregalia@gmail.com>
2022-12-06 06:16:08 +0100

    day 6

--- src/main/kotlin/aoc2022/day06.kt
+++ src/main/kotlin/aoc2022/day06.kt
@@ -1,17 +1,5 @@
 package aoc2022
 
-import com.google.gson.Gson
-import java.security.MessageDigest
-
 fun main() = day06(String(System.`in`.readAllBytes())).forEach(::println)
 
-private fun day06(input: String): List<Any?> {
-
-    return listOf(null, null)
-}
-
-private fun String.fullyMatch(regex: Regex) = regex.matchEntire(this)!!.destructured.toList()
-private fun String.toJsonList() = Gson().fromJson(this, ArrayList::class.java)
-private fun String.md5() = toByteArray().md5().joinToString("") { it.toUByte().toHex() }
-private fun ByteArray.md5() = MessageDigest.getInstance("MD5").digest(this)
-private fun UByte.toHex() = toString(16).padStart(2, '0')
+private fun day06(s: String) = listOf(4, 14).map { n -> s.windowed(n).indexOfFirst { it.toSet().size == n } + n }
