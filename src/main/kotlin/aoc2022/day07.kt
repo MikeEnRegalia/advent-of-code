6ef3bc93136d4eb4d534460babeb0334a7428b33
Michael Böiers <mikeenregalia@gmail.com>
2022-12-07 08:33:05 +0100

    refactor

--- src/main/kotlin/aoc2022/day07.kt
+++ src/main/kotlin/aoc2022/day07.kt
@@ -4,34 +4,30 @@ fun main() = day07(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day07(input: String): List<Any?> {
     val currentPath = mutableListOf<String>()
-    val sizes = mutableMapOf<String, Long>()
-    var used = 0L
+    val sizes = mutableMapOf<String, Int>()
+    var used = 0
     for (line in input.lines()) {
-        if (line.startsWith("$ cd ")) with(currentPath) {
-            when (val to = line.substringAfterLast(" ")) {
-                ".." -> removeLast()
-                "/" -> clear()
-                else -> add(to)
+        if (line.startsWith("$ cd ")) {
+            with(currentPath) {
+                when (val to = line.substringAfterLast(" ")) {
+                    ".." -> removeLast()
+                    "/" -> clear()
+                    else -> add(to)
+                }
             }
+            continue
         }
-        else {
-            val size = line.split(" ").first().toIntOrNull() ?: continue
-            var dir = "/"
-            for (e in currentPath) {
-                if (!dir.endsWith("/")) dir += "/"
-                dir += e
-                sizes.compute(dir) { _, prev -> (prev ?: 0L) + size }
-            }
-            used += size
+        val size = line.split(" ").first().toIntOrNull() ?: continue
+        var dir = "/"
+        for (path in currentPath) {
+            if (!dir.endsWith("/")) dir += "/"
+            dir += path
+            sizes.compute(dir) { _, prev -> (prev ?: 0) + size }
         }
+        used += size
     }
 
-    val total = 70000000L
-    val needed = 30000000L
-    val free = total - used
-    val toFree = needed - free
-
     return with(sizes.values) {
-        listOf(filter { it <= 100000 }.sum(), filter { it >= toFree }.min())
+        listOf(filter { it <= 100_000 }.sum(), filter { it >= used - 40_000_000 }.min())
     }
 }
