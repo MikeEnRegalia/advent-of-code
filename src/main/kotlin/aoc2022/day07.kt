04be0bf58566d2c63b18c690dff55275fdc277d3
Michael Böiers <mikeenregalia@gmail.com>
2022-12-07 09:01:05 +0100

    refactor

--- src/main/kotlin/aoc2022/day07.kt
+++ src/main/kotlin/aoc2022/day07.kt
@@ -3,25 +3,25 @@ package aoc2022
 fun main() = day07(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day07(input: String): List<Any?> {
-    val currentPath = mutableListOf<String>()
+    var dir = listOf<String>()
     val sizes = mutableMapOf<List<String>, Int>()
     var used = 0
 
-    fun MutableList<String>.cd(dir: String): Any = when (dir) {
-        ".." -> removeLast()
-        "/" -> clear()
-        else -> add(dir)
+    fun List<String>.cd(dir: String) = when (dir) {
+        ".." -> subList(0, size - 1)
+        "/" -> listOf()
+        else -> plus(dir)
     }
 
     fun addFile(size: Int) {
-        currentPath.indices.map { currentPath.subList(0, it + 1).toList() }.forEach { path ->
+        dir.indices.map { dir.subList(0, it + 1).toList() }.forEach { path ->
             sizes[path] = (sizes[path] ?: 0) + size
         }
         used += size
     }
 
     for (line in input.lines()) when {
-        line.startsWith("$ cd ") -> currentPath.cd(line.substringAfterLast(" "))
+        line.startsWith("$ cd ") -> dir = dir.cd(line.substringAfterLast(" "))
         else -> line.split(" ").first().toIntOrNull()?.let { addFile(it) }
     }
 
