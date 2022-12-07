be9e1def56538263f4854cb292ba2e16c86bf48d
Michael Böiers <mikeenregalia@gmail.com>
2022-12-07 08:56:20 +0100

    refactor

--- src/main/kotlin/aoc2022/day07.kt
+++ src/main/kotlin/aoc2022/day07.kt
@@ -4,27 +4,26 @@ fun main() = day07(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day07(input: String): List<Any?> {
     val currentPath = mutableListOf<String>()
+    val sizes = mutableMapOf<List<String>, Int>()
+    var used = 0
+
     fun MutableList<String>.cd(dir: String): Any = when (dir) {
         ".." -> removeLast()
         "/" -> clear()
         else -> add(dir)
     }
 
-    val sizes = mutableMapOf<List<String>, Int>()
-    var used = 0
-
-    for (line in input.lines()) {
-        if (line.startsWith("$ cd ")) currentPath.cd(line.substringAfterLast(" "))
-        else {
-            val size = line.split(" ").first().toIntOrNull() ?: continue
-            currentPath.indices.map { currentPath.subList(0, it + 1).toList() }.forEach { path ->
-                sizes[path] = (sizes[path] ?: 0) + size
-            }
-            used += size
+    fun addFile(size: Int) {
+        currentPath.indices.map { currentPath.subList(0, it + 1).toList() }.forEach { path ->
+            sizes[path] = (sizes[path] ?: 0) + size
         }
+        used += size
     }
 
-    return with(sizes.values) {
-        listOf(filter { it <= 100_000 }.sum(), filter { it >= used - 40_000_000 }.min())
+    for (line in input.lines()) when {
+        line.startsWith("$ cd ") -> currentPath.cd(line.substringAfterLast(" "))
+        else -> line.split(" ").first().toIntOrNull()?.let { addFile(it) }
     }
+
+    return with(sizes.values) { listOf(filter { it <= 100_000 }.sum(), filter { it >= used - 40_000_000 }.min()) }
 }
