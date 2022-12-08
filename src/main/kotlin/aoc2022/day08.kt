09dcc638461b44837666495a40803de584cb769f
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 22:29:18 +0100

    refactor day 8

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -1,19 +1,18 @@
 package aoc2022
 
 fun main() {
-    val M = generateSequence(::readlnOrNull).asIterable().map { it.map(Char::digitToInt) }
+    val M = generateSequence(::readlnOrNull).map { it.map(Char::digitToInt) }.toList()
 
     fun linesOfSight(x: Int, y: Int) = sequenceOf(
         sequenceOf((x - 1 downTo 0), (x + 1 until M[0].size)).map { it.map { x -> x to y } },
         sequenceOf((y - 1 downTo 0), (y + 1 until M.size)).map { it.map { y -> x to y } }
     ).flatten().map { it.map { (x, y) -> M[y][x] } }
 
+    fun List<Int>.countVisible(h: Int) = indexOfFirst { it >= h }.let { if (it == -1) size else it + 1 }
+
     with(M.flatMapIndexed { y, l -> l.mapIndexed { x, h -> Triple(x, y, h) } }) {
-        println(count { (x, y, h) -> linesOfSight(x, y).any { it.isVisible(h) } })
+        println(count { (x, y, h) -> linesOfSight(x, y).any { it.all { oh -> oh < h } } })
         println(maxOf { (x, y, h) -> linesOfSight(x, y).map { it.countVisible(h) }.reduce(Int::times) })
     }
 }
-
-private fun List<Int>.isVisible(h: Int) = all { it < h }
-private fun List<Int>.countVisible(h: Int) = indexOfFirst { it >= h }.let { if (it == -1) size else it + 1 }
 
