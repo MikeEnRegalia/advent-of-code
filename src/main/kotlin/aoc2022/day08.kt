97ca2548fbebe9e2ffc0cc6268df79a0e4f7a986
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 15:46:12 +0100

    refactor day 8

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -8,12 +8,12 @@ fun main() {
         sequenceOf((y - 1 downTo 0), (y + 1 until M.size)).map { it.map { y -> x to y } }
     ).flatten().map { it.map { (x, y) -> M[y][x] } }
 
-    fun List<Int>.isVisible(h: Int) = all { it < h }
-    fun List<Int>.countVisible(h: Int) = indexOfFirst { it >= h }.let { if (it == -1) size else it + 1 }
-
     with(M.flatMapIndexed { y, l -> l.mapIndexed { x, h -> Triple(x, y, h) } }) {
         println(count { (x, y, h) -> linesOfSight(x, y).any { it.isVisible(h) } })
         println(maxOf { (x, y, h) -> linesOfSight(x, y).map { it.countVisible(h) }.reduce(Int::times) })
     }
 }
+
+private fun List<Int>.isVisible(h: Int) = all { it < h }
+private fun List<Int>.countVisible(h: Int) = indexOfFirst { it >= h }.let { if (it == -1) size else it + 1 }
 
