d52bb242950c478dbfbbc4b70137ac99d4318860
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 09:54:24 +0100

    refactor

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -1,23 +1,21 @@
 package aoc2022
 
-fun main() = day00(String(System.`in`.readAllBytes())).forEach(::println)
+fun main() = day00(System.`in`.reader().readLines()).forEach(::println)
 
-private fun day00(input: String): List<Any?> {
-    data class Pos(val x: Int, val y: Int)
+private fun day00(input: List<String>): List<Any?> {
+    val heights = input.map { it.map(Char::digitToInt) }
 
-    val heights = input.lines().flatMapIndexed { y, l -> l.mapIndexed { x, h -> Pos(x, y) to h.digitToInt() } }.toMap()
-    val (xMax, yMax) = heights.keys.maxOf { it.x } + 1 to heights.keys.maxOf { it.y } + 1
+    fun linesOfSight(x: Int, y: Int) = sequenceOf(
+        (0 until x).reversed().map { it to y }, (x + 1 until heights[0].size).map { it to y },
+        (0 until y).reversed().map { x to it }, (y + 1 until heights.size).map { x to it }
+    ).map { it.map { (x, y) -> heights[y][x] } }
 
-    fun Pos.linesOfSight() = sequenceOf(
-        (0 until x).reversed().map { Pos(it, y) }, (x + 1 until xMax).map { Pos(it, y) },
-        (0 until y).reversed().map { Pos(x, it) }, (y + 1 until yMax).map { Pos(x, it) }
-    ).map { it.map(heights::getValue) }
+    fun List<Int>.countViewable(h: Int) = if (none { it >= h }) size else takeWhile { it < h }.count() + 1
 
-    fun List<Int>.countViewable(fromHeight: Int) =
-        if (none { it >= fromHeight }) size else takeWhile { it < fromHeight }.count() + 1
-
-    return listOf(
-        heights.count { (pos, height) -> pos.linesOfSight().any { los -> los.all { it < height } } },
-        heights.maxOf { (pos, height) -> pos.linesOfSight().map { it.countViewable(height) }.reduce(Int::times) }
-    )
+    return with(heights.flatMapIndexed { y, l -> l.mapIndexed { x, h -> Triple(x, y, h) } }) {
+        listOf(
+            count { (x, y, h) -> linesOfSight(x, y).any { los -> los.all { it < h } } },
+            maxOf { (x, y, h) -> linesOfSight(x, y).map { it.countViewable(h) }.reduce(Int::times) }
+        )
+    }
 }
