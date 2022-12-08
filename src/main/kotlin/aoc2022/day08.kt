20e155ee44408b576017f66c64134e689d3b360b
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 07:59:06 +0100

    day 8 solved

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -1,17 +1,43 @@
 package aoc2022
 
-import com.google.gson.Gson
-import java.security.MessageDigest
-
 fun main() = day00(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day00(input: String): List<Any?> {
 
-    return listOf(null, null)
-}
+    data class Pos(val x: Int, val y: Int)
 
-private fun String.fullyMatch(regex: Regex) = regex.matchEntire(this)!!.destructured.toList()
-private fun String.toJsonList() = Gson().fromJson(this, ArrayList::class.java)
-private fun String.md5() = toByteArray().md5().joinToString("") { it.toUByte().toHex() }
-private fun ByteArray.md5() = MessageDigest.getInstance("MD5").digest(this)
-private fun UByte.toHex() = toString(16).padStart(2, '0')
+    val lines = input.lines()
+    val heights = lines.flatMapIndexed { y, l ->
+        l.mapIndexed { x, h -> Pos(x, y) to h.toString().toInt() }
+    }.toMap()
+
+    val part1 = heights.count { (pos, height) ->
+        (0 until pos.x).all { heights.getValue(Pos(it, pos.y)) < height } ||
+                (pos.x + 1 until lines[0].length).all { heights.getValue(Pos(it, pos.y)) < height } ||
+                (0 until pos.y).all { heights.getValue(Pos(pos.x, it)) < height } ||
+                (pos.y + 1 until lines.size).all { heights.getValue(Pos(pos.x, it)) < height }
+    }
+
+    fun List<Pos>.countViewable(ownHeight: Int): Long {
+        var count = 0L
+        for (pos in this) {
+            val posHeight = heights.getValue(pos)
+            count++
+            if (posHeight >= ownHeight) return count
+        }
+        return count
+    }
+
+    val part2 = heights.keys.maxOf { pos ->
+        val left = (0 until pos.x).reversed().map { Pos(it, pos.y) }
+        val right = (pos.x + 1 until lines[0].length).map { Pos(it, pos.y) }
+        val up = (0 until pos.y).reversed().map { Pos(pos.x, it) }
+        val down = (pos.y + 1 until lines.size).map { Pos(pos.x, it) }
+
+        listOf(up, left, down, right)
+            .map { it.countViewable(heights.getValue(pos)) }
+            .also { if (pos.x == 2 && pos.y == 3) println("$it") }
+            .reduce { a, b -> a * b }
+    }
+    return listOf(part1, part2)
+}
