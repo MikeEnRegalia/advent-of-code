f868d7fe44d7466fe2b1004e31ba627095a80ad2
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 10:23:59 +0100

    refactor

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -2,18 +2,18 @@ package aoc2022
 
 fun main() = day08(System.`in`.reader().readLines()).forEach(::println)
 
-private fun day08(input: List<String>): List<Any?> {
-    val heights = input.map { it.map(Char::digitToInt) }
+private fun day08(input: List<String>): List<Int> {
+    val H = input.map { it.map(Char::digitToInt) }
 
     fun linesOfSight(x: Int, y: Int) = sequenceOf(
-        (0 until x).reversed().map { it to y }, (x + 1 until heights[0].size).map { it to y },
-        (0 until y).reversed().map { x to it }, (y + 1 until heights.size).map { x to it }
-    ).map { it.map { (x, y) -> heights[y][x] } }
+        (0 until x).reversed().map { it to y }, (x + 1 until H[0].size).map { it to y },
+        (0 until y).reversed().map { x to it }, (y + 1 until H.size).map { x to it }
+    ).map { it.map { (x, y) -> H[y][x] } }
 
     fun List<Int>.isVisible(h: Int) = all { it < h }
     fun List<Int>.countVisible(h: Int) = indexOfFirst { it >= h }.let { if (it == -1) size else it + 1 }
 
-    return with(heights.flatMapIndexed { y, l -> l.mapIndexed { x, h -> Triple(x, y, h) } }) {
+    return with(H.flatMapIndexed { y, l -> l.mapIndexed { x, h -> Triple(x, y, h) } }) {
         listOf(
             count { (x, y, h) -> linesOfSight(x, y).any { it.isVisible(h) } },
             maxOf { (x, y, h) -> linesOfSight(x, y).map { it.countVisible(h) }.reduce(Int::times) }
