6c1cb346d98237cc1542780d853cd6e2e5b97914
Michael Böiers <mikeenregalia@gmail.com>
2022-12-08 09:20:32 +0100

    refactor

--- src/main/kotlin/aoc2022/day08.kt
+++ src/main/kotlin/aoc2022/day08.kt
@@ -3,41 +3,20 @@ package aoc2022
 fun main() = day00(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day00(input: String): List<Any?> {
-
     data class Pos(val x: Int, val y: Int)
 
-    val lines = input.lines()
-    val heights = lines.flatMapIndexed { y, l ->
-        l.mapIndexed { x, h -> Pos(x, y) to h.toString().toInt() }
-    }.toMap()
+    val heights = input.lines().flatMapIndexed { y, l -> l.mapIndexed { x, h -> Pos(x, y) to h.digitToInt() } }.toMap()
+    val (xMax, yMax) = heights.keys.maxOf { it.x } + 1 to heights.keys.maxOf { it.y } + 1
 
-    val part1 = heights.count { (pos, height) ->
-        (0 until pos.x).all { heights.getValue(Pos(it, pos.y)) < height } ||
-                (pos.x + 1 until lines[0].length).all { heights.getValue(Pos(it, pos.y)) < height } ||
-                (0 until pos.y).all { heights.getValue(Pos(pos.x, it)) < height } ||
-                (pos.y + 1 until lines.size).all { heights.getValue(Pos(pos.x, it)) < height }
-    }
+    fun Pos.linesOfSight() = sequenceOf(
+        (0 until x).reversed().map { Pos(it, y) }, (x + 1 until xMax).map { Pos(it, y) },
+        (0 until y).reversed().map { Pos(x, it) }, (y + 1 until yMax).map { Pos(x, it) }
+    ).map { it.map(heights::getValue) }
 
-    fun List<Pos>.countViewable(ownHeight: Int): Long {
-        var count = 0L
-        for (pos in this) {
-            val posHeight = heights.getValue(pos)
-            count++
-            if (posHeight >= ownHeight) return count
-        }
-        return count
-    }
+    fun List<Int>.countViewable(fromHeight: Int) =
+        if (none { it >= fromHeight }) size else takeWhile { it < fromHeight }.count() + 1
 
-    val part2 = heights.keys.maxOf { pos ->
-        val left = (0 until pos.x).reversed().map { Pos(it, pos.y) }
-        val right = (pos.x + 1 until lines[0].length).map { Pos(it, pos.y) }
-        val up = (0 until pos.y).reversed().map { Pos(pos.x, it) }
-        val down = (pos.y + 1 until lines.size).map { Pos(pos.x, it) }
-
-        listOf(up, left, down, right)
-            .map { it.countViewable(heights.getValue(pos)) }
-            .also { if (pos.x == 2 && pos.y == 3) println("$it") }
-            .reduce { a, b -> a * b }
-    }
-    return listOf(part1, part2)
+    return listOf(
+        heights.count { (pos, height) -> pos.linesOfSight().any { los -> los.all { it < height } } },
+        heights.maxOf { (pos, height) -> pos.linesOfSight().map { it.countViewable(height) }.reduce(Int::times) })
 }
