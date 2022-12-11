16349b5f22b29b821ea476549f7fd828fe4bc75a
Michael Böiers <mikeenregalia@gmail.com>
2022-12-11 06:19:27 +0100

    refactor

--- src/main/kotlin/aoc2022/day10.kt
+++ src/main/kotlin/aoc2022/day10.kt
@@ -3,13 +3,12 @@ package aoc2022
 fun main() {
     val screen = Array(6) { BooleanArray(40) }
     fun String.toRow() = if (this == "noop") sequenceOf(0) else sequenceOf(0, substringAfterLast(" ").toInt())
-    val (_, part1) = generateSequence(::readlnOrNull).flatMap(String::toRow).foldIndexed(1 to 0) { cycle, (x, p1), n ->
+    generateSequence(::readlnOrNull).flatMap(String::toRow).foldIndexed(1 to 0) { cycle, (x, p1), n ->
         val row = (cycle) / 40
         val col = (cycle) % 40
         if (col - x in -1..1) screen[row][col] = true
-        x + n to (cycle + 1).let { if (it in listOf(20, 60, 100, 140, 180, 220)) p1 + it * x else it }
-    }
-    println(part1)
+        x + n to (cycle + 1).let { if (it in listOf(20, 60, 100, 140, 180, 220)) p1 + it * (x + n) else p1 }
+    }.let { (_, part1) -> println(part1) }
     println(screen.joinToString("\n") { it.joinToString("") { c -> if (c) "O" else " " } })
 }
 
