26f6d232cb29eabc5a8ce0818238c771811de3f0
Michael Böiers <mikeenregalia@gmail.com>
2022-12-12 09:32:42 +0100

    refactor

--- src/main/kotlin/aoc2022/day11.kt
+++ src/main/kotlin/aoc2022/day11.kt
@@ -1,68 +1,40 @@
 package aoc2022
 
-import java.math.BigInteger
-import java.math.BigInteger.ZERO
-
 fun main() = day11(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day11(input: String): List<Any?> {
-    data class N(val r: Long, val factors: MutableSet<Long> = mutableSetOf()) {
-        fun toBigInteger() = BigInteger.valueOf(r) * (if (factors.isEmpty()) BigInteger.ONE else factors.map { BigInteger.valueOf(it) }.reduce(BigInteger::times))
-        fun divisibleBy(d: Int) = toBigInteger().mod(BigInteger.valueOf(d.toLong())) == ZERO
-    }
-
-    fun BigInteger.toN() = N(longValueExact())
-
-    fun Int.big() = N(toLong())
     data class Monkey(
-        var items2: MutableList<Int>,
-        val operation: (BigInteger) -> BigInteger,
-        val test: (BigInteger) -> Int
+        var items: MutableList<Long>,
+        val operation: (Long) -> Long,
+        val divBy: Long,
+        val ifTrue: Int,
+        val ifFalse: Int
     ) {
-        val items = items2.map { it.big() }.toMutableList()
+        fun test(item: Long) = if (item % divBy == 0L) ifTrue else ifFalse
     }
 
-    val monkeys = listOf(
-        Monkey(
-            mutableListOf(74, 64, 74, 63, 53),
-            { it * BigInteger.valueOf(7L) },
-            { if (it.mod(BigInteger.valueOf(5L)) == ZERO) 1 else 6 }),
-        Monkey(mutableListOf(69, 99, 95, 62), { it * it }, { if (it.mod(BigInteger.valueOf(17L)) == ZERO) 2 else 5 }),
-        Monkey(
-            mutableListOf(59, 81),
-            { it + BigInteger.valueOf(8L) },
-            { if (it.mod(BigInteger.valueOf(7L)) == ZERO) 4 else 3 }),
-        Monkey(
-            mutableListOf(50, 67, 63, 57, 63, 83, 97),
-            { it + BigInteger.valueOf(4L) },
-            { if (it.mod(BigInteger.valueOf(13L)) == ZERO) 0 else 7 }),
-        Monkey(
-            mutableListOf(61, 94, 85, 52, 81, 90, 94, 70),
-            { it + BigInteger.valueOf(3L) },
-            { if (it.mod(BigInteger.valueOf(19L)) == ZERO) 7 else 3 }),
-        Monkey(
-            mutableListOf(69),
-            { it + BigInteger.valueOf(5L) },
-            { if (it.mod(BigInteger.valueOf(3L)) == ZERO) 4 else 2 }),
-        Monkey(
-            mutableListOf(54, 55, 58),
-            { it + BigInteger.valueOf(7L) },
-            { if (it.mod(BigInteger.valueOf(11L)) == ZERO) 1 else 5 }),
-        Monkey(
-            mutableListOf(79, 51, 83, 88, 93, 76),
-            { it * BigInteger.valueOf(3L) },
-            { if (it.mod(BigInteger.valueOf(2L)) == ZERO) 0 else 6 })
-    )
-
+    val monkeys = input.split("\n\n").map { it.split("\n") }.map { lines ->
+        val items = lines[1].split(" ", ",").mapNotNull(String::toLongOrNull).toMutableList()
+        val opChar = lines[2][lines[2].lastIndexOf(" ") - 1]
+        val operand = lines[2].substringAfterLast(" ")
+        val operation: (Long) -> Long = when (opChar) {
+            '+' -> { old -> old + (operand.toLongOrNull() ?: old) }
+            '*' -> { old -> old * (operand.toLongOrNull() ?: old) }
+            else -> throw IllegalArgumentException()
+        }
+        val divBy = lines[3].split(" ").mapNotNull(String::toLongOrNull).single()
+        val monkeyIfTrue = lines[4].split(" ").mapNotNull(String::toIntOrNull).single()
+        val monkeyIfFalse = lines[5].split(" ").mapNotNull(String::toIntOrNull).single()
+        Monkey(items, operation, divBy, monkeyIfTrue, monkeyIfFalse)
+    }
 
     val inspections = mutableMapOf<Int, Long>()
-    repeat(10000) {
+    repeat(20) {
         for ((monkeyIndex, monkey) in monkeys.withIndex()) {
             inspections[monkeyIndex] = inspections.getOrDefault(monkeyIndex, 0) + monkey.items.size
-            monkey.items.map { monkey.operation(it.toBigInteger()) }
-                .map { it }
+            monkey.items.map { monkey.operation(it) / 3 }
                 .forEach {
-                    monkeys[monkey.test(it)].items += it.toN()
+                    monkeys[monkey.test(it)].items += it
                 }
             monkey.items.clear()
         }
