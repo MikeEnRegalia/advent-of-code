6edf351f4f14f2dd3679984f3c5f4863f1077af1
Michael Böiers <mikeenregalia@gmail.com>
2022-12-13 23:12:40 +0100

    refactor

--- src/main/kotlin/aoc2022/day11.kt
+++ src/main/kotlin/aoc2022/day11.kt
@@ -1,50 +1,37 @@
 package aoc2022
 
 fun main() {
-    data class Item(private val initial: Int, private val remainders: Map<Int, Int>) {
-        fun rem(d: Int, useInitial: Boolean) = 0 == if (useInitial) (initial % d) else remainders.getValue(d)
-        fun transform(op: (Int) -> Int) = Item(op(initial), remainders.toMutableMap().apply {
-            keys.forEach { prime -> set(prime, op(getValue(prime)) % prime) }
-        })
+    data class Monkey(val items: MutableList<Long>, val operation: (Long) -> Long, val test: Triple<Int, Int, Int>) {
+        var inspections: Long = 0
+        fun test(item: Long) = (if (item % test.first == 0L) test.second else test.third).also { inspections++ }
     }
 
-    fun Int.toItem() = Item(this, buildMap {
-        for (prime in listOf(2, 3, 5, 7, 9, 11, 13, 17, 19, 23)) set(prime, this@toItem % prime)
-    })
-
-    data class Monkey(val items: MutableList<Item>, val operation: (Item) -> Item, val test: Triple<Int, Int, Int>) {
-        fun test(item: Item, useInitial: Boolean) = if (item.rem(test.first, useInitial)) test.second else test.third
-    }
-
-    val input = System.`in`.reader().readText()
+    val input = System.`in`.reader().readText().split("\n\n").map { it.split("\n") }
     fun doMonkeyBusiness(rounds: Int, div: Boolean): Long {
-        val monkeys = input.split("\n\n").map { it.split("\n") }.map { lines ->
-            val items = lines[1].split(" ", ",").mapNotNull(String::toIntOrNull).map { it.toItem() }.toMutableList()
+        val monkeys = input.map { lines ->
+            val items = lines[1].split(" ", ",").mapNotNull(String::toLongOrNull).toMutableList()
             val opChar = lines[2][lines[2].lastIndexOf(" ") - 1]
             val operand = lines[2].substringAfterLast(" ").toIntOrNull()
-            val operation: (Item) -> Item = when {
-                opChar == '+' -> { old -> old.transform { it + operand!! } }
-                operand != null -> { old -> old.transform { it * operand } }
-                else -> { old -> old.transform { it * it } }
+            val operation: (Long) -> Long = when {
+                opChar == '+' -> ({ it + operand!! })
+                operand != null -> ({ it * operand })
+                else -> ({ it * it })
             }
-
             val divBy = lines[3].split(" ").mapNotNull(String::toIntOrNull).single()
             val ifTrue = lines[4].split(" ").mapNotNull(String::toIntOrNull).single()
             val ifFalse = lines[5].split(" ").mapNotNull(String::toIntOrNull).single()
             Monkey(items, operation, Triple(divBy, ifTrue, ifFalse))
         }
+        val commonProduct by lazy { monkeys.map { it.test.first }.reduce(Int::times) }
+        fun Long.relax() = if (div) this / 3 else this % commonProduct
 
-        val inspections = LongArray(monkeys.size) { 0 }
         repeat(rounds) {
-            for ((i, monkey) in monkeys.withIndex()) with(monkey) {
-                inspections[i] = inspections[i] + items.size
-                for (item in items) operation(item)
-                    .let { newItem -> if (div) newItem.transform { it / 3 } else newItem }
-                    .also { monkeys[test(it, div)].items += it }
-                items.clear()
+            for (monkey in monkeys) with(monkey) {
+                while (items.isNotEmpty())
+                    operation(items.removeFirst()).relax().also { monkeys[test(it)].items += it }
             }
         }
-        return inspections.sortedDescending().take(2).reduce(Long::times)
+        return monkeys.map(Monkey::inspections).sortedDescending().take(2).reduce(Long::times)
     }
     listOf(doMonkeyBusiness(20, true), doMonkeyBusiness(10_000, false)).forEach(::println)
 }
