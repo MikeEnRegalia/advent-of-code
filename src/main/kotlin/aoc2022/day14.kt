131c5244f3d8626dd5f2605ce2c6cd4f63cf4f4f
Michael Böiers <mikeenregalia@gmail.com>
2022-12-14 11:28:12 +0100

    refactor

--- src/main/kotlin/aoc2022/day14.kt
+++ src/main/kotlin/aoc2022/day14.kt
@@ -4,31 +4,27 @@ import kotlin.math.max
 import kotlin.math.min
 
 fun main() {
-    data class Pos(val x: Int, val y: Int) {
-        fun flowTo(p: (Pos) -> Boolean) =
-            sequenceOf(copy(y = y + 1), copy(x = x - 1, y = y + 1), copy(x = x + 1, y = y + 1)).firstOrNull(p)
-    }
-
-    val rock = buildSet {
-        generateSequence(::readlnOrNull).forEach { row ->
-            row.split(" -> ").map { it.split(",").map(String::toInt) }.windowed(2).forEach { (from, to) ->
-                for (x in min(from[0], to[0])..max(from[0], to[0]))
-                    for (y in min(from[1], to[1])..max(from[1], to[1])) this.add(Pos(x, y))
+    val rock = generateSequence(::readlnOrNull).flatMap { row ->
+        row.split(" -> ").map { it.split(",").map(String::toInt) }.windowed(2).flatMap { (from, to) ->
+            (min(from[0], to[0])..max(from[0], to[0])).flatMap { x ->
+                (min(from[1], to[1])..max(from[1], to[1])).map { y -> Pair(x, y) }
             }
         }
-    }
+    }.toSet()
 
-    val maxY = rock.maxOf { it.y }
-    val source = Pos(500, 0)
+    val maxY = rock.maxOf { it.second }
+    val source = Pair(500, 0)
+    fun Pair<Int, Int>.flowTo() = let { (x, y) -> sequenceOf(x to y + 1, x - 1 to y + 1, x + 1 to y + 1) }
+        .filterNot { it in rock || it.second >= maxY + 2 }
 
-    val stableSand = mutableSetOf<Pos>()
+    val stableSand = mutableSetOf<Pair<Int, Int>>()
 
-    var hitFloor = false
-    var s: Pos = source
+    var reachedFloor = false
+    var s: Pair<Int, Int> = source
     while (source !in stableSand) {
-        s = s.flowTo { it.y < maxY + 2 && it !in rock && it !in stableSand } ?: source.also { stableSand += s }
-        if (!hitFloor && s.y == maxY + 1) {
-            hitFloor = true
+        s = s.flowTo().firstOrNull { it !in stableSand } ?: source.also { stableSand += s }
+        if (s.second == maxY + 1 && !reachedFloor) {
+            reachedFloor = true
             println(stableSand.size)
         }
     }
