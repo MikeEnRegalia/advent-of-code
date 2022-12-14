918cb293e8d43df27d69e9594d0057bfb568f1a7
Michael Böiers <mikeenregalia@gmail.com>
2022-12-14 09:07:59 +0100

    refactor

--- src/main/kotlin/aoc2022/day14.kt
+++ src/main/kotlin/aoc2022/day14.kt
@@ -3,43 +3,37 @@ package aoc2022
 import kotlin.math.max
 import kotlin.math.min
 
-fun main() = day14(String(System.`in`.readAllBytes())).forEach(::println)
-
-private fun day14(input: String): List<Any?> {
+fun main() {
     data class Pos(val x: Int, val y: Int) {
-        val below by lazy { copy(y = y + 1) }
-        val diagLeft by lazy { copy(x = x - 1, y = y + 1) }
-        val diagRight by lazy { copy(x = x + 1, y = y + 1) }
+        fun flowTo(p: (Pos) -> Boolean) =
+            sequenceOf(copy(y = y + 1), copy(x = x - 1, y = y + 1), copy(x = x + 1, y = y + 1)).firstOrNull(p)
     }
 
     val rock = buildSet {
-        input.lines().map { it.split(" -> ").map { it.split(",").map(String::toInt) } }.forEach {
-            it.windowed(2).forEach { (from, to) ->
+        generateSequence(::readlnOrNull).forEach { row ->
+            row.split(" -> ").map { it.split(",").map(String::toInt) }.windowed(2).forEach { (from, to) ->
                 for (x in min(from[0], to[0])..max(from[0], to[0]))
-                    for (y in min(from[1], to[1])..max(from[1], to[1])) add(Pos(x, y))
+                    for (y in min(from[1], to[1])..max(from[1], to[1])) this.add(Pos(x, y))
             }
         }
     }
 
-    val floor = rock.maxOf { it.y } + 2
-    val sandFrom = Pos(500, 0)
+    val maxY = rock.maxOf { it.y }
+    val floor = maxY + 2
+    val source = Pos(500, 0)
 
-    fun simulate(haveFloor: Boolean): Int {
-        val restingSand = mutableSetOf<Pos>()
-
-        var s: Pos = sandFrom
-        while (true) {
-            when {
-                !haveFloor -> if (s.y >= floor) break
-                else -> if (sandFrom in restingSand) break
-            }
-            s = sequenceOf(s.below, s.diagLeft, s.diagRight)
-                .firstOrNull { (!haveFloor || it.y < floor) && it !in rock && it !in restingSand }
-                ?: sandFrom.also { restingSand += s }
-        }
-        return restingSand.size
+    fun part1() = mutableSetOf<Pos>().also { stableSand ->
+        var s: Pos = source
+        while (s.y < maxY)
+            s = s.flowTo { it !in rock && it !in stableSand } ?: source.also { stableSand += s }
     }
 
-    return listOf(simulate(false), simulate(true))
+    fun part2() = mutableSetOf<Pos>().also { stableSand ->
+        var s: Pos = source
+        while (source !in stableSand)
+            s = s.flowTo { it.y < floor && it !in rock && it !in stableSand } ?: source.also { stableSand += s }
+    }
+
+    listOf(part1().size, part2().size).forEach(::println)
 }
 
