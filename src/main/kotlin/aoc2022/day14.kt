2295b2c9923bd6683eea107258bd6e8a8966f1f1
Michael Böiers <mikeenregalia@gmail.com>
2022-12-14 06:39:36 +0100

    refactor

--- src/main/kotlin/aoc2022/day14.kt
+++ src/main/kotlin/aoc2022/day14.kt
@@ -15,22 +15,17 @@ private fun day14(input: String): List<Any?> {
     val rock = buildSet {
         input.lines().map { it.split(" -> ").map { it.split(",").map(String::toInt) } }.forEach {
             it.windowed(2).forEach { (from, to) ->
-                for (x in min(from[0], to[0])..max(from[0], to[0])) {
-                    for (y in min(from[1], to[1])..max(from[1], to[1])) {
-                        add(Pos(x, y))
-                    }
-                }
+                for (x in min(from[0], to[0])..max(from[0], to[0]))
+                    for (y in min(from[1], to[1])..max(from[1], to[1])) add(Pos(x, y))
             }
         }
     }
 
     val floor = rock.maxOf { it.y } + 2
-
     val sandFrom = Pos(500, 0)
 
     fun simulate(haveFloor: Boolean): Int {
         val restingSand = mutableSetOf<Pos>()
-        fun Pos.aboveFloor() = !haveFloor || y < floor
 
         var s: Pos = sandFrom
         while (true) {
@@ -38,16 +33,9 @@ private fun day14(input: String): List<Any?> {
                 !haveFloor -> if (s.y >= floor) break
                 else -> if (sandFrom in restingSand) break
             }
-
-            when {
-                s.below !in rock && s.below !in restingSand && s.below.aboveFloor() -> s = s.below
-                s.diagLeft !in rock && s.diagLeft !in restingSand && s.below.aboveFloor() -> s = s.diagLeft
-                s.diagRight !in rock && s.diagRight !in restingSand && s.below.aboveFloor() -> s = s.diagRight
-                else -> {
-                    restingSand += s
-                    s = sandFrom
-                }
-            }
+            s = sequenceOf(s.below, s.diagLeft, s.diagRight)
+                .firstOrNull { (!haveFloor || it.y < floor) && it !in rock && it !in restingSand }
+                ?: sandFrom.also { restingSand += s }
         }
         return restingSand.size
     }
