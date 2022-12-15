09d7d58506557084fb37b255f1941077dd99072c
Michael Böiers <mikeenregalia@gmail.com>
2022-12-15 12:06:03 +0100

    refactor

--- src/main/kotlin/aoc2022/day15.kt
+++ src/main/kotlin/aoc2022/day15.kt
@@ -2,6 +2,9 @@ package aoc2022
 
 import kotlin.math.abs
 
+private const val MAX_Y = 4000000
+private const val PART1_Y = 2000000
+
 fun main() {
     val data = generateSequence(::readlnOrNull).map { l ->
         l.split("""[=,:\s]""".toRegex()).mapNotNull(String::toIntOrNull).let { listOf(it[0] to it[1], it[2] to it[3]) }
@@ -9,24 +12,15 @@ fun main() {
 
     fun dist(a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
 
-    data.fold(mutableSetOf<Pair<Int, Int>>()) { acc, (s, b) ->
-        val d = dist(s, b)
-        val start = s.first to 2000000
-        var p = start
+    val knownBeacons = data.map { it[1] }.toSet()
 
-        while (dist(s, p) < d) {
-            acc += p
-            p = p.first + 1 to p.second
-        }
-        p = start
-        while (dist(s, p) <= d) {
-            acc += p
-            p = p.first - 1 to p.second
-        }
+    data.filter { (s, b) -> dist(s, s.first to PART1_Y) <= dist(s, b) }.fold(mutableSetOf<Int>()) { acc, (s, b) ->
+        val d = dist(s, b) - dist(s, s.first to PART1_Y)
+        (-d..d).asSequence().map { s.first + it }.filter { it to PART1_Y !in knownBeacons }.forEach(acc::add)
         acc
     }.size.also(::println)
 
-    fun Pair<Int, Int>.pIsValid() = first in 0..4000000 && second in 0..4000000
+    fun Pair<Int, Int>.pIsValid() = first in 0..MAX_Y && second in 0..MAX_Y
     fun Pair<Int, Int>.beyondAllSensors() = data.none { (sensor, beacon) -> dist(sensor, this) <= dist(sensor, beacon) }
 
     for ((s, b) in data) {
@@ -36,7 +30,7 @@ fun main() {
             val (dx, dy) = listOf(x to y, x to -y, -x to y, -x to -y)
                 .map { (x, y) -> s.first + x to s.second + y }
                 .singleOrNull { it.pIsValid() && it.beyondAllSensors() } ?: continue
-            println(dx.toLong() * 4000000 + dy)
+            println(dx.toLong() * MAX_Y + dy)
             return
         }
     }
