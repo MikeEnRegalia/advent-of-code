8eb19263ae18c73e1e8a7053cc86b44cf5ce8de7
Michael Böiers <mikeenregalia@gmail.com>
2022-12-15 13:04:07 +0100

    refactor

--- src/main/kotlin/aoc2022/day15.kt
+++ src/main/kotlin/aoc2022/day15.kt
@@ -2,35 +2,36 @@ package aoc2022
 
 import kotlin.math.abs
 
-private const val MAX_Y = 4000000
+private const val MAX = 4000000
 private const val PART1_Y = 2000000
 
 fun main() {
+    data class Pos(val x: Int, val y: Int)
+
     val data = generateSequence(::readlnOrNull).map { l ->
-        l.split("""[=,:\s]""".toRegex()).mapNotNull(String::toIntOrNull).let { listOf(it[0] to it[1], it[2] to it[3]) }
+        l.split("""[=,:\s]""".toRegex()).mapNotNull(String::toIntOrNull).let { Pos(it[0], it[1]) to Pos(it[2], it[3]) }
     }.toList()
 
-    fun dist(a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
+    infix fun Pos.dist(p: Pos) = abs(x - p.x) + abs(y - p.y)
 
-    val knownBeacons = data.map { it[1] }.toSet()
+    val knownBeacons = data.map { it.second }.toSet()
 
-    data.filter { (s, b) -> dist(s, s.first to PART1_Y) <= dist(s, b) }.fold(mutableSetOf<Int>()) { acc, (s, b) ->
-        val d = dist(s, b) - dist(s, s.first to PART1_Y)
-        (-d..d).asSequence().map { s.first + it }.filter { it to PART1_Y !in knownBeacons }.forEach(acc::add)
+    data.filter { (s, b) -> s dist Pos(s.x, PART1_Y) <= s dist b }.fold(mutableSetOf<Int>()) { acc, (s, b) ->
+        val d = s.dist(b) - s.dist(Pos(s.x, PART1_Y))
+        (-d..d).asSequence().map { s.x + it }.filter { Pos(it, PART1_Y) !in knownBeacons }.forEach(acc::add)
         acc
     }.size.also(::println)
 
-    fun Pair<Int, Int>.pIsValid() = first in 0..MAX_Y && second in 0..MAX_Y
-    fun Pair<Int, Int>.beyondAllSensors() = data.none { (sensor, beacon) -> dist(sensor, this) <= dist(sensor, beacon) }
+    fun Pos.pIsValid() = x in 0..MAX && y in 0..MAX
+    fun Pos.beyondAllSensors() = data.none { (sensor, beacon) -> dist(sensor) <= sensor.dist(beacon) }
 
     for ((s, b) in data) {
-        val dist = dist(s, b) + 1
+        val dist = s.dist(b) + 1
         for (x in 1..dist) {
-            val y = dist - x
-            val (dx, dy) = listOf(x to y, x to -y, -x to y, -x to -y)
-                .map { (x, y) -> s.first + x to s.second + y }
+            val (dx, dy) = (dist - x).let { y -> listOf(x to y, x to -y, -x to y, -x to -y) }
+                .map { (x, y) -> Pos(s.x + x, s.y + y) }
                 .singleOrNull { it.pIsValid() && it.beyondAllSensors() } ?: continue
-            println(dx.toLong() * MAX_Y + dy)
+            println(dx.toLong() * MAX + dy)
             return
         }
     }
