453a6135915d07cf899e386829e6ea184003d731
Michael Böiers <mikeenregalia@gmail.com>
2022-12-16 14:23:52 +0100

    re-write for part 2

--- src/main/kotlin/aoc2022/day16.kt
+++ src/main/kotlin/aoc2022/day16.kt
@@ -1,12 +1,12 @@
 package aoc2022
 
 import util.remove
+import kotlin.Int.Companion.MAX_VALUE
+import kotlin.math.min
 
 fun main() = day16(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day16(input: String): List<Any?> {
-    data class Valve(val name: String, val rate: Int, val to: List<String>, val open: Boolean = false)
-
     val flowRates = mutableMapOf<String, Int>()
     val tunnels = mutableMapOf<String, Set<String>>()
     input.lines().map { it.split(" ") }.forEach { tokens ->
@@ -17,41 +17,78 @@ private fun day16(input: String): List<Any?> {
         tunnels[name] = to.toSet()
     }
 
-    var max = 0
-    fun findMax(
-        valve: String,
-        elephantValve: String? = null,
-        minute: Int = 0,
-        acc: Int = 0,
-        open: Set<String> = setOf(),
-        path: List<String> = listOf(valve),
-        elephantPath: List<String> = listOfNotNull(elephantValve)
-    ): Int {
-        if (minute == 30) {
-            if (acc > max) {
-                max = acc
+    val functioningValves = flowRates.filter { it.value > 0 }.keys
+
+    fun whereToNext(valve: String, remainingValves: Set<String>) = buildSet {
+        for (target in remainingValves.minus(valve)) {
+            data class State(val valve: String, val path: List<String> = listOf())
+
+            var s = State(valve)
+            val v = mutableSetOf<State>()
+            val u = mutableSetOf<State>()
+            val d = mutableMapOf(s to 0)
+
+            while (true) {
+                tunnels.getValue(s.valve)
+                    .map { State(it, s.path + it) }
+                    .filter { n -> v.none { it.valve == n.valve } }.forEach { n ->
+                        u += n
+                        val distance = d.getValue(s) + 1
+                        d.compute(n) { _, old -> min(distance, old ?: MAX_VALUE) }
+                    }
+                if (s.valve == target) {
+                    add(target to s.path.size + 1)
+                    break
+                }
+                v += s
+                u -= s
+                s = u.minByOrNull { d.getValue(it) } ?: break
             }
-            return acc
         }
-        val rate = open.sumOf { flowRates.getValue(it) }
-        if (acc + rate + (29 - minute) * flowRates.values.sum() < max) {
-            return 0
-        }
-
-        fun nextValves(valve :String, path: List<String>) = tunnels.getValue(valve)
-            .sortedByDescending { flowRates.getValue(it) }
-            .filterNot { it in path }
-
-        val next = buildList {
-            nextValves(valve, path + elephantPath)
-                .map { findMax(it, elephantValve, minute + 1, acc + rate, open, path + it) }.forEach(::add)
-
-            if (flowRates.getValue(valve) > 0 && valve !in open)
-                add(findMax(valve, elephantValve, minute + 1, acc + rate, open + valve))
-        }
-        return next.maxOfOrNull { it } ?: (acc + (30 - minute) * rate)
     }
 
-    return listOf(findMax("AA"), null)
-}
+    data class Helper(val valve: String, val minutesSpent: Int = 1)
+    data class OpenedValve(val valve: String, val openedInMinute: Int) {
+        override fun toString(): String {
+            return "$valve:$openedInMinute"
+        }
+    }
 
+    fun Iterable<OpenedValve>.pressureReleased(deadline: Int) =
+        (1..deadline).sumOf { minute ->
+            filter { it.openedInMinute <= minute }.sumOf { flowRates.getValue(it.valve) }
+        }
+
+    var max = 0
+    fun openValves(deadline: Int, helpers: List<Helper>, openedValves: List<OpenedValve> = listOf()): Int {
+        val pressureReleased = openedValves.pressureReleased(deadline)
+
+        return buildList {
+            for (helper in helpers) {
+                val nextValves = functioningValves.minus(openedValves.map { it.valve }.toSet())
+                val moves = whereToNext(helper.valve, nextValves)
+                    .filter { (_, distance) -> helper.minutesSpent + distance <= deadline }
+
+                moves.forEach { (valve, distance) ->
+                    val newHelper = Helper(valve, helper.minutesSpent + distance)
+                    val openedValve = OpenedValve(valve, helper.minutesSpent + distance)
+                    add(openValves(deadline, helpers - helper + newHelper, openedValves + openedValve))
+                }
+            }
+        }.maxOfOrNull { it } ?: pressureReleased.also {
+            if (it >= max) {
+                max = it
+            }
+        }
+    }
+
+    val part1 = openValves(30, listOf(Helper("AA")))
+    println(part1)
+
+    max = 0
+
+    val part2 = openValves(26, listOf(Helper("AA"), Helper("AA")))
+    println(part2)
+
+    return listOf()
+}
