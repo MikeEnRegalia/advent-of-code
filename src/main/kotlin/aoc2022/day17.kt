4edbcd3efed73b812324ab8d79e5df609d6713c1
Michael Böiers <mikeenregalia@gmail.com>
2022-12-19 19:28:52 +0100

    solved day 17

--- src/main/kotlin/aoc2022/day17.kt
+++ src/main/kotlin/aoc2022/day17.kt
@@ -1,7 +1,5 @@
 package aoc2022
 
-import java.util.*
-
 fun main() {
     val jets = generateSequence(::readlnOrNull).first().split("").filter { it.isNotBlank() }
 
@@ -10,7 +8,7 @@ fun main() {
         operator fun minus(p: Pos) = Pos(x - p.x, y - p.y)
     }
 
-    data class Shape(val points: Set<Pos>, val pos: Pos = Pos(0, 0)) {
+    data class Rock(val points: Set<Pos>, val pos: Pos = Pos(0, 0)) {
         fun minX() = points.minOf { it.x }
         fun maxX() = points.maxOf { it.x }
         fun minY() = points.minOf { it.y }
@@ -19,94 +17,90 @@ fun main() {
         fun left() = at(pos + Pos(-1, 0))
         fun right() = at(pos + Pos(1, 0))
         fun down() = at(pos + Pos(0, -1))
-        fun intersects(shape: Shape) = points.intersect(shape.points).isNotEmpty()
-        fun similar(shape: Shape) = (points.first().y - shape.points.first().y).let { dy ->
+        fun intersects(shape: Rock) = points.intersect(shape.points).isNotEmpty()
+        fun similar(shape: Rock) = (points.first().y - shape.points.first().y).let { dy ->
             points.zip(shape.points).all { (a, b) -> b + Pos(0, dy) == a }
         }
     }
 
-    fun Iterable<Shape>.merge() = flatMap { it.points }.toSet().let { points ->
-        Shape(points, Pos(points.minOf { it.x }, points.minOf { it.y }))
+    fun Iterable<Rock>.merge() = flatMap { it.points }.toSet().let { points ->
+        Rock(points, Pos(points.minOf { it.x }, points.minOf { it.y }))
     }
 
-    val shapes = listOf(
-        Shape(setOf(Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0))),
-        Shape(setOf(Pos(0, 1), Pos(1, 1), Pos(2, 1), Pos(1, 0), Pos(1, 2))),
-        Shape(setOf(Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(2, 1), Pos(2, 2))),
-        Shape(setOf(Pos(0, 0), Pos(0, 1), Pos(0, 2), Pos(0, 3))),
-        Shape(setOf(Pos(0, 0), Pos(1, 0), Pos(0, 1), Pos(1, 1)))
+    val rocks = listOf(
+        Rock(setOf(Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0))),
+        Rock(setOf(Pos(0, 1), Pos(1, 1), Pos(2, 1), Pos(1, 0), Pos(1, 2))),
+        Rock(setOf(Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(2, 1), Pos(2, 2))),
+        Rock(setOf(Pos(0, 0), Pos(0, 1), Pos(0, 2), Pos(0, 3))),
+        Rock(setOf(Pos(0, 0), Pos(1, 0), Pos(0, 1), Pos(1, 1)))
     )
 
-    var shapesTick = 0
-    fun newShape() = shapes[shapesTick].also { shapesTick = (shapesTick + 1) % shapes.size }
+    var rocksTick = 0
+    fun newRock() = rocks[rocksTick].also { rocksTick = (rocksTick + 1) % rocks.size }
 
+    val xRange = 0..6
     var jetsTick = 0
     fun newJet() = jets[jetsTick].also { jetsTick = (jetsTick + 1) % jets.size }
 
-    val stuckShapes = ArrayDeque<Shape>()
+    val stuckRocks = ArrayDeque<Rock>()
+    fun Rock.applyJet() = when (newJet()) {
+        "<" -> if (minX() > xRange.first && stuckRocks.none { it.intersects(left()) }) left() else this
+        else -> if (maxX() < xRange.last && stuckRocks.none { it.intersects(right()) }) right() else this
+    }
 
-    var shape = newShape().at(Pos(2, 4))
-    val xRange = 0..6
-    var nStuckShapes = 0
+    fun Rock.canFall() = down().minY() > 0 && stuckRocks.none { it.intersects(down()) }
 
-    data class State(val jetsTick: Int, val shapesTick: Int, val stuckShapes: Shape)
+    var fallingRock = newRock().at(Pos(2, 4))
+    var nStuckRocks = 0
+
+    data class State(val jetsTick: Int, val shapesTick: Int, val stuckRocks: Rock)
 
     val states = mutableMapOf<Int, State>()
-    val history = mutableMapOf<Pair<Int, Int>, Pair<Int, Iterable<Shape>>>()
-    val heights = mutableMapOf<Int, Int>()
+    val history = mutableMapOf<Pair<Int, Int>, Pair<Int, Iterable<Rock>>>()
 
     var cycleStart: Int? = null
 
     var part1: Int? = null
     var part2: Long? = null
     while (part1 == null || part2 == null) {
-        with(shape) {
-            shape = when (newJet()) {
-                "<" -> if (minX() > xRange.first && stuckShapes.none { it.intersects(left()) }) left() else this
-
-                else -> if (maxX() < xRange.last && stuckShapes.none { it.intersects(right()) }) right() else this
-            }
-        }
-
-        if (shape.down().minY() != 0 && !stuckShapes.any { it.intersects(shape.down()) }) {
-            shape = shape.down()
+        fallingRock = fallingRock.applyJet()
+        if (fallingRock.canFall()) {
+            fallingRock = fallingRock.down()
             continue
         }
 
-        stuckShapes += shape
-        nStuckShapes++
-        states[nStuckShapes] = State(jetsTick, shapesTick, stuckShapes.merge())
+        stuckRocks += fallingRock
+        nStuckRocks++
+        states[nStuckRocks] = State(jetsTick, rocksTick, stuckRocks.merge())
 
-        heights[nStuckShapes] = stuckShapes.maxOf { it.maxY() }
-        if (stuckShapes.size > 40) {
-            stuckShapes.removeFirst()
+        if (stuckRocks.size > 40) {
+            stuckRocks.removeFirst()
 
-            val k = Pair(jetsTick, shapesTick)
+            val k = Pair(jetsTick, rocksTick)
             val prev = history[k]
             if (prev != null) {
-                val prevShape = prev.second.merge()
-                val currShape = stuckShapes.merge()
+                val (prevRocks, prevShapes) = prev
+                val prevShape = prevShapes.merge()
+                val currShape = stuckRocks.merge()
                 if (prevShape.similar(currShape) && cycleStart == null) {
-                    val y1 = prevShape.maxY()
-                    val y2 = currShape.maxY()
-                    val cycleHeight = y2 - y1
-                    val cycleCount = nStuckShapes - prev.first
-                    cycleStart = prev.first
+                    val cycleHeight = currShape.maxY() - prevShape.maxY()
+                    val cycleCount = nStuckRocks - prevRocks
+                    cycleStart = prevRocks
 
                     val fullCycles = (1_000_000_000_000 - cycleStart) / cycleCount
                     val partialCycle = ((1_000_000_000_000 - cycleStart) % cycleCount).toInt()
-                    val remIndex = states[cycleStart + partialCycle]!!.stuckShapes.maxY() - cycleStart
-                    part2 = prev.first + (fullCycles * cycleHeight) + remIndex
+                    val remaining = states.getValue(cycleStart + partialCycle).stuckRocks.maxY() - cycleStart
+                    part2 = prevRocks + (fullCycles * cycleHeight) + remaining
                 }
             }
-            history[k] = nStuckShapes to stuckShapes.toList()
+            history[k] = nStuckRocks to stuckRocks.toList()
         }
 
-        if (nStuckShapes == 2022) {
-            part1 = stuckShapes.maxOf { it.maxY() }
-        }
+        val currentHeight = stuckRocks.maxOf { it.maxY() }
+        if (nStuckRocks == 2022)
+            part1 = currentHeight
 
-        shape = newShape().at(Pos(2, stuckShapes.maxOf { it.maxY() } + 4))
+        fallingRock = newRock().at(Pos(2, currentHeight + 4))
     }
 
     println(part1)
