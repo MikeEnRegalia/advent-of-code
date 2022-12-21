fa9775ec4fc757e2898ea548cb044a30e82e35a2
Michael Böiers <mikeenregalia@gmail.com>
2022-12-21 09:42:51 +0100

    solved day 21

--- src/main/kotlin/aoc2022/day19.kt
+++ src/main/kotlin/aoc2022/day19.kt
@@ -5,56 +5,100 @@ import kotlin.math.min
 fun main() = day19(String(System.`in`.readAllBytes())).forEach(::println)
 
 private fun day19(input: String): List<Any?> {
-    val blueprints = input.lines().map { it.split(" ").mapNotNull(String::toIntOrNull)}
+    data class Configuration(
+        val oreForOreRobot: Int,
+        val oreForClayRobot: Int,
+        val oreForObsidianRobot: Int,
+        val clayForObsidianRobot: Int,
+        val oreForGeodeRobot: Int,
+        val obsidianForGeodeRobot: Int
+    )
 
-    fun calc(bp: List<Int>): Int {
-        val (oreForOre, oreForClay, oreForObs, clayForObs, oreForG) = bp
-        val obsForG = bp[5]
-        var ore = 0
-        var oreRobot = 1
-        var clay = 0
-        var clayRobot = 0
-        var obs = 0
-        var obsRobot = 0
-        var g = 0
-        var gRobot = 0
-        for (minute in 1..24) {
-            ore += oreRobot
-            clay += clayRobot
-            obs += obsRobot
-            g += gRobot
-            val makeG = min(ore/oreForG, obs/obsForG)
-            if (makeG > 0) {
-                gRobot += makeG
-                ore -= makeG * oreForG
-                obs -= makeG * obsForG
-            }
-            val makeObs = min(ore/oreForObs, clay/clayForObs)
-            if (makeObs > 0) {
-                obsRobot += makeObs
-                ore -= makeObs * oreForObs
-                clay -= makeObs * clayForObs
-            }
-            val makeClayRobot = ore/oreForClay
-            if (makeClayRobot > 0) {
-                clayRobot += makeClayRobot
-                ore -= makeClayRobot * oreForClay
-            }
-            val makeOreRobot = ore/oreForOre
-            if (makeOreRobot > 0) {
-                oreRobot += makeOreRobot
-                ore -= makeOreRobot * oreForOre
-            }
-            println("$minute: $oreRobot ($ore) $clayRobot ($clay) $obsRobot ($obs) $gRobot ($g)")
+    val blueprints = input.lines().map { line ->
+        line.split(" ").mapNotNull(String::toIntOrNull).let {
+            Configuration(it[0], it[1], it[2], it[3], it[4], it[5])
         }
-        return g
     }
 
-    calc(listOf(4, 2, 3, 14, 2, 7)).also(::println)
+    data class State(
+        val cfg: Configuration,
+        val minute: Int = 0,
+        val ore: Int = 0,
+        val clay: Int = 0,
+        val obsidian: Int = 0,
+        val geode: Int = 0,
+        val oreRobots: Int = 1,
+        val clayRobots: Int = 0,
+        val obsidianRobots: Int = 0,
+        val geodeRobots: Int = 0
+    ) {
+        override fun toString(): String {
+            return "$minute: $ore $clay $obsidian $geode"
+        }
+        fun maxNewOreRobot(ore: Int) = ore / cfg.oreForOreRobot
+        fun maxNewClayRobot(ore: Int) = ore / cfg.oreForClayRobot
+        fun maxNewObsidianRobot(ore: Int, clay: Int) = min(ore / cfg.oreForObsidianRobot, clay / cfg.clayForObsidianRobot)
+        fun maxNewGeodeRobot(ore: Int, obsidian: Int) = min(ore / cfg.oreForGeodeRobot, obsidian / cfg.obsidianForGeodeRobot)
 
-    val stats = mutableMapOf<Int, Int>()
-    for ((bpi, bp) in blueprints.withIndex()) stats[bpi] = calc(bp)
+        fun next() = buildList {
+            var ore = this@State.ore
+            var clay = this@State.clay
+            var obsidian = this@State.obsidian
+            val geode = this@State.geode
+            val newGeodeRobots = maxNewGeodeRobot(ore, obsidian)
+            ore -= newGeodeRobots * cfg.oreForGeodeRobot
+            obsidian -= newGeodeRobots * cfg.obsidianForGeodeRobot
+            val needMoreObsidian = obsidianRobots/oreRobots < cfg.obsidianForGeodeRobot/cfg.oreForGeodeRobot
+            val newObsidianRobots = if (needMoreObsidian) maxNewObsidianRobot(ore, clay) else 0
+            ore -= newObsidianRobots * cfg.oreForObsidianRobot
+            clay -= newObsidianRobots * cfg.clayForObsidianRobot
+            val needMoreClayRobots = clayRobots/oreRobots < cfg.clayForObsidianRobot/cfg.oreForObsidianRobot
+            val newClayRobots = if (needMoreClayRobots) maxNewClayRobot(ore) else 0
+            ore -= newClayRobots * cfg.oreForClayRobot
+            val newOreRobots = maxNewOreRobot(ore)
+            ore -= newOreRobots * cfg.oreForOreRobot
+            add(copy(
+                minute = minute + 1,
+                ore = ore + oreRobots,
+                clay = clay + clayRobots,
+                obsidian = obsidian + obsidianRobots,
+                geode = geode + geodeRobots,
+                oreRobots = oreRobots + newOreRobots,
+                clayRobots = clayRobots + newClayRobots,
+                obsidianRobots = obsidianRobots + newObsidianRobots,
+                geodeRobots = geodeRobots + newGeodeRobots))
+        }
 
-    return listOf(stats.entries.sumOf { (it.key + 1) * it.value }, null)
+    }
+
+    fun simulate(s: State, cache: MutableMap<State, Int> = mutableMapOf()): Int? {
+        println(s)
+        if (s.minute == 24) {
+            return s.geode
+        }
+        val cached = cache[s]
+        if (cached != null) {
+            //println("cached")
+            return cached
+        }
+        return s.next().maxOfOrNull { s2 ->simulate(s2, cache)?.also { cache[s2] = it } ?: 0 }
+    }
+
+    println("x: " + simulate(State(Configuration(4, 2, 3, 14, 2, 7))))
+    println("y: " + simulate(State(Configuration(2, 3, 3, 8, 3, 12))))
+
+    fun part1(): Int {
+        val stats = mutableMapOf<Int, Int>()
+        for ((index, cfg) in blueprints.withIndex()) {
+            val s = State(cfg)
+            val max = simulate(s) ?: 0
+            println(max)
+            stats[index] = max
+        }
+        return stats.entries.sumOf { (it.key + 1) * it.value }
+    }
+
+
+    return listOf(null, null)
 }
 
---
+++ src/main/kotlin/aoc2022/day21.kt
@@ -0,0 +1,62 @@
+package aoc2022
+
+fun main() = day21(String(System.`in`.readAllBytes())).forEach(::println)
+
+private fun day21(input: String): List<Any?> {
+    val monkeys = input.lines().map { it.split(": ").map { it.split(" ") } }.associate { it[0].first() to it[1] }
+
+    fun lookup(name: String, part2: Boolean = false): Long? {
+        if (part2 && name == "humn") return null
+        val number = name.toLongOrNull()
+        if (number != null) return number
+
+        val op = monkeys.getValue(name)
+        if (op.size == 1) return op.first().toLong()
+
+        val a = lookup(op.first(), part2)
+        val o = op[1]
+        val b = lookup(op.last(), part2)
+        if (a == null || b == null) return null
+        return when (o) {
+            "+" -> a + b
+            "*" -> a * b
+            "-" -> a - b
+            "/" -> a / b
+            else -> throw IllegalArgumentException(o)
+        }
+    }
+
+    fun part2(): Long {
+        var curr = "root"
+        var currVal = 0L
+        while (true) {
+            if (curr == "humn") return currVal
+            val (m1, origOp, m2) = monkeys.getValue(curr)
+            val v1 = lookup(m1, part2 = true)
+            val v2 = lookup(m2, part2 = true)
+            val op = if (curr == "root") "=" else origOp
+            if (v1 == null && v2 != null) {
+                curr = m1
+                currVal = when (op) {
+                    "=" -> v2
+                    "+" -> currVal - v2
+                    "-" -> currVal + v2
+                    "*" -> currVal / v2
+                    else -> currVal * v2
+                }
+            }
+            if (v2 == null && v1 != null) {
+                curr = m2
+                currVal = when (op) {
+                    "=" -> v1
+                    "+" -> currVal - v1
+                    "-" -> v1 - currVal
+                    "*" -> currVal / v1
+                    else -> v1 / currVal
+                }
+            }
+        }
+    }
+    return listOf(lookup("root"), part2())
+}
+
