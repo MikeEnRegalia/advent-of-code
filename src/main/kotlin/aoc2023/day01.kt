3bc935c1db5e52f57515375d864ab59d0efbcb13
Michael Böiers <mikeenregalia@gmail.com>
2023-12-01 23:30:40 +0100

    refactor

--- src/main/kotlin/aoc2023/day01.kt
+++ src/main/kotlin/aoc2023/day01.kt
@@ -4,21 +4,18 @@ fun main() = day01(String(System.`in`.readAllBytes()).split("\n")).forEach(::pri
 
 private fun day01(lines: List<String>): List<Any?> {
     fun String.part1() = mapNotNull(Char::digitToIntOrNull)
-    fun String.part2() = listOf(
-        digits.getValue(digits.keys.minBy { indexOfOrNull(it) ?: Int.MAX_VALUE }),
-        digits.getValue(digits.keys.maxBy { lastIndexOf(it) })
-    )
+    fun String.part2() = with(digits) {
+        listOf(keys.minBy { indexOfOrMax(it) }, keys.maxBy { lastIndexOf(it) }).map { getValue(it) }
+    }
 
-    fun List<Int>.firstLastDigits() = first() * 10 + last()
+    fun List<String>.sumOfCalibrationValues(toDigits: (String) -> List<Int>) =
+        sumOf { line -> toDigits(line).let { it.first() * 10 + it.last() } }
 
-    return listOf(
-        lines.sumOf { it.part1().firstLastDigits() },
-        lines.sumOf { it.part2().firstLastDigits() }
-    )
+    return listOf(String::part1, String::part2).map { lines.sumOfCalibrationValues(it) }
 }
 
 private val digits = sequenceOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
     .flatMapIndexed { i, s -> (i + 1).let { sequenceOf(s to it, it.toString() to it) } }
     .toMap()
 
-fun String.indexOfOrNull(s: String) = indexOf(s).takeIf { it >= 0 }
+fun String.indexOfOrMax(s: String) = indexOf(s).takeIf { it >= 0 } ?: Int.MAX_VALUE
