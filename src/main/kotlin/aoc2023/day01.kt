af49803e0bd5b2ef4f4bc356ece119cbabaa635d
Michael Böiers <mikeenregalia@gmail.com>
2023-12-01 13:16:31 +0100

    extract functions

--- src/main/kotlin/aoc2023/day01.kt
+++ src/main/kotlin/aoc2023/day01.kt
@@ -3,22 +3,22 @@ package aoc2023
 fun main() = day01(String(System.`in`.readAllBytes()).split("\n")).forEach(::println)
 
 private fun day01(lines: List<String>): List<Any?> {
+    fun String.part1() = mapNotNull(Char::digitToIntOrNull)
+    fun String.part2() = listOf(
+        digits.getValue(digits.keys.minBy { indexOfOrNull(it) ?: Int.MAX_VALUE }),
+        digits.getValue(digits.keys.maxBy { lastIndexOf(it) })
+    )
 
-    val part1 = lines.sumOf { line ->
-        line.mapNotNull(Char::digitToIntOrNull).let { it.first() * 10 + it.last() }
-    }
+    fun List<Int>.firstLastDigits() = first() * 10 + last()
 
-    val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-        .flatMapIndexed { i, s -> (i + 1).let { listOf(s to it, it.toString() to it) } }
-        .toMap()
-
-    val part2 = lines.sumOf { line ->
-        val first = digits.getValue(digits.keys.minBy { line.indexOfOrNull(it) ?: Int.MAX_VALUE })
-        val last = digits.getValue(digits.keys.maxBy { line.lastIndexOf(it) })
-        first * 10 + last
-    }
-
-    return listOf(part1, part2)
+    return listOf(
+        lines.sumOf { it.part1().firstLastDigits() },
+        lines.sumOf { it.part2().firstLastDigits() }
+    )
 }
+
+private val digits = sequenceOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    .flatMapIndexed { i, s -> (i + 1).let { sequenceOf(s to it, it.toString() to it) } }
+    .toMap()
 
 fun String.indexOfOrNull(s: String) = indexOf(s).takeIf { it >= 0 }
