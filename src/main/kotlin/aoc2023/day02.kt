8f480aa6f29506dcc2432617e287e78723a85c09
Michael Böiers <mikeenregalia@gmail.com>
2023-12-02 07:07:45 +0100

    refactor

--- src/main/kotlin/aoc2023/day02.kt
+++ src/main/kotlin/aoc2023/day02.kt
@@ -6,17 +6,16 @@ private fun day02(lines: List<String>): List<Any?> {
 
     val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
-    val sum = lines.map { line ->
+    val games = lines.map { line ->
         val game = line.split(" ")[1].replace(":", "").toInt()
-        val maxValues = mutableMapOf<String, Int>()
-        line.substring(line.indexOf(":") + 2).split(";").map { part ->
-            part.split(",").map { it.trim() }.forEach {
+        val maxValues = line.substring(line.indexOf(":") + 2).split(";").flatMap { part ->
+            part.split(",").map { it.trim() }.map {
                 val (nS, color) = it.split(" ")
                 val n = nS.toInt()
-                val curr = maxValues[color]
-                if (curr == null || curr < n) maxValues[color] = n
+                color to n
             }
-        }
+        }.groupBy { it.first }.mapValues { (k, v) -> v.maxOf { it.second } }
+
         val part1 = game.takeUnless {
             limits.any { (color, maxAllowed) ->
                 val maxEncountered = maxValues[color]
@@ -27,5 +26,5 @@ private fun day02(lines: List<String>): List<Any?> {
         part1 to part2
     }
 
-    return listOf(sum.sumOf { it.first }, sum.sumOf { it.second })
+    return listOf(games.sumOf { it.first }, games.sumOf { it.second })
 }
