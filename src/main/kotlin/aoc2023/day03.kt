41693ada02c6d5044592753a6873465478f5eef3
Michael Riess <mlriess@gmx.de>
2023-12-03 23:34:32 +0100

    refactor

--- src/main/kotlin/aoc2023/day03.kt
+++ src/main/kotlin/aoc2023/day03.kt
@@ -1,61 +1,51 @@
 package aoc2023
 
-fun main() = day03(String(System.`in`.readAllBytes()).split("\n")).forEach(::println)
+fun main() = day03(System.`in`.bufferedReader().lines().toList()).forEach(::println)
 
 private fun day03(lines: List<String>): List<Any?> {
     data class Pos(val x: Int, val y: Int)
 
     fun Pos.neighbors() = (-1..1).asSequence()
-        .flatMap { y -> (-1..1).map { x -> Pos(x, y) } }
-        .map { Pos(x + it.x, y + it.y) }
+        .flatMap { y -> (-1..1).map { x -> Pos(this.x + x, this.y + y) } }
         .filterNot { it.x == x && it.y == y }
-        .filter { it.x in (lines.first().indices) }
-        .filter { it.y in lines.indices }.toList()
+        .filter { it.y in lines.indices && it.x in lines[y].indices }
+        .toList()
 
 
-    var sum = 0L
-
+    var part1 = 0L
     val numbers = mutableMapOf<Pos, Long>()
+    val gears = mutableMapOf<Pos, Set<Pos>>()
 
-    val adjacentGears = mutableMapOf<Pos, Set<Pos>>()
-
-    for (y in lines.indices) {
-        val row = lines[y]
-
+    for ((y, row) in lines.withIndex()) {
         var x = 0
         while (x in row.indices) {
-            if (row[x].isDigit()) {
-                val n = row.drop(x).takeWhile { it.isDigit() }
-                val symbols = n.indices.flatMap {
-                    Pos(x + it, y).neighbors()
-                }
+            if (!row[x].isDigit()) x++
+            else {
+                val n = row.drop(x).takeWhile(Char::isDigit)
+                val neighbors = n.indices.flatMap { Pos(x + it, y).neighbors() }
 
-                val adjacentSymbols = symbols.filter { neighbor ->
+                val symbols = neighbors.filter { neighbor ->
                     val c = lines[neighbor.y][neighbor.x]
                     !(c.isDigit() || c == '.')
                 }
 
-                if (adjacentSymbols.isNotEmpty()) {
-                    sum += n.toLong()
+                if (symbols.isNotEmpty()) {
+                    part1 += n.toLong()
 
-                    adjacentSymbols.forEach {
-                        if (lines[it.y][it.x] == '*') {
-                            numbers[Pos(x, y)] = n.toLong()
-                            adjacentGears.compute(it) { _, old ->
-                                (old ?: setOf()) + Pos(x, y)
-                            }
-                        }
+                    symbols.filter { lines[it.y][it.x] == '*' }.forEach {
+                        numbers[Pos(x, y)] = n.toLong()
+                        gears.compute(it) { _, old -> (old ?: setOf()) + Pos(x, y) }
                     }
                 }
 
                 x += n.length
-            } else x++
+            }
         }
     }
 
-    println(adjacentGears)
+    val part2 = gears
+        .filterValues { it.size == 2 }
+        .values.sumOf { it.map(numbers::getValue).reduce(Long::times) }
 
-    val part2 = adjacentGears.filterValues { it.size == 2 }
-        .values.sumOf { it.map { numbers.getValue(it) }.reduce(Long::times) }
-    return listOf(sum, part2)
+    return listOf(part1, part2)
 }
