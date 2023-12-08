c52bff2ace5468e2798f727fbf5d5a6103a2f905
Michael Böiers <mikeenregalia@gmail.com>
2023-12-09 00:05:50 +0100

    prepare day 9

---
+++ src/main/kotlin/aoc2023/day09.kt
@@ -0,0 +1,9 @@
+package aoc2023
+
+import aoc2018.day09.day09
+
+fun main() = day09(generateSequence(::readlnOrNull).toList()).forEach(::println)
+
+private fun day09(lines: List<String>): List<Any?> {
+    return listOf()
+}
