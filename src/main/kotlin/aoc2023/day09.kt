d895d8e60f91433e11a60b3229cb056e30cfd845
Michael Böiers <mikeenregalia@gmail.com>
2023-12-09 00:06:00 +0100

    prepare day 9

--- src/main/kotlin/aoc2023/day09.kt
+++ src/main/kotlin/aoc2023/day09.kt
@@ -1,7 +1,5 @@
 package aoc2023
 
-import aoc2018.day09.day09
-
 fun main() = day09(generateSequence(::readlnOrNull).toList()).forEach(::println)
 
 private fun day09(lines: List<String>): List<Any?> {
