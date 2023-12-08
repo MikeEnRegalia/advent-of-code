2540794f997dea08724e6eb5ebc112953b635e56
Michael Böiers <mikeenregalia@gmail.com>
2023-12-08 07:30:15 +0100

    solve day 8 part 2

--- src/main/kotlin/aoc2023/day08.kt
+++ src/main/kotlin/aoc2023/day08.kt
@@ -13,12 +13,34 @@ private fun day08(lines: List<String>): List<Any?> {
         }
     }.toMap()
 
-    var curr = "AAA"
-    var n = 0
-    while (curr != "ZZZ") {
-        val move = moves[n % moves.length]
-        curr = dirs.getValue(curr).let { if (move == 'L') it.left else it.right }
-        n++
+    fun part1(start: String, predicate: (String) -> Boolean): Int {
+        var curr = start
+        var n = 0
+        while (!predicate(curr)) {
+            val move = moves[n % moves.length]
+            curr = dirs.getValue(curr).let { if (move == 'L') it.left else it.right }
+            n++
+        }
+        return n
     }
-    return listOf(n)
+
+    return listOf(
+        part1("AAA") { it == "ZZZ" },
+        dirs.keys.filter { it.endsWith("A") }
+            .map { part1(it) { it.endsWith("Z") } }
+            .map(Int::toLong)
+            .reduce(::findLCM))
+}
+
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
 }
