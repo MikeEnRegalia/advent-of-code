22a61eadd43f8c7f465738e3496503b70a30aff2
Michael Böiers <mikeenregalia@gmail.com>
2023-12-10 07:57:09 +0100

    solve day 9

--- src/main/kotlin/aoc2023/day09.kt
+++ src/main/kotlin/aoc2023/day09.kt
@@ -3,5 +3,21 @@ package aoc2023
 fun main() = day09(generateSequence(::readlnOrNull).toList()).forEach(::println)
 
 private fun day09(lines: List<String>): List<Any?> {
-    return listOf()
+    fun String.predict(): Pair<Int, Int> {
+        val data = mutableListOf<MutableList<Int>>()
+        data += split(" ").map(String::toInt).toMutableList()
+        while (!data.last().all { it == 0 }) {
+            data += data.last().windowed(2).map { it[1] - it[0] }.toMutableList()
+        }
+        data.indices.reversed().forEach { i ->
+            data[i] += if (i == data.indices.last) 0 else data[i][data[i].size - 1] + data[i + 1].last()
+        }
+        data.indices.reversed().forEach { i ->
+            data[i].add(0, if (i == data.indices.last) 0 else data[i][0] - data[i + 1].first())
+        }
+        return data.first().first() to data.first().last()
+    }
+
+    val data = lines.map { it.predict() }
+    return listOf(data.sumOf { it.second }, data.sumOf { it.first })
 }
