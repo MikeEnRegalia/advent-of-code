9a2596e30145a794a79137ac797511f7c5cefc1d
Michael Böiers <mikeenregalia@gmail.com>
2023-12-11 10:10:56 +0100

    refactor

--- src/main/kotlin/aoc2023/day10.kt
+++ src/main/kotlin/aoc2023/day10.kt
@@ -4,29 +4,34 @@ import kotlin.math.min
 
 fun main() = day10(generateSequence(::readlnOrNull).toList()).forEach(::println)
 
+private const val fromWest = "7-J"
+private const val fromEast = "F-L"
+private const val fromNorth = "L|J"
+private const val fromSouth = "F|7"
+
 private fun day10(lines: List<String>): List<Any?> {
     data class Pos(val x: Int, val y: Int) {
+        val inGrid by lazy { x in lines[0].indices && y in lines.indices }
+        val atGridBorder by lazy { x == 0 || y == 0 || x == lines[0].indices.last || y == lines.indices.last }
+
         val c: Char by lazy {
             when {
                 !inGrid -> 'X'
                 else -> lines[y][x].takeIf { it != 'S' } ?: when {
-                    copy(x = x + 1).c in fromWest && copy(x = x - 1).c in fromEast -> '-'
-                    copy(y = y + 1).c in fromNorth && copy(y = y - 1).c in fromSouth -> '|'
-                    copy(x = x + 1).c in fromWest && copy(y = y + 1).c in fromSouth -> 'F'
-                    copy(x = x + 1).c in fromWest && copy(y = y - 1).c in fromNorth -> 'L'
-                    copy(x = x - 1).c in fromEast && copy(y = y + 1).c in fromSouth -> '7'
-                    copy(x = x - 1).c in fromEast && copy(y = y - 1).c in fromNorth -> 'J'
+                    d(dx = 1).c in fromWest && d(dx = -1).c in fromEast -> '-'
+                    d(dy = 1).c in fromNorth && d(dy = -1).c in fromSouth -> '|'
+                    d(dx = 1).c in fromWest && d(dy = 1).c in fromSouth -> 'F'
+                    d(dx = 1).c in fromWest && d(dy = -1).c in fromNorth -> 'L'
+                    d(dx = -1).c in fromEast && d(dy = 1).c in fromSouth -> '7'
+                    d(dx = -1).c in fromEast && d(dy = -1).c in fromNorth -> 'J'
                     else -> '?'
                 }
             }
         }
-        private val fromWest = "7-J"
-        private val fromEast = "F-L"
-        private val fromNorth = "L|J"
-        private val fromSouth = "F|7"
-        val inGrid = x in lines[0].indices && y in lines.indices
-        fun neighbors() = listOf(copy(x = x + 1), copy(x = x - 1), copy(y = y + 1), copy(y = y - 1))
-            .filter { it.inGrid }
+
+        fun d(dx: Int = 0, dy: Int = 0) = copy(x = x + dx, y = y + dy)
+
+        fun neighbors() = sequenceOf(d(dx = 1), d(dx = -1), d(dy = 1), d(dy = -1)).filter { it.inGrid }
 
         fun pipeNeighbors() = neighbors()
             .filter {
@@ -47,6 +52,7 @@ private fun day10(lines: List<String>): List<Any?> {
 
                     c == 'L' && dx == 1 -> it.c in fromWest
                     c == 'L' && dy == -1 -> it.c in fromSouth
+
                     else -> false
                 }
             }
@@ -65,15 +71,14 @@ private fun day10(lines: List<String>): List<Any?> {
         }
         V += curr
         val remaining = D.keys - V
-        curr = remaining.toList().minByOrNull { D.getValue(it) } ?: break
+        curr = remaining.minByOrNull { D.getValue(it) } ?: break
     }
 
     val part1 = D.values.max()
 
     val path = mutableListOf(start)
-    while (true) {
-        path += path.last().pipeNeighbors().filter { it !in path }.randomOrNull() ?: break
-    }
+    while (true)
+        path += path.last().pipeNeighbors().filter { it !in path }.firstOrNull() ?: break
 
     fun Pos.isTile() = c != '?' && this !in path
 
@@ -83,43 +88,45 @@ private fun day10(lines: List<String>): List<Any?> {
         val prev = if (i == 0) path.last() else path[i - 1]
         val next = if (i == path.size - 1) path.first() else path[i + 1]
         val dx = next.x - prev.x
-        val dy = next.y - prev.y
 
-        fun MutableSet<Pos>.addIfTile(p: Pos, dx: Int = 0, dy: Int = 0) = p.copy(x = p.x + dx, y = p.y + dy)
+        fun MutableSet<Pos>.addIfTile(dx: Int = 0, dy: Int = 0) = p.d(dx, dy)
             .let { if (it.isTile()) add(it) else null }
+
+        val rightIfEastward = if (dx > 0) rightTiles else leftTiles
+        val leftIfEastward = if (dx > 0) leftTiles else rightTiles
 
         when (p.c) {
             'F' -> {
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = 1, dy = 1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = 0, dy = -1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = -1, dy = -1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = -1, dy = 0)
+                rightIfEastward.addIfTile(dx = 1, dy = 1)
+                leftIfEastward.addIfTile(dx = 0, dy = -1)
+                leftIfEastward.addIfTile(dx = -1, dy = -1)
+                leftIfEastward.addIfTile(dx = -1, dy = 0)
             }
 
             '7' -> {
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = -1, dy = 1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = 0, dy = -1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = 1, dy = -1)
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = 1, dy = 0)
+                rightIfEastward.addIfTile(dx = -1, dy = 1)
+                leftIfEastward.addIfTile(dx = 0, dy = -1)
+                leftIfEastward.addIfTile(dx = 1, dy = -1)
+                leftIfEastward.addIfTile(dx = 1, dy = 0)
             }
 
             'J' -> {
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = -1, dy = -1)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = 1, dy = 0)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = 1, dy = 1)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = 0, dy = 1)
+                leftIfEastward.addIfTile(dx = -1, dy = -1)
+                rightIfEastward.addIfTile(dx = 1, dy = 0)
+                rightIfEastward.addIfTile(dx = 1, dy = 1)
+                rightIfEastward.addIfTile(dx = 0, dy = 1)
             }
 
             'L' -> {
-                (if (dx > 0) leftTiles else rightTiles).addIfTile(p, dx = 1, dy = -1)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = 0, dy = 1)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = -1, dy = 1)
-                (if (dx > 0) rightTiles else leftTiles).addIfTile(p, dx = -1, dy = 0)
+                leftIfEastward.addIfTile(dx = 1, dy = -1)
+                rightIfEastward.addIfTile(dx = 0, dy = 1)
+                rightIfEastward.addIfTile(dx = -1, dy = 1)
+                rightIfEastward.addIfTile(dx = -1, dy = 0)
             }
         }
     }
 
-    fun Set<Pos>.expand(): Set<Pos> {
+    fun Set<Pos>.exploreTiles(): Set<Pos> {
         val toFollow = toMutableList()
         val tiles = toMutableSet()
         while (toFollow.isNotEmpty()) {
@@ -134,12 +141,8 @@ private fun day10(lines: List<String>): List<Any?> {
     }
 
     val tiles = listOf(leftTiles, rightTiles)
-        .map { it.expand() }
-        .singleOrNull { tiles ->
-            tiles.none { it.x == 0 || it.y == 0 || it.x == lines[0].indices.last || it.y == lines.indices.last }
-        }
-
-
+        .map(MutableSet<Pos>::exploreTiles)
+        .singleOrNull { it.none(Pos::atGridBorder) }
 
     return listOf(part1, tiles?.size)
 }
