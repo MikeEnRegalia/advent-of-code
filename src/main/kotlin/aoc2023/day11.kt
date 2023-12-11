81fdbf7f3f40e4830fb6edc85102413f3cd65e3d
Michael Böiers <mikeenregalia@gmail.com>
2023-12-11 14:35:50 +0100

    refactor

--- src/main/kotlin/aoc2023/day11.kt
+++ src/main/kotlin/aoc2023/day11.kt
@@ -1,39 +1,36 @@
 package aoc2023
 
-fun main() = day11(generateSequence(::readlnOrNull).toList()).forEach(::println)
-
-private fun day11(space: List<String>): List<Any?> {
+fun main() {
+    val space = generateSequence(::readlnOrNull).toList()
 
     data class Galaxy(val x: Int, val y: Int)
-
-    val allGalaxies = space.flatMapIndexed { y, line ->
-        line.mapIndexedNotNull { x, c -> if (c == '#') Galaxy(x, y) else null }
-    }.toSet()
 
     val doubleRows = space.indices.filter { y -> space[y].all { it == '.' } }
     val doubleColumns = space[0].indices.filter { x -> space.map { it[x] }.all { it == '.' } }
 
+    fun List<String>.findGalaxies() = flatMapIndexed { y, line ->
+        line.mapIndexedNotNull { x, c -> if (c == '#') Galaxy(x, y) else null }
+    }
+
     var part1 = 0L
     var part2 = 0L
 
-    (allGalaxies * allGalaxies).forEach { pair ->
+    space.findGalaxies().let { it * it }.forEach { pair ->
         val rx = pair.map(Galaxy::x).sorted().let { (a, b) -> a until b }
         val ry = pair.map(Galaxy::y).sorted().let { (a, b) -> a until b }
 
-        val dx = doubleColumns.count { it in rx }
-        val dy = doubleRows.count { it in ry }
+        val doubles = doubleRows.count { it in ry } + doubleColumns.count { it in rx }
 
         val dist = rx.size() + ry.size()
 
-        part1 += dist + dx + dy
-        part2 += dist + 999999 * (dx + dy)
-
+        part1 += dist + doubles
+        part2 += dist + 999999 * doubles
     }
 
-    return listOf(part1, part2)
+    listOf(part1, part2).forEach(::println)
 }
 
-fun IntRange.size() = last - first + 1L
+fun IntRange.size() = last - first + 1
 
-operator fun <T> Set<T>.times(set: Set<T>): Set<Set<T>> =
+operator fun <T> Collection<T>.times(set: Collection<T>): Set<Set<T>> =
     asSequence().flatMap { a -> mapNotNull { b -> if (a == b) null else setOf(a, b) } }.toSet()
