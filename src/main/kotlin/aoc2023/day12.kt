3299169cc23c3b6989d3522fcb6d94db2d9ac9a8
Michael Böiers <mikeenregalia@gmail.com>
2023-12-12 16:35:27 +0100

    solve day 12

--- src/main/kotlin/aoc2023/day12.kt
+++ src/main/kotlin/aoc2023/day12.kt
@@ -11,19 +11,20 @@ fun main() {
         Record(Array(5) { pattern }.joinToString("?"), Array(5) { numbers }.reduce { a, b -> a + b })
     }
 
-    fun String.completed():Pair<String, List<Int>> = "${this}.".indexOf("#.")
+    fun String.completed(): Pair<String, List<Int>> = "${this}.".indexOf("#.")
         .takeIf { it > -1 && (indexOf("?") == -1 || it < indexOf("?")) }?.let { substring(0, it + 1) }
-            ?.let { s ->
-                s to s.split(".")
-                    .filter { it.isNotEmpty() }
-                    .map { it.length }
-            } ?: ("" to listOf())
+        ?.let { s ->
+            s to s.split(".")
+                .filter { it.isNotEmpty() }
+                .map { it.length }
+        } ?: ("" to listOf())
 
     fun String.supports(numbers: List<Int>, all: Boolean) = split(".")
         .filter(String::isNotEmpty)
         .map { it.length }
         .let { it == if (all) numbers else numbers.take(it.size) }
 
+    val cache = mutableMapOf<Pair<String, List<Int>>, Long>()
     fun String.solve(numbers: List<Int>): Long {
         val indexOfQ = indexOf("?").takeIf { it > -1 } ?: return if (supports(numbers, all = true)) 1L else 0L
         val modified = listOf(".", "#").map { substring(0, indexOfQ) + it + substring(indexOfQ + 1) }
@@ -31,12 +32,16 @@ fun main() {
             val (completedString, completedNumbers) = it.completed()
             //println("considering $it: completed $completedString ($completedNumbers)")
             if (completedNumbers != numbers.take(completedNumbers.size)) 0
-            else it.drop(completedString.length).solve(numbers.drop(completedNumbers.size))
+            else {
+                val nextString = it.drop(completedString.length)
+                val nextNumbers = numbers.drop(completedNumbers.size)
+                cache[nextString to nextNumbers]
+                    ?: nextString.solve(nextNumbers).also { cache[nextString to nextNumbers] = it }
+            }
         }
     }
 
     fun List<Record>.solve() = sumOf { (patternWithPlaceholders, numbers) ->
-        println("$patternWithPlaceholders: $numbers")
         patternWithPlaceholders.solve(numbers)
     }
 
