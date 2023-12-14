5387b4662459be4697622047a7e6379dda06dd87
Michael Böiers <mikeenregalia@gmail.com>
2023-12-14 11:45:52 +0100

    solve day 14

--- src/main/kotlin/aoc2023/day14.kt
+++ src/main/kotlin/aoc2023/day14.kt
@@ -3,20 +3,12 @@ package aoc2023
 fun main() {
     data class Pos(val x: Int, val y: Int)
 
-    val data = generateSequence(::readLine).flatMapIndexed { y, l ->
+    var data = generateSequence(::readLine).flatMapIndexed { y, l ->
         l.mapIndexedNotNull { x, c -> if (c == '.') null else Pos(x, y) to c }
     }.toMap().toMutableMap()
 
     val width = data.keys.maxOf { it.x } + 1
     val height = data.keys.maxOf { it.y } + 1
-
-    fun Map<Pos, Char>.asString() = buildString {
-        for (y in 0 until height) {
-            for (x in 0 until width) append(this@asString[Pos(x, y)] ?: '.')
-            append("\n")
-        }
-        append("\n")
-    }
 
     fun rollNorth() {
         for (y in 0 until height) {
@@ -33,74 +25,33 @@ fun main() {
         }
     }
 
-    fun rollSouth() {
-        for (y in (0 until height).reversed()) {
-            for (x in 0 until width) {
-                val c = data[Pos(x, y)]
-                if (c == 'O') {
-                    val newY = (y + 1 until height).takeWhile { data[Pos(x, it)] == null }.lastOrNull()
-                    if (newY != null) {
-                        data -= Pos(x, y)
-                        data[Pos(x, newY)] = 'O'
-                    }
-                }
-            }
-        }
-    }
-
-    fun rollWest() {
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                val c = data[Pos(x, y)]
-                if (c == 'O') {
-                    val newX = (0 until x).reversed().takeWhile { data[Pos(it, y)] == null }.lastOrNull()
-                    if (newX != null) {
-                        data -= Pos(x, y)
-                        data[Pos(newX, y)] = 'O'
-                    }
-                }
-            }
-        }
-    }
-
-    fun rollEast() {
-        for (y in 0 until height) {
-            for (x in (0 until width).reversed()) {
-                val c = data[Pos(x, y)]
-                if (c == 'O') {
-                    val newX = (x + 1 until width).takeWhile { data[Pos(it, y)] == null }.lastOrNull()
-                    if (newX != null) {
-                        data -= Pos(x, y)
-                        data[Pos(newX, y)] = 'O'
-                    }
-                }
-            }
-        }
+    fun rotate() {
+        data = data.mapKeys { (k, _) -> Pos(height - 1 - k.y, k.x) } as MutableMap
     }
 
     fun Map<Pos, Char>.summarize() = filterValues { it == 'O' }.keys.sumOf { height - it.y }
 
-    val history = mutableListOf<Pair<String, Int>>()
+    val history = mutableListOf<Pair<Any, Int>>()
 
     while (true) {
         rollNorth()
-
         if (history.isEmpty()) println(data.summarize())
+        rotate()
 
-        rollWest()
-        rollSouth()
-        rollEast()
+        repeat(3) {
+            rollNorth()
+            rotate()
+        }
 
-        val toCache = data.asString() to data.summarize()
-        if (toCache in history) {
+        val toCache = data.entries.toSet() to data.summarize()
+        if (toCache !in history) history.add(toCache) else {
             val cycleStart = history.indexOf(toCache)
-            var pos = -1
-            for (i in 1..1_000_000_000) {
-                pos = if (pos == history.indices.last) cycleStart else (pos + 1)
+            var pos = 0
+            for (i in 0..<1_000_000_000 - 1) {
+                pos = if (pos == history.indices.last) cycleStart else pos + 1
             }
             println(history[pos].second)
             return
         }
-        history.add(toCache)
     }
 }
