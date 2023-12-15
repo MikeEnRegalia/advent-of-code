5da8ecbabe3657e063b0dd9a71d576d276e5eeec
Michael Böiers <mikeenregalia@gmail.com>
2023-12-15 14:19:24 +0100

    refactor

--- src/main/kotlin/aoc2023/day15.kt
+++ src/main/kotlin/aoc2023/day15.kt
@@ -1,51 +1,33 @@
 package aoc2023
 
 fun main() {
+    val input = generateSequence(::readLine).single().split(",")
 
     fun String.hash() = fold(0) { acc, c -> (17 * (acc + c.code)) % 256 }
+    println(input.sumOf(String::hash))
 
-    val input = generateSequence(::readLine)
-        .single()
-        .split(",")
+    data class Lens(val label: String, val focalLength: Int)
 
-    val part1 = input.sumOf(String::hash)
-
-    println(part1)
-
-    data class Lens(val label: String, val focalLength: Int) {
-        override fun toString() = "[$label, $focalLength]"
-    }
-
-    val boxes = Array(256) { ArrayDeque<Lens>() }
-
+    val boxes = Array(256) { mutableListOf<Lens>() }
     input.forEach { s ->
         when {
             "=" in s -> {
-                val (label, focalLength) = s.split("=")
-                val lens = Lens(label, focalLength.toInt())
-                val boxIndex = label.hash()
-                val box = boxes[boxIndex]
-                val index = box.indexOfFirst { it.label == label }
-                if (index == -1) box.add(lens)
-                else box[index] = lens
+                val lens = s.split("=").let { Lens(it[0], it[1].toInt()) }
+                val box = boxes[lens.label.hash()]
+                when (val index = box.indexOfFirst { it.label == lens.label }) {
+                    -1 -> box.add(lens)
+                    else -> box[index] = lens
+                }
             }
 
             else -> {
                 val label = s.split("-").first()
-                val boxIndex = label.hash()
-                val box = boxes[boxIndex]
-                box.removeIf { it.label == label }
+                boxes[label.hash()].removeIf { it.label == label }
             }
         }
     }
 
-    val part2 = boxes.foldIndexed(0) { boxIndex, acc, box ->
-        var boxResult = 0
-        for ((slot, lens) in box.withIndex()) {
-            boxResult += (boxIndex + 1) * (slot + 1) * lens.focalLength
-        }
-        acc + boxResult
-    }
-
-    println(part2)
+    boxes.foldIndexed(0) { boxIndex, acc, box ->
+        acc + box.foldIndexed(0) { slot, boxAcc, lens -> boxAcc + (boxIndex + 1) * (slot + 1) * lens.focalLength }
+    }.also(::println)
 }
