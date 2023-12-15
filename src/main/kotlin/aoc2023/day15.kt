a5280269ea84c92fd2c06e7ec48f6bb08b45ef80
Michael Böiers <mikeenregalia@gmail.com>
2023-12-15 14:05:00 +0100

    solve day 15

--- src/main/kotlin/aoc2023/day15.kt
+++ src/main/kotlin/aoc2023/day15.kt
@@ -2,13 +2,50 @@ package aoc2023
 
 fun main() {
 
-    fun String.hash() = fold(0) { acc, c ->
-        (17 * (acc + c.code)) % 256
-    }
+    fun String.hash() = fold(0) { acc, c -> (17 * (acc + c.code)) % 256 }
 
-    val part1 = generateSequence(::readLine).single()
+    val input = generateSequence(::readLine)
+        .single()
         .split(",")
-        .sumOf { it.hash() }
+
+    val part1 = input.sumOf(String::hash)
 
     println(part1)
+
+    data class Lens(val label: String, val focalLength: Int) {
+        override fun toString() = "[$label, $focalLength]"
+    }
+
+    val boxes = Array(256) { ArrayDeque<Lens>() }
+
+    input.forEach { s ->
+        when {
+            "=" in s -> {
+                val (label, focalLength) = s.split("=")
+                val lens = Lens(label, focalLength.toInt())
+                val boxIndex = label.hash()
+                val box = boxes[boxIndex]
+                val index = box.indexOfFirst { it.label == label }
+                if (index == -1) box.add(lens)
+                else box[index] = lens
+            }
+
+            else -> {
+                val label = s.split("-").first()
+                val boxIndex = label.hash()
+                val box = boxes[boxIndex]
+                box.removeIf { it.label == label }
+            }
+        }
+    }
+
+    val part2 = boxes.foldIndexed(0) { boxIndex, acc, box ->
+        var boxResult = 0
+        for ((slot, lens) in box.withIndex()) {
+            boxResult += (boxIndex + 1) * (slot + 1) * lens.focalLength
+        }
+        acc + boxResult
+    }
+
+    println(part2)
 }
