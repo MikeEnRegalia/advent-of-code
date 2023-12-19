417a5423ad30609870fb01f2ac4cdeaa6822aa83
Michael Böiers <mikeenregalia@gmail.com>
2023-12-19 23:25:23 +0100

    refactor

--- src/main/kotlin/aoc2023/day04.kt
+++ src/main/kotlin/aoc2023/day04.kt
@@ -4,7 +4,7 @@ fun main() {
     val lines = generateSequence(::readLine).toList()
     var part1 = 0
     val part2Cards = MutableList(lines.size) { 1 }
-    lines.forEachIndexed<String> { game, line ->
+    lines.forEachIndexed { game, line ->
         val (winningNumbers, myNumbers) = line.split("|")
             .map { it.split(" ").mapNotNull(String::toIntOrNull) }
 
@@ -13,10 +13,9 @@ fun main() {
         val score = (1..wonCards).fold(0) { acc, _ -> if (acc == 0) 1 else acc * 2 }
 
         for (i in game + 1 until game + 1 + wonCards)
-            if (i in lines.indices)
-                part2Cards[i] += part2Cards[game]
+            if (i in lines.indices) part2Cards[i] += part2Cards[game]
 
         part1 += score
     }
-    listOf(part1, part2Cards.sumOf { it }).forEach(::println)
+    listOf(part1, part2Cards.sum()).forEach(::println)
 }
