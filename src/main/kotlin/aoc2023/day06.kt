45e27a4919e734e4b1084c21a56e75d102e8a142
Michael Böiers <mikeenregalia@gmail.com>
2023-12-19 23:27:12 +0100

    refactor

--- src/main/kotlin/aoc2023/day06.kt
+++ src/main/kotlin/aoc2023/day06.kt
@@ -1,8 +1,8 @@
 package aoc2023
 
-fun main() = day06(generateSequence(::readlnOrNull).toList()).forEach(::println)
+fun main() {
+    val lines = generateSequence(::readLine).toList()
 
-private fun day06(lines: List<String>): List<Any?> {
     data class Race(val duration: Long, val distance: Long)
 
     val races = lines
@@ -10,10 +10,7 @@ private fun day06(lines: List<String>): List<Any?> {
         .let { (durations, distances) -> durations.indices.map { i -> Race(durations[i], distances[i]) } }
 
     fun Race.wonIfWoundUpFor(woundUpFor: Long) = woundUpFor * (duration - woundUpFor) > distance
-
     fun List<Race>.sumWinningWays() = map { (0L..it.duration).count(it::wonIfWoundUpFor) }.reduce(Int::times)
-
     val part2Race = lines.map { it.filter(Char::isDigit).toLong() }.let { Race(it[0], it[1]) }
-
-    return listOf(races, listOf(part2Race)).map { it.sumWinningWays() }
+    listOf(races, listOf(part2Race)).map { it.sumWinningWays() }.forEach(::println)
 }
