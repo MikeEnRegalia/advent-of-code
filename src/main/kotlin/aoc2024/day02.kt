6959fbc58276b2a21807f32172aa828c56dc26a0
Michael Böiers <michael.boiers2@ingka.com>
2024-12-02 22:37:45 +0100

    refactor

--- src/main/kotlin/aoc2024/day02.kt
+++ src/main/kotlin/aoc2024/day02.kt
@@ -1,35 +1,22 @@
 package aoc2024
 
 fun main() {
-    fun List<Int>.check(): Boolean {
-        for ((i, n) in withIndex()) {
-            if (i == 0) continue
-            val diff = n - get(i - 1)
-            if (diff !in 1..3) {
-                return false
-            }
-        }
-
-        return true
+    fun prepare(record: String) = record.split(" ").map(String::toInt).let {
+        if (it.first() < it.last()) it else it.reversed()
     }
+
+    fun check(l: List<Int>) = l.windowed(2).map { it[1] - it[0] }.all { it in 1..3 }
 
     val lines = String(System.`in`.readAllBytes()).trim().lines()
 
-    val part1 = lines.count {
-        val x = it.split(" ").map(String::toInt)
-        val z = if (x.first() < x.last()) x else x.reversed()
-        z.check()
-    }
+    val part1 = lines.map(::prepare).count(::check)
     println(part1)
 
-    val part2 = lines.count {
-        val x = it.split(" ").map(String::toInt)
-        val z = if (x.first() < x.last()) x else x.reversed()
-        if (z.check()) return@count true
+    val part2 = lines.map(::prepare).count {
+        if (check(it)) return@count true
 
-        for (j in z.indices) {
-            val z2 = z.take(j) + z.drop(j + 1)
-            if (z2.check()) return@count true
+        for (j in it.indices) {
+            if (check(it.take<Int>(j) + it.drop<Int>(j + 1))) return@count true
         }
         false
     }
