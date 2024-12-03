b5b5420fb3b2e564983c740065ce7b55178f6767
Michael Böiers <michael.boiers2@ingka.com>
2024-12-03 23:51:53 +0100

    solve 2025 day 3

---
+++ src/main/kotlin/aoc2024/day03.kt
@@ -0,0 +1,15 @@
+package aoc2024
+
+fun main() {
+    val code = generateSequence(::readLine).joinToString()
+
+    fun calc(code: String) = code.split("""(mul\(|\))""".toRegex()).asSequence()
+        .map { it.split(",") }
+        .filter { it.size == 2 }
+        .map { it.mapNotNull(String::toIntOrNull) }
+        .filter { it.size == 2 }
+        .sumOf { it[0] * it[1] }
+
+    println(calc(code))
+    println(code.split("""don't\(\).*?(do\(\)|$)""".toRegex()).sumOf { calc(it) })
+}
