7196feb1a6b6cdc86a96f15d05dc8bca6640d3b5
Michael Böiers <michael.boiers2@ingka.com>
2024-12-05 00:06:23 +0100

    refactor

--- src/main/kotlin/aoc2024/day04.kt
+++ src/main/kotlin/aoc2024/day04.kt
@@ -3,31 +3,23 @@ package aoc2024
 fun main() {
     val lines = generateSequence(::readLine).toList()
 
-    fun wordsAt(x: Int, y: Int) = sequenceOf(
-        (0..3).map { lines.getOrNull(y)?.getOrNull(x + it) },
-        (0..3).map { lines.getOrNull(y + it)?.getOrNull(x) },
-        (0..3).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
-        (0..3).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
-    ).map { it.joinToString("") }
-
-    var part1 = 0
-    for (y in lines.indices) {
-        for (x in lines[y].indices) {
-            part1 += wordsAt(x, y).count { it == "XMAS" || it == "SAMX" }
-        }
+    fun points() = sequence {
+        for (y in lines.indices) for (x in lines[y].indices) yield(x to y)
     }
-    println(part1)
 
-    fun xWordsAt(x: Int, y: Int) = sequenceOf(
-        (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
-        (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
-    ).map { it.joinToString("") }
+    points().sumOf { (x, y) ->
+        sequenceOf(
+            (0..3).map { lines.getOrNull(y)?.getOrNull(x + it) },
+            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x) },
+            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
+            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
+        ).map { it.joinToString("") }.count { it == "XMAS" || it == "SAMX" }
+    }.also(::println)
 
-    var part2 = 0
-    for (y in lines.indices) {
-        for (x in lines[y].indices) {
-            part2 += if (xWordsAt(x, y).count { it == "MAS" || it == "SAM" } == 2) 1 else 0
-        }
-    }
-    println(part2)
+    points().count { (x, y) ->
+        sequenceOf(
+            (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
+            (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
+        ).map { it.joinToString("") }.count { it == "MAS" || it == "SAM" } == 2
+    }.also(::println)
 }
