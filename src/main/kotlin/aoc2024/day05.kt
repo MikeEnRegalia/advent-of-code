3a8ee94dc3310626dec42b77f5c51299680936f4
Michael Böiers <michael.boiers2@ingka.com>
2024-12-06 00:16:49 +0100

    refactor

--- src/main/kotlin/aoc2024/day05.kt
+++ src/main/kotlin/aoc2024/day05.kt
@@ -2,20 +2,14 @@ package aoc2024
 
 fun main() {
     val lines = generateSequence(::readLine).toList()
-
-    val rules = lines
-        .filter { '|' in it }
-        .map { line -> line.split("|").map(String::toInt).let { it[0] to it[1] } }
-
-    val updates = lines
-        .filter { ',' in it }
-        .map { it.split(",").map(String::toInt) }
+    val rules = lines.filter { '|' in it }.map { it.split("|").map(String::toInt) }
+    val updates = lines.filter { ',' in it }.map { it.split(",").map(String::toInt) }
 
     fun List<List<Int>>.checksum() = sumOf { it[it.size / 2] }
 
     val correctlyOrdered = updates.filter { update ->
-        rules.filter { it.first in update && it.second in update }.all { rule ->
-            update.indexOf(rule.first) < update.indexOf(rule.second)
+        rules.filter(update::containsAll).all { rule ->
+            update.indexOf(rule[0]) < update.indexOf(rule[1])
         }
     }
 
@@ -23,12 +17,12 @@ fun main() {
 
     val fixed = (updates - correctlyOrdered.toSet()).map { update ->
         val ordered = buildList {
-            val remainingRules = rules.filter { it.first in update && it.second in update }.toMutableList()
+            val remainingRules = rules.filter(update::containsAll).toMutableList()
             while (true) {
                 val remainingPages = update - toSet()
-                val smallest = remainingPages.singleOrNull { page -> page !in remainingRules.map { it.second } } ?: break
+                val smallest = remainingPages.singleOrNull { page -> page !in remainingRules.map { it[1] } } ?: break
                 add(smallest)
-                remainingRules.removeAll { it.first == smallest }
+                remainingRules.removeAll { it[0] == smallest }
             }
         }
 
