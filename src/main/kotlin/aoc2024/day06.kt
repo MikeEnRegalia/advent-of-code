f4204e734d77e1868084dfb248782ec963acec45
Michael Böiers <michael.boiers2@ingka.com>
2024-12-06 15:09:20 +0100

    refactor

--- src/main/kotlin/aoc2024/day06.kt
+++ src/main/kotlin/aoc2024/day06.kt
@@ -11,50 +11,53 @@ fun main() {
             3 -> copy(x = x - 1)
             else -> throw IllegalArgumentException("Unknown direction")
         }
+        fun content() = grid.getOrNull(y)?.getOrNull(x)
+    }
+
+    data class Pos(val p: Point, val dir: Int) {
+        fun next() = copy(p = p.next(dir))
+        fun turn() = copy(dir = (dir + 1) % 4)
     }
 
     fun points() = sequence {
         for (y in grid.indices) for (x in grid[y].indices) yield(Point(x, y))
     }
 
-    fun gridAt(point: Point) = grid.getOrNull(point.y)?.getOrNull(point.x)
-
-    val start = points().single { gridAt(it) == '^' }
+    val start = points().single { it.content() == '^' }
 
     fun part1(): Int {
-        var curr = start
-        var dir = 0
-        val visited = mutableSetOf(curr)
+        var curr = Pos(start, 0)
+        val visited = mutableSetOf(curr.p)
         while (true) {
-            val next = curr.next(dir)
-            when (gridAt(next)) {
+            val next = curr.next()
+            when (next.p.content()) {
                 null -> return visited.size
-                '#' -> dir = (dir + 1) % 4
+                '#' -> curr = curr.turn()
                 else -> {
                     curr = next
-                    visited += curr
+                    visited += curr.p
                 }
             }
         }
     }
-    println(part1())
 
-    val part2 = points().filter { point ->
-        var curr = start
-        var dir = 0
-        val walked = mutableSetOf(curr to dir)
+    fun part2() = points().count { point ->
+        var curr = Pos(start, 0)
+        val walked = mutableSetOf(curr)
         while (true) {
-            val next = curr.next(dir)
-            when (if (next == point) '#' else gridAt(next)) {
-                null -> return@filter false
-                '#' -> dir = (dir + 1) % 4
+            val next = curr.next()
+            when (if (next.p == point) '#' else next.p.content()) {
+                null -> return@count false
+                '#' -> curr = curr.turn()
                 else -> {
                     curr = next
-                    if (!walked.add(curr to dir)) break
+                    if (!walked.add(curr)) break
                 }
             }
         }
         true
-    }.count()
-    println(part2)
+    }
+
+    println(part1())
+    println(part2())
 }
