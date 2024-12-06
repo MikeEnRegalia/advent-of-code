0939d970ddb8f04dd0b9017e40585324cf9ec759
Michael Böiers <michael.boiers2@ingka.com>
2024-12-06 13:21:58 +0100

    solved 2024 day 6 part 2

--- src/main/kotlin/aoc2024/day06.kt
+++ src/main/kotlin/aoc2024/day06.kt
@@ -10,33 +10,66 @@ fun main() {
     }
 
     val start = points().single { gridAt(it) == '^' }
+    fun part1(): Int {
+        var curr = start
+        var dir = 0
+        val visited = mutableSetOf(curr)
+        while (true) {
+            val next = with(curr) {
+                when (dir) {
+                    0 -> copy(y = y - 1)
+                    1 -> copy(x = x + 1)
+                    2 -> copy(y = y + 1)
+                    3 -> copy(x = x - 1)
+                    else -> throw IllegalArgumentException("Unknown direction")
+                }
+            }
+            when (gridAt(next)) {
+                null -> break
+                '#' -> {
+                    dir = (dir + 1) % 4
+                    continue
+                }
 
-    var curr = start
-    var dir = 0
-    val visited = mutableSetOf(curr)
-    while (true) {
-        println("$curr $dir")
-        val next = with(curr) {
-            when (dir) {
-                0 -> copy(y = y - 1)
-                1 -> copy(x = x + 1)
-                2 -> copy(y = y + 1)
-                3 -> copy(x = x - 1)
-                else -> throw IllegalArgumentException("Unknown direction")
+                else -> {
+                    curr = next
+                    visited += curr
+                }
             }
         }
-        when (gridAt(next)) {
-            null -> break
-            '#' -> {
-                dir = (dir + 1) % 4
-                continue
-            }
-
-            else -> {
-                curr = next
-                visited += curr
-            }
-        }
+        return visited.size
     }
-    println(visited.size)
+
+    println(part1())
+
+    val part2 = points().filter { point ->
+        var curr = start
+        var dir = 0
+        val walked = mutableSetOf(curr to dir)
+        while (true) {
+            val next = with(curr) {
+                when (dir) {
+                    0 -> copy(y = y - 1)
+                    1 -> copy(x = x + 1)
+                    2 -> copy(y = y + 1)
+                    3 -> copy(x = x - 1)
+                    else -> throw IllegalArgumentException("Unknown direction")
+                }
+            }
+            when (if (next == point) '#' else gridAt(next)) {
+                null -> return@filter false
+                '#' -> {
+                    dir = (dir + 1) % 4
+                    continue
+                }
+
+                else -> {
+                    curr = next
+                    if (!walked.add(curr to dir)) break
+                }
+            }
+        }
+        true
+    }.count()
+    println(part2)
 }
