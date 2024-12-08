68995754d87a8d2863c97266327e186063bfab82
Michael Böiers <michael.boiers2@ingka.com>
2024-12-08 22:15:13 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -1,21 +1,17 @@
 package aoc2024
 
+typealias Operations = (a: Long, b: Long) -> Sequence<Long>
+
 fun main() = generateSequence(::readLine)
     .map { it.split(" ", ":").filter(String::isNotBlank).map(String::toLong) }.toList()
     .run {
-        println(check { a, b -> sequenceOf(a + b, a * b)})
-        println(check { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong())})
+        println(test { a, b -> sequenceOf(a + b, a * b) })
+        println(test { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong()) })
     }
 
-fun List<List<Long>>.check(op: (a: Long, b: Long) -> Sequence<Long>) =
-    filter { it.test(op) }.sumOf { it.first() }
+fun List<List<Long>>.test(op: Operations) = filter { it.test(op) }.sumOf(List<Long>::first)
 
-fun List<Long>.test(op: (a: Long, b: Long) -> Sequence<Long>, x: Long = 0L): Boolean = when (size) {
+fun List<Long>.test(op: Operations, x: Long = 0L): Boolean = when (size) {
     1 -> x == first()
-    else -> {
-        val (first, rest) = get(1) to listOf(first()) + drop(2)
-        with(rest) {
-            op(x, first).any { test(op, it) }
-        }
-    }
+    else -> op(x, get(1)).any { (listOf(first()) + drop(2)).test(op, it) }
 }
