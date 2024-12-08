80b301009cd3152efcd76936fca31621c7186c76
Michael Böiers <michael.boiers2@ingka.com>
2024-12-08 21:52:32 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -1,21 +1,22 @@
 package aoc2024
 
-fun main() {
-    val lines = generateSequence(::readLine).map {
-        it.split(" ", ":").filter(String::isNotBlank).map(String::toLong)
-    }.toList()
+fun main() = generateSequence(::readLine)
+    .map { it.split(" ", ":").filter(String::isNotBlank).map(String::toLong) }.toList()
+    .run {
+        println(check(false))
+        println(check(true))
+    }
 
-    fun List<List<Long>>.check(concat: Boolean) = mapNotNull { numbers ->
-        fun List<Long>.test(n: Long, x: Long = 0L): Boolean {
-            if (isEmpty()) return x == n
-            val (first, rest) = first() to drop(1)
-            return with(rest) {
-                test(n, x + first) || test(n, x * first) || concat && test(n, "$x$first".toLong())
-            }
+fun List<List<Long>>.check(concat: Boolean) = mapNotNull { numbers ->
+    numbers.first().takeIf { numbers.test(concat) }
+}.sum()
+
+fun List<Long>.test(concat: Boolean, x: Long = 0L): Boolean = when (size) {
+    1 -> x == first()
+    else -> {
+        val (first, rest) = get(1) to listOf(first()) + drop(2)
+        with(rest) {
+            test(concat, x + first) || test(concat, x * first) || concat && test(true, "$x$first".toLong())
         }
-        numbers.first().takeIf { numbers.drop(1).test(it) }
-    }.sum()
-
-    println(lines.check(false))
-    println(lines.check(true))
+    }
 }
