be10a734a902ab109741aae36153f96cfa4c1a7d
Michael Böiers <michael.boiers2@ingka.com>
2024-12-08 22:31:11 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -4,14 +4,15 @@ typealias Operations = (a: Long, b: Long) -> Sequence<Long>
 
 fun main() = generateSequence(::readLine)
     .map { it.split(" ", ":").filter(String::isNotBlank).map(String::toLong) }.toList()
-    .run {
-        println(test { a, b -> sequenceOf(a + b, a * b) })
-        println(test { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong()) })
+    .let { numberLists ->
+        sequenceOf<Operations>(
+            { a, b -> sequenceOf(a + b, a * b) },
+            { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong()) })
+            .map { op -> numberLists.filter { it.check(op) }.sumOf(List<Long>::first) }
+            .forEach(::println)
     }
 
-fun List<List<Long>>.test(op: Operations) = filter { it.test(op) }.sumOf(List<Long>::first)
-
-fun List<Long>.test(op: Operations, x: Long = 0L): Boolean = when (size) {
+fun List<Long>.check(op: Operations, x: Long = 0L): Boolean = when (size) {
     1 -> x == first()
-    else -> op(x, get(1)).any { (listOf(first()) + drop(2)).test(op, it) }
+    else -> op(x, get(1)).any { (listOf(first()) + drop(2)).check(op, it) }
 }
