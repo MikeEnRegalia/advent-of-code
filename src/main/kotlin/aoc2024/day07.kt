1a1057efcd8dd6aed436fda903aaf4ff707de113
Michael Böiers <michael.boiers2@ingka.com>
2024-12-08 11:59:37 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -1,18 +1,19 @@
 package aoc2024
 
 fun main() {
-    val lines = generateSequence(::readLine).toList()
-    fun List<String>.check(concat: Boolean) = mapNotNull { line ->
-        val numbers = line.split(" ", ":").filter(String::isNotBlank).map(String::toLong)
-        fun List<Long>.test(x: Long, n: Long): Boolean {
+    val lines = generateSequence(::readLine).map {
+        it.split(" ", ":").filter(String::isNotBlank).map(String::toLong)
+    }.toList()
+
+    fun List<List<Long>>.check(concat: Boolean) = mapNotNull { numbers ->
+        fun List<Long>.test(n: Long, x: Long = 0L): Boolean {
             if (isEmpty()) return x == n
             val (first, rest) = first() to drop(1)
-            val (a, b, c) = Triple(x * first, x + first, "$x$first".toLong())
-            if (size == 1 && (a == n || b == n || concat && c == n)) return true
-            return rest.test(b, n) || rest.test(a, n) || concat && rest.test(c, n)
+            return with(rest) {
+                test(n, x + first) || test(n, x * first) || concat && test(n, "$x$first".toLong())
+            }
         }
-
-        numbers.first().takeIf { numbers.drop(1).test(0L, it) }
+        numbers.first().takeIf { numbers.drop(1).test(it) }
     }.sum()
 
     println(lines.check(false))
