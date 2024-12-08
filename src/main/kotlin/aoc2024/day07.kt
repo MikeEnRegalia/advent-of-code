9822d74683fe23e434c1fe686d6d6aee88cb1ec3
Michael Böiers <michael.boiers2@ingka.com>
2024-12-08 22:00:35 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -3,20 +3,19 @@ package aoc2024
 fun main() = generateSequence(::readLine)
     .map { it.split(" ", ":").filter(String::isNotBlank).map(String::toLong) }.toList()
     .run {
-        println(check(false))
-        println(check(true))
+        println(check { a, b -> sequenceOf(a + b, a * b)})
+        println(check { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong())})
     }
 
-fun List<List<Long>>.check(concat: Boolean) = mapNotNull { numbers ->
-    numbers.first().takeIf { numbers.test(concat) }
-}.sum()
+fun List<List<Long>>.check(op: (a: Long, b: Long) -> Sequence<Long>) =
+    filter { it.test(op) }.sumOf { it.first() }
 
-fun List<Long>.test(concat: Boolean, x: Long = 0L): Boolean = when (size) {
+fun List<Long>.test(op: (a: Long, b: Long) -> Sequence<Long>, x: Long = 0L): Boolean = when (size) {
     1 -> x == first()
     else -> {
         val (first, rest) = get(1) to listOf(first()) + drop(2)
         with(rest) {
-            test(concat, x + first) || test(concat, x * first) || concat && test(true, "$x$first".toLong())
+            op(x, first).any { test(op, it) }
         }
     }
 }
