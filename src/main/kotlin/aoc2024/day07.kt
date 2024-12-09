fd7abc94588a7e69241a5339404abfc378153851
Michael Böiers <michael.boiers2@ingka.com>
2024-12-09 22:46:05 +0100

    refactor

--- src/main/kotlin/aoc2024/day07.kt
+++ src/main/kotlin/aoc2024/day07.kt
@@ -5,6 +5,10 @@ typealias Operations = (a: Long, b: Long) -> Sequence<Long>
 fun main() = generateSequence(::readLine)
     .map { it.split(" ", ":").filter(String::isNotBlank).map(String::toLong) }.toList()
     .let { numberLists ->
+        fun List<Long>.check(op: Operations, x: Long = 0L): Boolean = when (size) {
+            1 -> x == first()
+            else -> op(x, get(1)).any { (listOf(first()) + drop(2)).check(op, it) }
+        }
         sequenceOf<Operations>(
             { a, b -> sequenceOf(a + b, a * b) },
             { a, b -> sequenceOf(a + b, a * b, "$a$b".toLong()) })
@@ -12,7 +16,3 @@ fun main() = generateSequence(::readLine)
             .forEach(::println)
     }
 
-fun List<Long>.check(op: Operations, x: Long = 0L): Boolean = when (size) {
-    1 -> x == first()
-    else -> op(x, get(1)).any { (listOf(first()) + drop(2)).check(op, it) }
-}
