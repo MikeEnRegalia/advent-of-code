046026775cce96e80d5b84a0d474d13be567a2f7
Michael Böiers <michael.boiers2@ingka.com>
2024-12-09 22:12:12 +0100

    refactor

--- src/main/kotlin/aoc2024/day02.kt
+++ src/main/kotlin/aoc2024/day02.kt
@@ -1,20 +1,12 @@
 package aoc2024
 
 fun main() {
-    fun prepare(record: String) = record.split(" ").map(String::toInt).let {
-        if (it.first() < it.last()) it else it.reversed()
-    }
-
+    fun prepare(record: String) = record.split(" ").map(String::toInt)
+        .let { if (it.first() < it.last()) it else it.reversed() }
     fun check(l: List<Int>) = l.windowed(2).map { it[1] - it[0] }.all { it in 1..3 }
 
-    val lines = generateSequence(::readLine).toList()
-
-    val part1 = lines.map(::prepare).count(::check)
-    println(part1)
-
-    val part2 = lines.map(::prepare).count {
-        if (check(it)) return@count true
-        (it.indices.map { j -> it.take(j) + it.drop(j + 1) }).any(::check)
+    with(generateSequence(::readLine).map(::prepare).toList()) {
+        println(count(::check))
+        println(count { check(it) || it.indices.map { j -> it.take(j) + it.drop(j + 1) }.any(::check) })
     }
-    println(part2)
 }
