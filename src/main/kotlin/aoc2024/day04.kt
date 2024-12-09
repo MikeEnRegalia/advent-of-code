81e86cca5e16f32a8b79ecfa0842abe85ddf0cfa
Michael Böiers <michael.boiers2@ingka.com>
2024-12-09 22:25:10 +0100

    refactor

--- src/main/kotlin/aoc2024/day04.kt
+++ src/main/kotlin/aoc2024/day04.kt
@@ -2,6 +2,7 @@ package aoc2024
 
 fun main() {
     val lines = generateSequence(::readLine).toList()
+    fun gridAt(x: Int, y: Int) = lines.getOrNull(y)?.getOrNull(x)
 
     fun points() = sequence {
         for (y in lines.indices) for (x in lines[y].indices) yield(x to y)
@@ -9,17 +10,17 @@ fun main() {
 
     points().sumOf { (x, y) ->
         sequenceOf(
-            (0..3).map { lines.getOrNull(y)?.getOrNull(x + it) },
-            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x) },
-            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
-            (0..3).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
+            (0..3).map { gridAt(x+ it, y) },
+            (0..3).map { gridAt(x, y + it) },
+            (0..3).map { gridAt(x + it, y + it) },
+            (0..3).map { gridAt(x - it, y + it) },
         ).map { it.joinToString("") }.count { it == "XMAS" || it == "SAMX" }
     }.also(::println)
 
     points().count { (x, y) ->
         sequenceOf(
-            (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x + it) },
-            (-1..1).map { lines.getOrNull(y + it)?.getOrNull(x - it) },
+            (-1..1).map { gridAt(x + it, y + it) },
+            (-1..1).map { gridAt(x - it, y + it) },
         ).map { it.joinToString("") }.all { it == "MAS" || it == "SAM" }
     }.also(::println)
 }
