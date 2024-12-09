47fdc6abe1bf13869eab76bd8db8712b3fcd0f13
Michael Böiers <michael.boiers2@ingka.com>
2024-12-09 22:28:31 +0100

    refactor

--- src/main/kotlin/aoc2024/day04.kt
+++ src/main/kotlin/aoc2024/day04.kt
@@ -4,13 +4,11 @@ fun main() {
     val lines = generateSequence(::readLine).toList()
     fun gridAt(x: Int, y: Int) = lines.getOrNull(y)?.getOrNull(x)
 
-    fun points() = sequence {
-        for (y in lines.indices) for (x in lines[y].indices) yield(x to y)
-    }
+    fun points() = sequence { for (y in lines.indices) for (x in lines[y].indices) yield(x to y) }
 
     points().sumOf { (x, y) ->
         sequenceOf(
-            (0..3).map { gridAt(x+ it, y) },
+            (0..3).map { gridAt(x + it, y) },
             (0..3).map { gridAt(x, y + it) },
             (0..3).map { gridAt(x + it, y + it) },
             (0..3).map { gridAt(x - it, y + it) },
