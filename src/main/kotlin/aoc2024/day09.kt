68f80cb197be340c5a666236b0fd3bceb200baa7
Michael Böiers <michael.boiers2@ingka.com>
2024-12-09 12:26:39 +0100

    solved day 9

--- src/main/kotlin/aoc2024/day09.kt
+++ src/main/kotlin/aoc2024/day09.kt
@@ -1,27 +1,59 @@
 package aoc2024
 
 fun main() {
-    var file = -1
-    val data = buildList {
-        readln().map(Char::digitToInt).chunked(2).forEach {
-            file++
-            repeat(it[0]) { add(file) }
-            if (it.size == 2) {
-                repeat(it[1]) { add(-1) }
+    data class Chunk(val id: Int?, val size: Int)
+
+    val originalChunks = buildMap {
+        var index = 0
+        readln().map(Char::digitToInt).forEachIndexed { i, size ->
+            this[index] = Chunk(if (i % 2 == 0) i / 2 else null, size)
+            index += size
+        }
+    }
+
+    fun buildList(chunks: Map<Int, Chunk>) = mutableListOf<Int>().apply {
+        chunks.entries.toList().sortedBy { it.key }.forEach { (pos, chunk) ->
+            repeat(chunk.size) {
+                add(chunk.id ?: -1)
             }
         }
     }
 
-    val part1 = data.toMutableList().also { fs ->
+
+    val part1 = buildList(originalChunks).also { fs ->
         var nextFree = -1
-        var nextFile = data.size
+        var nextFile = fs.size
         while (true) {
             while (nextFree < 0 || fs[nextFree] != -1) nextFree++
-            while (nextFile >= data.size || fs[nextFile] == -1) nextFile--
+            while (nextFile >= fs.size || fs[nextFile] == -1) nextFile--
             if (nextFree > nextFile) break
-            fs[nextFree] = data[nextFile]
+            fs[nextFree] = fs[nextFile]
             fs[nextFile] = -1
         }
     }
-    println(part1.mapIndexedNotNull { i, it -> if (it == -1) null else (i * it).toLong() }.sum())
+
+    val part2 = originalChunks.toMutableMap().let { new ->
+        val fileIDs = new.entries.mapNotNull { it.value.id }
+        for (fileId in fileIDs.sorted().reversed()) {
+            val (fileIndex, fileChunk) = new.entries.single { it.value.id == fileId }
+            val (freeIndex, freeChunk) = new.entries
+                .filter { it.value.id == null && it.key < fileIndex && it.value.size >= fileChunk.size }
+                .minByOrNull { it.key } ?: continue
+
+            new[fileIndex] = Chunk(null, fileChunk.size)
+            new[freeIndex] = fileChunk
+
+            val newFreeSize = freeChunk.size - fileChunk.size
+            if (newFreeSize > 0) {
+                val newFreeIndex = freeIndex + fileChunk.size
+                new[newFreeIndex] = Chunk(null, newFreeSize)
+            }
+        }
+        buildList(new)
+    }
+
+
+    listOf(part1, part2).map { fs ->
+        fs.mapIndexedNotNull { i, it -> if (it == -1) null else (i * it).toLong() }.sum()
+    }.also(::println)
 }
