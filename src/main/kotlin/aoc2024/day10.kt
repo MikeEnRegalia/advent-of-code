f3a610f58890398d645a57eeebbda675f9cf3fbf
Michael Böiers <michael.boiers2@ingka.com>
2024-12-10 07:10:12 +0100

    refactor

--- src/main/kotlin/aoc2024/day10.kt
+++ src/main/kotlin/aoc2024/day10.kt
@@ -13,59 +13,34 @@ fun main() {
         }
     }
 
-
     val starts = sequence {
         for (y in grid.indices) for (x in grid[y].indices)
             if (grid[y][x] == 0) yield(Point(x, y))
     }
 
-    fun part1(): Int {
-        var total = 0
-        for (start in starts) {
-            val found = mutableSetOf<Point>()
-            val visited = mutableSetOf(start)
-            val distances = mutableMapOf(start to 0)
-            var curr = start
-            while (true) {
-                curr.neighbors().filter { it.content() == curr.content()!! + 1 && it !in visited }.forEach { neighbor ->
-                    val d = distances.getValue(curr) + 1
-                    distances.compute(neighbor) { _, v -> min(d, v ?: Int.MAX_VALUE) }
+    var part1 = 0
+    var part2 = 0
+    for (start in starts) {
+        val found = mutableSetOf<List<Point>>()
+        val visited = mutableSetOf(listOf(start))
+        val distances = mutableMapOf(listOf(start) to 0)
+        var curr = listOf(start)
+        while (true) {
+            curr.last().neighbors()
+                .filter { it.content() == curr.last().content()!! + 1 }
+                .map { curr + it }
+                .filter { it !in visited }
+                .forEach { neighbor ->
+                    distances.compute(neighbor) { _, v -> min(distances.getValue(curr) + 1, v ?: Int.MAX_VALUE) }
                 }
-                curr = distances.keys.filter { it !in visited }.minByOrNull { distances[it]!! } ?: break
-                visited += curr
-                if (curr.content() == 9) found += curr
-            }
-            total += found.size
+            curr = distances.keys.filter { it !in visited }.minByOrNull { distances[it]!! } ?: break
+            visited += curr
+            if (curr.last().content() == 9) found += curr
         }
-        return total
-    }
-    println(part1())
-
-    fun part2(): Int {
-        var total = 0
-        for (start in starts) {
-            val found = mutableSetOf<List<Point>>()
-            val visited = mutableSetOf(listOf(start))
-            val distances = mutableMapOf(listOf(start) to 0)
-            var curr = listOf(start)
-            while (true) {
-                println(curr)
-                curr.last().neighbors()
-                    .filter { it.content() == curr.last().content()!! + 1 }
-                    .map { curr + it }
-                    .filter { it !in visited }
-                    .forEach { neighbor ->
-                        distances.compute(neighbor) { _, v -> min(distances.getValue(curr) + 1, v ?: Int.MAX_VALUE) }
-                    }
-                curr = distances.keys.filter { it !in visited }.minByOrNull { distances[it]!! } ?: break
-                visited += curr
-                if (curr.last().content() == 9) found += curr
-            }
-            println(found)
-            total += found.size
-        }
-        return total
+        part1 += found.map { it.last() }.distinct().size
+        part2 += found.size
     }
 
-    println(part2())
+    println(part1)
+    println(part2)
 }
