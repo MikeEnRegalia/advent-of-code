aa6d1edcf66821a80b59a5143801faf8f3f39082
Michael Böiers <michael.boiers2@ingka.com>
2024-12-10 08:46:48 +0100

    refactor

--- src/main/kotlin/aoc2024/day10.kt
+++ src/main/kotlin/aoc2024/day10.kt
@@ -3,42 +3,39 @@ package aoc2024
 import kotlin.math.min
 
 fun main() {
-    val grid = generateSequence(::readLine).map { it.map { it.digitToInt() } }.toList()
+    val area = generateSequence(::readLine).map { it.map { it.digitToInt() } }.toList()
 
-    data class Point(val x: Int, val y: Int) {
-        fun neighbors() = sequenceOf(copy(x = x + 1), copy(x = x - 1), copy(y = y + 1), copy(y = y - 1))
-        fun content() = grid.getOrNull(y)?.getOrNull(x)
-        override fun toString(): String {
-            return "($x,$y:${content()})"
-        }
+    data class Location(val x: Int, val y: Int) {
+        fun next() = sequenceOf(copy(x = x + 1), copy(x = x - 1), copy(y = y + 1), copy(y = y - 1))
+            .filter { it.content() == content()?.plus(1) }
+
+        fun content() = area.getOrNull(y)?.getOrNull(x)
     }
 
-    val starts = sequence {
-        for (y in grid.indices) for (x in grid[y].indices)
-            if (grid[y][x] == 0) yield(Point(x, y))
+    val heads = sequence {
+        for (y in area.indices) for (x in area[y].indices) if (area[y][x] == 0) yield(Location(x, y))
     }
 
     var part1 = 0
     var part2 = 0
-    for (start in starts) {
-        val found = mutableSetOf<List<Point>>()
-        val visited = mutableSetOf(listOf(start))
-        val distances = mutableMapOf(listOf(start) to 0)
-        var curr = listOf(start)
+
+    for (head in heads) {
+        val trails = mutableSetOf<List<Location>>()
+        val V = mutableSetOf(listOf(head))
+        val D = mutableMapOf(listOf(head) to 0)
+        var curr = listOf(head)
         while (true) {
-            curr.last().neighbors()
-                .filter { it.content() == curr.last().content()!! + 1 }
-                .map { curr + it }
-                .filter { it !in visited }
+            curr.last().next().map { curr + it }
+                .filter { it !in V }
                 .forEach { neighbor ->
-                    distances.compute(neighbor) { _, v -> min(distances.getValue(curr) + 1, v ?: Int.MAX_VALUE) }
+                    D.compute(neighbor) { _, v -> min(D.getValue(curr) + 1, v ?: Int.MAX_VALUE) }
                 }
-            curr = distances.keys.filter { it !in visited }.minByOrNull { distances[it]!! } ?: break
-            visited += curr
-            if (curr.last().content() == 9) found += curr
+            curr = D.keys.filter { it !in V }.minByOrNull { D[it]!! } ?: break
+            V += curr
+            if (curr.last().content() == 9) trails += curr
         }
-        part1 += found.map { it.last() }.distinct().size
-        part2 += found.size
+        part1 += trails.map { it.last() }.distinct().size
+        part2 += trails.size
     }
 
     println(part1)
