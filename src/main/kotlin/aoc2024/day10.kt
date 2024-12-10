d2e162899b6573a64cb21bae2a4746ae52ba2fe6
Michael Böiers <michael.boiers2@ingka.com>
2024-12-10 11:10:50 +0100

    refactor

--- src/main/kotlin/aoc2024/day10.kt
+++ src/main/kotlin/aoc2024/day10.kt
@@ -2,31 +2,31 @@ package aoc2024
 
 import kotlin.math.min
 
-fun main() {
-    with(generateSequence(::readLine).map { it.map(Char::digitToInt) }.toList()) {
-        data class Location(val x: Int, val y: Int) {
-            fun neighbors() = sequenceOf(d(dx = 1), d(dx = -1), d(dy = 1), d(dy = -1)).filter { isNext(it) }
-            fun d(dx: Int = 0, dy: Int = 0) = copy(x = x + dx, y = y + dy)
-            fun isNext(l: Location) = l.height() == height() + 1
-            fun height() = getOrNull(y)?.getOrNull(x) ?: -1
-        }
-
-        val hikes = flatMapIndexed { y, l -> l.mapIndexedNotNull { x, c -> c.takeIf { c == 0 }?.let { Location(x, y) } } }
-            .fold(mutableSetOf<List<Location>>()) { acc, trailhead ->
-                var t = listOf(trailhead)
-                val V = mutableSetOf(t)
-                val D = mutableMapOf(t to 0)
-                while (true) {
-                    t.last().neighbors().map { t + it }.forEach {
-                        D.compute(it) { _, v -> min(D.getValue(t) + 1, v ?: Int.MAX_VALUE) }
-                    }
-                    t = D.keys.firstOrNull { it !in V } ?: break
-                    if (t.last().height() == 9) acc += t
-                    V += t
-                }
-                acc
-            }
-        println(hikes.map { listOf(it.first(), it.last()) }.distinct().size)
-        println(hikes.size)
+fun main() = with(generateSequence(::readLine).map { it.map(Char::digitToInt) }.toList()) {
+    data class Loc(val x: Int, val y: Int) {
+        fun d(dx: Int = 0, dy: Int = 0) = copy(x = x + dx, y = y + dy)
+        fun height() = getOrNull(y)?.getOrNull(x)
     }
+
+    fun Loc.hikes() = buildSet {
+        var t = listOf(this@hikes)
+        val (V, D) = mutableSetOf(t) to mutableMapOf(t to 0)
+        while (true) {
+            with(t.last()) {
+                sequenceOf(d(dx = 1), d(dx = -1), d(dy = 1), d(dy = -1))
+                    .filter { it.height() == (height() ?: 0) + 1 }
+                    .map { t + it }
+                    .forEach { D.compute(it) { _, v -> min(D.getValue(t) + 1, v ?: Int.MAX_VALUE) } }
+            }
+            t = D.keys.firstOrNull { it !in V } ?: break
+            if (t.last().height() == 9) add(t)
+            V += t
+        }
+    }
+
+    val hikes = flatMapIndexed { y, l ->
+        l.mapIndexedNotNull { x, c -> c.takeIf { c == 0 }?.let { Loc(x, y).hikes() } }
+    }.reduce(Set<List<Loc>>::union)
+
+    sequenceOf(hikes.map { listOf(it.first(), it.last()) }.distinct(), hikes).forEach { println(it.size) }
 }
