bf223bc5c14d88e1bcc9e78d4693a9acfad6118b
Michael Böiers <michael.boiers2@ingka.com>
2024-12-10 12:28:30 +0100

    refactor

--- src/main/kotlin/aoc2024/day08.kt
+++ src/main/kotlin/aoc2024/day08.kt
@@ -3,36 +3,33 @@ package aoc2024
 fun main() {
     val grid = generateSequence(::readLine).toList()
 
-    data class Point(val x: Int, val y: Int, val antenna: Char) {
-        fun antinodesFor(other: Point, f: Int): Set<Point> {
+    data class Loc(val x: Int, val y: Int, val antenna: Char) {
+        fun antinodesFor(other: Loc, f: Int = 1): Set<Loc> {
             val (dx, dy) = f * (x - other.x) to f * (y - other.y)
-            val (a1, a2) = Point(x + dx, y + dy, '#') to Point(other.x - dx, other.y - dy, '#')
+            val (a1, a2) = Loc(x + dx, y + dy, '#') to Loc(other.x - dx, other.y - dy, '#')
             return sequenceOf(a1, a2).filter { grid.getOrNull(it.y)?.getOrNull(it.x) != null }.toSet()
         }
     }
 
-    val allAntennas = sequence {
-        for (y in grid.indices) for (x in grid[y].indices)
-            grid[y][x].takeIf { it !in ".#" }?.let { yield(Point(x, y, it)) }
-    }.toList()
+    val allAntennas = buildMap<Char, Set<Loc>> {
+        for (y in grid.indices) for (x in grid[y].indices) grid[y][x].takeIf { it !in ".#" }
+            ?.let { compute(it) { _, v -> (v ?: setOf()) + Loc(x, y, it) } }
+    }
 
-    fun antinodes(simple: Boolean) = buildSet {
-        for (antenna in allAntennas.map { it.antenna }.toSet()) {
-            val antennas = allAntennas.filter { it.antenna == antenna }.takeIf { it.size > 1 } ?: continue
-            for (a in antennas) for (b in antennas.filter { a != it }) when {
-                simple -> addAll(a.antinodesFor(b, 1))
-                else -> {
-                    var f = 0
-                    while (true) {
-                        val antinodes = a.antinodesFor(b, f).takeIf { it.isNotEmpty() } ?: break
-                        addAll(antinodes)
-                        f++
-                    }
-                }
+    val antennaCombinations = allAntennas.values.flatMap { antennas ->
+        antennas.flatMap { a -> antennas.filter { it != a }.map { b -> a to b } }
+    }
+
+    fun antinodes(simple: Boolean) = antennaCombinations.fold(mutableSetOf<Loc>()) { acc, (a, b) ->
+        when {
+            simple -> acc.addAll(a.antinodesFor(b))
+            else -> {
+                var f = 0
+                while (true) acc.addAll(a.antinodesFor(b, f++).takeIf { it.isNotEmpty() } ?: break)
             }
         }
-    }.size
+        acc
+    }
 
-    println(antinodes(simple = true))
-    println(antinodes(simple = false))
+    listOf(true, false).forEach { println(antinodes(simple = it).size) }
 }
