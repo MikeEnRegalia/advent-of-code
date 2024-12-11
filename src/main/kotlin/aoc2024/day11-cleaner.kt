eb7862fc627ab8f231c01fb6a6e4a56983fb104d
Michael Böiers <michael.boiers2@ingka.com>
2024-12-11 11:55:12 +0100

    refactor, revert problematic solution

--- src/main/kotlin/aoc2024/day11-cleaner.kt
+++ src/main/kotlin/aoc2024/day11-cleaner.kt
@@ -1,17 +1,15 @@
 package aoc2024
 
-import util.mapToCountAsLong
-
 fun main() {
-    var stones = readln().split(" ").map(String::toLong).mapToCountAsLong()
-    repeat(75) { i ->
-        stones = stones.flatMap { (stone, n) ->
+    val input = readln().split(" ").map(String::toLong).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+    (1..75).fold(input) { acc, i ->
+        acc.flatMap { (stone, n) ->
             when {
                 stone == 0L -> listOf(1L to n)
                 "$stone".length % 2 == 0 -> "$stone".let { it.chunked(it.length / 2).map { s -> s.toLong() to n } }
                 else -> listOf(stone * 2024 to n)
             }
-        }.associate { (stone, n) -> stone to (stones.getOrDefault(n, 0L) + n) }
-        if (i + 1 in listOf(25, 75)) println(stones.values.sum())
+        }.associate { (stone, n) -> stone to (acc.getOrDefault(stone, 0L) + n) }
+            .also { println(it.values.sum()) }
     }
 }
--- src/main/kotlin/aoc2024/day11.kt
+++ src/main/kotlin/aoc2024/day11.kt
@@ -3,13 +3,13 @@ package aoc2024
 fun main() {
     var stones = readln().split(" ").map(String::toLong).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
     repeat(75) { i ->
-        stones = stones.flatMap { (stone, n) ->
-            when {
-                stone == 0L -> listOf(1L to n)
-                "$stone".length % 2 == 0 -> "$stone".let { it.chunked(it.length / 2).map { s -> s.toLong() to n } }
-                else -> listOf(stone * 2024 to n)
-            }
-        }.associate { (stone, n) -> stone to (stones.getOrDefault(n, 0L) + n) }
-        if (i + 1 in listOf(25, 75)) println(stones.values.sum())
+        stones = buildMap {
+            for ((stone, n) in stones) when {
+                stone == 0L -> listOf(1L)
+                "$stone".length % 2 == 0 -> "$stone".let { it.chunked(it.length / 2).map(String::toLong) }
+                else -> listOf(stone * 2024)
+            }.forEach { compute(it) { _, oldN -> (oldN ?: 0) + n } }
+        }
+        if ( i+1 in listOf(25, 75)) println(stones.values.sum())
     }
 }
