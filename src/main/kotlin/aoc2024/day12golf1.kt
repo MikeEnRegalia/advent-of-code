0bd435af2050268f50a1250e45e6cfb357a79a4c
Michael Böiers <michael.boiers2@ingka.com>
2024-12-12 16:31:03 +0100

    refactor

--- src/main/kotlin/aoc2024/day12golf1.kt
+++ src/main/kotlin/aoc2024/day12golf1.kt
@@ -6,14 +6,12 @@ typealias Area = Pair<Char, Set<Plot>>
 fun main() {
     val grid = generateSequence(::readLine).toList()
 
-    fun Plot.adj() = sequenceOf(
+    fun Plot.neighbors() = sequenceOf(
         copy(first = first - 1),
         copy(first = first + 1),
         copy(second = second - 1),
         copy(second = second + 1)
-    )
-
-    fun Plot.neighbors() = adj().filter { grid.getOrNull(it.second)?.getOrNull(it.first) == grid[second][first] }
+    ).filter { grid.getOrNull(it.second)?.getOrNull(it.first) == grid[second][first] }
 
     val allPlots = grid.flatMapIndexed { i, l -> l.indices.map { Pair(it, i) } }
 
@@ -22,12 +20,10 @@ fun main() {
         if (areas.any { plot in it.second }) continue
         areas += Area(grid[plot.second][plot.first], buildSet {
             add(plot)
-            var newPlots: List<Plot>
-            do {
-                newPlots = flatMap(Plot::neighbors).filter { it !in this }
-                addAll(newPlots)
-            } while (newPlots.isNotEmpty())
+            while (true)
+                flatMap(Plot::neighbors).filterNot(this::contains).takeIf { it.isNotEmpty() }?.also(::addAll) ?: break
         })
     }
+
     println(areas.sumOf { it.second.size * it.second.sumOf { 4 - it.neighbors().count() } })
 }
