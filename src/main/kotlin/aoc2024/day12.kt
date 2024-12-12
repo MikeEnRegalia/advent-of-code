38bd60f8ea770f4aa69c83397a36fbd95054f5d9
Michael Böiers <michael.boiers2@ingka.com>
2024-12-12 15:32:27 +0100

    refactor

--- src/main/kotlin/aoc2024/day12.kt
+++ src/main/kotlin/aoc2024/day12.kt
@@ -3,22 +3,21 @@ package aoc2024
 fun main() {
     val grid = generateSequence(::readLine).toList()
 
-    data class Plot(val x: Int, val y: Int)
-
-    fun Plot.adj() = sequenceOf(copy(x = x - 1), copy(x = x + 1), copy(y = y - 1), copy(y = y + 1))
-    fun Plot.neighbors() = adj().filter { grid.getOrNull(it.y)?.getOrNull(it.x) == grid[y][x] }
+    data class Plot(val x: Int, val y: Int) {
+        fun adj() = sequenceOf(copy(x = x - 1), copy(x = x + 1), copy(y = y - 1), copy(y = y + 1))
+        fun neighbors() = adj().filter { grid.getOrNull(it.y)?.getOrNull(it.x) == grid[y][x] }
+    }
 
     data class Area(val id: Char, val plots: Set<Plot>)
 
     val areas = mutableSetOf<Area>()
-
     for (y in grid.indices) for (x in grid[0].indices) {
         val plot = Plot(x, y).takeIf { areas.none { a -> it in a.plots } } ?: continue
         val areaPlots = mutableSetOf(plot)
 
         var newPlots: List<Plot>
         do {
-            newPlots = areaPlots.flatMap { it.neighbors() }.filter { it !in areaPlots }
+            newPlots = areaPlots.flatMap(Plot::neighbors).filter { it !in areaPlots }
             areaPlots += newPlots
         } while (newPlots.isNotEmpty())
 
@@ -28,21 +27,19 @@ fun main() {
 
     data class Fence(val facing: Char, val plot: Plot)
 
-    fun Plot.fences() = adj()
-        .filter { grid.getOrNull(it.y)?.getOrNull(it.x) != grid[y][x] }
-        .map { o ->
-            val facing = when {
-                o.x == x - 1 -> 'W'
-                o.x == x + 1 -> 'E'
-                o.y == y - 1 -> 'N'
-                o.y == y + 1 -> 'S'
-                else -> throw IllegalStateException()
-            }
-            Fence(facing, this)
-        }
+    fun Plot.facing(o: Plot) = when {
+        o.x == x - 1 -> 'W'
+        o.x == x + 1 -> 'E'
+        o.y == y - 1 -> 'N'
+        o.y == y + 1 -> 'S'
+        else -> throw IllegalStateException()
+    }
 
-    fun Area.fenceSides(): Set<Set<Fence>> = plots.flatMap(Plot::fences).let { areaFences ->
-        buildSet {
+    fun Plot.fences() =
+        adj().filter { grid.getOrNull(it.y)?.getOrNull(it.x) != grid[y][x] }.map { Fence(facing(it), this) }
+
+    fun Area.fenceSides(): Set<Set<Fence>> = buildSet {
+        plots.flatMap(Plot::fences).let { areaFences ->
             for (fence in areaFences) {
                 if (any { fence in it }) continue
 
