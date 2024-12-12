310c5ddeae301a7236a11b417e2968b603ae38e0
Michael Böiers <michael.boiers2@ingka.com>
2024-12-12 21:52:24 +0100

    refactor

--- src/main/kotlin/aoc2024/day12golf1.kt
+++ src/main/kotlin/aoc2024/day12golf1.kt
@@ -1,29 +1,26 @@
 package aoc2024
 
 typealias Plot = Pair<Int, Int>
-typealias Area = Pair<Char, Set<Plot>>
 
 fun main() {
     val grid = generateSequence(::readLine).toList()
 
-    fun Plot.neighbors() = sequenceOf(
-        copy(first = first - 1),
-        copy(first = first + 1),
-        copy(second = second - 1),
-        copy(second = second + 1)
-    ).filter { grid.getOrNull(it.second)?.getOrNull(it.first) == grid[second][first] }
+    fun Plot.neighbors() =
+        sequenceOf(first - 1 to second, first + 1 to second, first to second - 1, first to second + 1)
+            .filter { grid.getOrNull(it.second)?.getOrNull(it.first) == grid[second][first] }
 
-    val allPlots = grid.flatMapIndexed { i, l -> l.indices.map { Pair(it, i) } }
-
-    val areas = mutableSetOf<Area>()
-    for (plot in allPlots) {
-        if (areas.any { plot in it.second }) continue
-        areas += Area(grid[plot.second][plot.first], buildSet {
-            add(plot)
-            while (true)
-                flatMap(Plot::neighbors).filterNot(this::contains).takeIf { it.isNotEmpty() }?.also(::addAll) ?: break
-        })
-    }
-
-    println(areas.sumOf { it.second.size * it.second.sumOf { 4 - it.neighbors().count() } })
+    println(
+        grid
+            .flatMapIndexed { i, l -> l.indices.map { it to i } }
+            .fold(mutableSetOf<Pair<Char, Set<Plot>>>()) { acc, plot ->
+                if (acc.none { plot in it.second })
+                    acc += grid[plot.second][plot.first] to buildSet {
+                        add(plot)
+                        while (true) flatMap(Plot::neighbors).filterNot(::contains).takeIf { it.isNotEmpty() }
+                            ?.also(::addAll)
+                            ?: break
+                    }
+                acc
+            }.sumOf { it.second.size * it.second.sumOf { 4 - it.neighbors().count() } }
+    )
 }
