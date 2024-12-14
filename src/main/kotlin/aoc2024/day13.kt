70fff1b21fc3e8b9d585b277375c6bb77fcc9445
Michael Böiers <michael.boiers2@ingka.com>
2024-12-14 23:42:03 +0100

    refactor

--- src/main/kotlin/aoc2024/day13.kt
+++ src/main/kotlin/aoc2024/day13.kt
@@ -8,45 +8,40 @@ fun main() {
         .map { it.split(" ").map { it.filter { it.isDigit() } }.filter { it.isNotBlank() }.map(String::toInt) }
         .chunked(3).toList()
 
-    fun gcd(numbers: List<Long>): Long {
-        require(numbers.isNotEmpty()) { "List must not be empty" }
-        var result = numbers[0]
-        for (i in 1 until numbers.size) {
-            var num1 = result
-            var num2 = numbers[i]
-            while (num2 != 0L) {
-                val temp = num2
-                num2 = num1 % num2
-                num1 = temp
-            }
-            result = num1
-        }
-        return result
-    }
-
-    fun solve(add: Long = 0L) = machines.sumOf { (a, b, p) ->
+    fun solve(add: Long = 0L) = machines.parallelStream().map { (a, b, p) ->
         val (ax, ay) = a
         val (bx, by) = b
         val (__px, __py) = p
         val (px, py) = __px + add to __py + add
 
-
         val maxA = min((px / ax), (py / ay))
-        val maxB = min((px / bx), (py / by))
 
-        val push = (maxA downTo 0).firstNotNullOfOrNull { fa ->
+        var push: Pair<Long, Long>? = null
+        for (fa in 0..maxA) {
             val rx = px - fa * ax
             val ry = py - fa * ay
-            if (rx % bx != 0L || ry % by != 0L || rx / bx != ry / by) null else fa to rx / bx
+            if (rx % bx != 0L || ry % by != 0L || rx/bx != ry/by)  continue
+
+            val fb = rx/bx
+
+            val tx = fa * ax + fb * bx
+            val ty = fa * ay + fb * by
+            val fx = px / tx
+            val fy = py / ty
+
+            if (px % tx == 0L && py % ty == 0L && fx == fy) {
+                push = fa * fx to fb * fx
+                break
+            }
         }
 
-        val cost = push?.let { (fa, fb) ->fa * 3L + fb }
+        val cost = push?.let { (fa, fb) -> fa * 3L + fb }
 
-        println("$maxA x $a $maxB x $b = $p: $push -> $cost")
+        println("$a x $b = $p: $cost $push")
 
         cost ?: 0L
-    }
+    }.reduce(Long::plus)
 
     println(solve())
-    //println(solve(10_000_000_000_000L))
+    println(solve(10_000_000_000_000L))
 }
