c4f6896bc48c7c42ac0ef78647f43fdaf4e9c580
Michael Böiers <michael.boiers2@ingka.com>
2024-12-16 12:00:52 +0100

    solved day 16 part 2

--- src/main/kotlin/aoc2024/day16.kt
+++ src/main/kotlin/aoc2024/day16.kt
@@ -2,7 +2,7 @@ package aoc2024
 
 import kotlin.math.min
 
-private const val DIRECTIONS = "NESW"
+private const val DIR = "NESW"
 
 fun main() {
     data class Point(val x: Int, val y: Int)
@@ -10,38 +10,60 @@ fun main() {
     val grid = generateSequence(::readLine).flatMapIndexed { y, s -> s.mapIndexed { x, c -> Point(x, y) to c } }.toMap()
 
     data class State(val pos: Point, val facing: Char) {
-        fun next() = sequenceOf(forward(), turnLeft(), turnRight()).filter { grid.getValue(it.pos) in ".SE" }
+        fun next() = when {
+            grid[pos] == 'E' -> sequenceOf()
+            else -> sequenceOf(forward(), turn(3), turn(1)).filterNotNull()
+        }
+
         fun forward() = when (facing) {
             'N' -> copy(pos = pos.copy(y = pos.y - 1))
             'E' -> copy(pos = pos.copy(x = pos.x + 1))
             'S' -> copy(pos = pos.copy(y = pos.y + 1))
             'W' -> copy(pos = pos.copy(x = pos.x - 1))
             else -> throw IllegalArgumentException(facing.toString())
-        }
+        }.takeIf { grid[it.pos].let { it != null && it in ".E" } }
 
-        fun turnLeft() = copy(facing = DIRECTIONS[(DIRECTIONS.indexOf(facing) - 1 + 4) % 4])
-        fun turnRight() = copy(facing = DIRECTIONS[(DIRECTIONS.indexOf(facing) + 1) % 4])
+        fun turn(d: Int) = copy(facing = DIR[(DIR.indexOf(facing) + d) % DIR.length]).takeIf { it.forward() != null }
     }
 
-    fun cost(s1: State, s2: State) = when {
-        s1.facing == s2.facing -> 1
-        else -> 1000
-    }
+    var curr = State(grid.filterValues { it == 'S' }.keys.single(), 'E')
 
-    fun part1(): Int? {
-        var curr = State(grid.filterValues { it == 'S' }.keys.single(), 'E')
-        val V = mutableSetOf(curr)
-        val D = mutableMapOf(curr to 0)
+    val V = mutableSetOf(curr)
+    val D = mutableMapOf(curr to 0)
+    val U = mutableSetOf<State>()
+    val P = mutableMapOf<State, Set<State>>()
 
-        while (true) {
-            curr.next().filter { it !in V }.forEach {
-                D.compute(it) { _, old -> min(old ?: Int.MAX_VALUE, D.getValue(curr) + cost(curr, it)) }
+    var shortestPath = Int.MAX_VALUE
+    while (true) {
+        curr.next().filter { it !in V }.forEach {
+            val cost = D.getValue(curr) + when {
+                curr.facing == it.facing -> 1
+                else -> 1000
             }
-            V += curr
-            if (grid[curr.pos] == 'E') return D.getValue(curr)
-            curr = D.filterKeys { it !in V}.minByOrNull { it.value }?.key ?: return null
+            if (cost <= shortestPath) {
+                U += it
+                val prevCost = D[it]
+                D.compute(it) { _, old -> min(old ?: Int.MAX_VALUE, cost) }
+                when {
+                    prevCost == null || cost < prevCost -> P[it] = setOf(curr)
+                    cost == prevCost -> P[it] = P.getValue(it) + curr
+                }
+            }
         }
+
+        V += curr
+        U -= curr
+
+        if (grid[curr.pos] == 'E')
+            shortestPath = min(shortestPath, D.getValue(curr))
+
+        curr = U.minByOrNull { D.getValue(it) } ?: break
     }
 
-    println(part1())
+    println(shortestPath)
+
+    with(P.filterKeys { grid[it.pos] == 'E' }.keys.toMutableSet()) {
+        while (true) if (!addAll(mapNotNull { P[it] }.flatten())) break
+        println(map { it.pos }.toSet().size)
+    }
 }
