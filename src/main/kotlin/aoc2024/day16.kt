ab9cf1427016d26965b0cf9bb5fc0bcbdabc4690
Michael Böiers <michael.boiers2@ingka.com>
2024-12-16 12:32:43 +0100

    refactor

--- src/main/kotlin/aoc2024/day16.kt
+++ src/main/kotlin/aoc2024/day16.kt
@@ -2,19 +2,13 @@ package aoc2024
 
 import kotlin.math.min
 
-private const val DIR = "NESW"
-
 fun main() {
     data class Point(val x: Int, val y: Int)
 
     val grid = generateSequence(::readLine).flatMapIndexed { y, s -> s.mapIndexed { x, c -> Point(x, y) to c } }.toMap()
 
     data class State(val pos: Point, val facing: Char) {
-        fun next() = when {
-            grid[pos] == 'E' -> sequenceOf()
-            else -> sequenceOf(forward(), turn(3), turn(1)).filterNotNull()
-        }
-
+        fun next() = sequenceOf(forward(), turn(3), turn(1)).filterNotNull()
         fun forward() = when (facing) {
             'N' -> copy(pos = pos.copy(y = pos.y - 1))
             'E' -> copy(pos = pos.copy(x = pos.x + 1))
@@ -23,7 +17,7 @@ fun main() {
             else -> throw IllegalArgumentException(facing.toString())
         }.takeIf { grid[it.pos].let { it != null && it in ".E" } }
 
-        fun turn(d: Int) = copy(facing = DIR[(DIR.indexOf(facing) + d) % DIR.length]).takeIf { it.forward() != null }
+        fun turn(d: Int) = copy(facing = "NESW".let { it[(it.indexOf(facing) + d) % it.length] })
     }
 
     var curr = State(grid.filterValues { it == 'S' }.keys.single(), 'E')
@@ -35,11 +29,8 @@ fun main() {
 
     var shortestPath = Int.MAX_VALUE
     while (true) {
-        curr.next().filter { it !in V }.forEach {
-            val cost = D.getValue(curr) + when {
-                curr.facing == it.facing -> 1
-                else -> 1000
-            }
+        curr.next()?.filter { it !in V }?.forEach {
+            val cost = D.getValue(curr) + if (curr.facing == it.facing) 1 else 1000
             if (cost <= shortestPath) {
                 U += it
                 val prevCost = D[it]
