bc92bf81e7bf8776b00aa70a4ae68d665739afa8
Michael Böiers <michael.boiers2@ingka.com>
2024-12-18 14:16:23 +0100

    refactor

--- src/main/kotlin/aoc2024/day18.kt
+++ src/main/kotlin/aoc2024/day18.kt
@@ -5,28 +5,25 @@ import kotlin.math.min
 fun main() {
     data class Point(val x: Int, val y: Int)
 
-    val (X, Y) = 70 to 70
-    val start = Point(0, 0)
-    val end = Point(X, Y)
-
     val allObstacles = generateSequence(::readLine)
         .map { it.split(",").map(String::toInt).let { (x, y) -> Point(x, y) } }.toList()
 
     val obstacles = allObstacles.take(1024).toMutableSet()
+    val (topLeft, lowerRight) = Point(0, 0) to Point(70, 70)
 
     fun solve(): Int? {
-        val U = mutableSetOf(start)
-        val D = mutableMapOf(start to 0)
+        val U = mutableSetOf(topLeft)
+        val D = mutableMapOf(topLeft to 0)
         val V = mutableSetOf<Point>()
 
         while (U.isNotEmpty()) {
             val curr = U.minBy(D::getValue)
             with(curr) { sequenceOf(copy(x = x + 1), copy(y = y + 1), copy(x = x - 1), copy(y = y - 1)) }
-                .filter { it.x in 0..X && it.y in 0..Y && it !in obstacles && it !in V }
+                .filter { it.x in 0..lowerRight.x && it.y in 0..lowerRight.y && it !in obstacles && it !in V }
                 .onEach { D[it] = min(D.getValue(curr) + 1, D[it] ?: Int.MAX_VALUE) }
                 .also(U::addAll)
 
-            if (curr == end) return D[curr]
+            if (curr == lowerRight) return D[curr]
             V += curr
             U -= curr
         }
@@ -36,6 +33,6 @@ fun main() {
 
     println(solve())
 
-    allObstacles.asSequence().drop(1024).onEach(obstacles::add).first { solve() == null }
+    allObstacles.drop(1024).asSequence().onEach(obstacles::add).first { solve() == null }
         .also { (x, y) -> println("$x,$y") }
 }
