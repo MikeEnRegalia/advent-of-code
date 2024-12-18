b49a9a718fbfb2ed9b0a7e335e2cd995d54d854e
Michael Böiers <michael.boiers2@ingka.com>
2024-12-18 11:15:20 +0100

    refactor

--- src/main/kotlin/aoc2024/day18.kt
+++ src/main/kotlin/aoc2024/day18.kt
@@ -3,15 +3,14 @@ package aoc2024
 import kotlin.math.min
 
 fun main() {
-    val (X, Y) = 70 to 70
     data class Point(val x: Int, val y: Int)
 
+    val (X, Y) = 70 to 70
     val start = Point(0, 0)
     val end = Point(X, Y)
 
-    val allObstacles = generateSequence(::readLine).map {
-        it.split(",").map(String::toInt).let { (x, y) -> Point(x, y) }
-    }.toList()
+    val allObstacles = generateSequence(::readLine)
+        .map { it.split(",").map(String::toInt).let { (x, y) -> Point(x, y) } }.toList()
 
     val obstacles = allObstacles.take(1024).toMutableSet()
 
@@ -20,22 +19,17 @@ fun main() {
         val D = mutableMapOf(start to 0)
         val V = mutableSetOf<Point>()
 
-        do {
-            val curr = U.minBy { D.getValue(it) }
+        while (U.isNotEmpty()) {
+            val curr = U.minBy(D::getValue)
             with(curr) { sequenceOf(copy(x = x + 1), copy(y = y + 1), copy(x = x - 1), copy(y = y - 1)) }
-                .filter { it.x in 0..X && it.y in 0..Y }
-                .filter { it !in obstacles }
-                .filter { it !in V }
-                .forEach { next ->
-                    U += next
-                    val d = D.getValue(curr) + 1
-                    val prevD = D[next] ?: Int.MAX_VALUE
-                    D[next] = min(d, prevD)
-                }
+                .filter { it.x in 0..X && it.y in 0..Y && it !in obstacles && it !in V }
+                .onEach { D[it] = min(D.getValue(curr) + 1, D[it] ?: Int.MAX_VALUE) }
+                .also(U::addAll)
+
             if (curr == end) return D[curr]
             V += curr
             U -= curr
-        } while (U.isNotEmpty())
+        }
 
         return null
     }
