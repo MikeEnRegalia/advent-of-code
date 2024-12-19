f83ac5a44931c3ee306a0479b8b3cd24d4b3dd5e
Michael Böiers <michael.boiers2@ingka.com>
2024-12-19 12:14:45 +0100

    golf day 19

--- src/main/kotlin/aoc2024/day19.kt
+++ src/main/kotlin/aoc2024/day19.kt
@@ -1,22 +1,12 @@
 package aoc2024
 
 fun main() {
-    val (towels, patterns) = with(generateSequence(::readLine).toList()) { first().split(", ") to drop(2) }
+    val (towels, designs) = with(generateSequence(::readLine).toList()) { first().split(", ") to drop(2) }
+    val cache = mutableMapOf("" to 0L)
 
-    val cache = mutableMapOf<String, Long>()
-
-    fun String.match(): Long = cache.getOrPut(this) {
-        towels.sumOf { towel ->
-            when {
-                !startsWith(towel) -> 0L
-                length == towel.length -> 1L
-                else -> drop(towel.length).match()
-            }
-        }
+    fun String.countCombinations(): Long = cache.getOrPut(this) {
+        towels.filter(::startsWith).sumOf { if (this == it) 1 else removePrefix(it).countCombinations() }
     }
 
-    with(patterns.map(String::match)) {
-        println(count { it > 0 })
-        println(sum())
-    }
+    with(designs.map(String::countCombinations)) { listOf(count { it > 0 }, sum()).forEach(::println) }
 }
