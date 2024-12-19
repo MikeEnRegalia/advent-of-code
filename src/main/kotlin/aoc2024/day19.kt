2a118f14e78745571bc61e0c29b065093fbc941f
Michael Böiers <michael.boiers2@ingka.com>
2024-12-19 09:30:51 +0100

    refactor

--- src/main/kotlin/aoc2024/day19.kt
+++ src/main/kotlin/aoc2024/day19.kt
@@ -1,18 +1,19 @@
 package aoc2024
 
 fun main() {
-    val lines = generateSequence(::readLine).toList()
-    val towels = lines.first().split(", ")
-    val patterns = lines.drop(2)
+    val (towels, patterns) = with(generateSequence(::readLine).toList()) { first().split(", ") to drop(2) }
+
     val cache = mutableMapOf<String, Long>()
 
-    fun String.match(): Long = cache[this] ?: towels.sumOf { towel ->
-        when {
-            !startsWith(towel) -> 0L
-            length == towel.length -> 1L
-            else -> drop(towel.length).match()
+    fun String.match(): Long = cache.getOrPut(this) {
+        towels.sumOf { towel ->
+            when {
+                !startsWith(towel) -> 0L
+                length == towel.length -> 1L
+                else -> drop(towel.length).match()
+            }
         }
-    }.also { cache[this] = it }
+    }
 
     with(patterns.map(String::match)) {
         println(count { it > 0 })
