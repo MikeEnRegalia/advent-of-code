e8523503131e0403c1914685b20c87a71f9299fb
Michael Böiers <michael.boiers2@ingka.com>
2024-12-19 08:38:13 +0100

    build day 19 part 2

--- src/main/kotlin/aoc2024/day19.kt
+++ src/main/kotlin/aoc2024/day19.kt
@@ -5,15 +5,32 @@ fun main() {
     val towels = lines.first().split(", ")
     val patterns = lines.drop(2)
 
-    val cache = mutableMapOf<String, Boolean>()
-    val part1 = patterns.count { pattern ->
-        fun foo(p: String): Boolean = when {
-            p.isBlank() -> true
-            cache[p] != null -> cache[p]!!
-            else -> towels.filter { p.startsWith(it) }.any { foo(p.drop(it.length)) }
-        }.also { cache[p] = it }
-        foo(pattern)
+    val cache = mutableMapOf<String, List<List<String>>>()
+
+    fun match(p: String): List<List<String>>? {
+        return when {
+            p.isBlank() -> listOf()
+            towels.none { p.startsWith(it) } -> null
+            p in cache -> cache.getValue(p)
+            else -> towels
+                .mapNotNull { if (p.startsWith(it)) it to p.drop(it.length) else null }
+                .mapNotNull { (t, newP) ->
+                    val match = match(newP) ?: return@mapNotNull null
+                    if (match.isEmpty()) listOf(listOf(t)) else {
+                        //println("matching $p with $t: $match")
+                        match.map {
+                            //println("match $p: $it")
+                            listOf(t) + it
+                        }
+                    }
+                }
+                .flatten()
+                .takeIf { it.isNotEmpty() }
+                ?.also { cache[p] = it }
+        }
     }
 
-    println(part1)
+    println(patterns.count { p -> match(p).also { println("$p -> $it")}?.isNotEmpty() == true })
+
+    println(cache.filterKeys { it in patterns }.values.sumOf { it.size})
 }
