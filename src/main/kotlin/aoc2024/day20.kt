ad9757fec7f0c25caf2252b40eb7eba9c0bb022c
Michael Böiers <michael.boiers2@ingka.com>
2024-12-20 12:00:48 +0100

    solved day 20

--- src/main/kotlin/aoc2024/day20.kt
+++ src/main/kotlin/aoc2024/day20.kt
@@ -1,5 +1,7 @@
 package aoc2024
 
+import kotlin.math.abs
+
 fun main() {
     data class Point(val x: Int, val y: Int)
 
@@ -7,42 +9,51 @@ fun main() {
     fun Point.next() = sequenceOf(copy(x = x + 1), copy(y = y + 1), copy(x = x - 1), copy(y = y - 1))
         .filter { it in grid }
 
-    fun Point.walk(): List<Point> = next().filter { grid[it] != '#' }.toList()
+    fun solve(start: Point): Map<Point, Int> {
+        fun Point.walk(): List<Point> = next().filter { grid[it] != '#' }.toList()
 
-    val start = grid.filterValues { it == 'S' }.keys.single()
+        val V = mutableSetOf<Point>()
+        val D = mutableMapOf(start to 0)
+        val U = mutableSetOf(start)
+        val P = mutableMapOf<Point, Set<Point>>()
 
-    val V = mutableSetOf<Point>()
-    val D = mutableMapOf(start to 0)
-    val U = mutableSetOf(start)
-    val P = mutableMapOf<Point, Set<Point>>()
+        do {
+            val curr = U.minBy { D.getValue(it) }
 
-    do {
-        val curr = U.minBy { D.getValue(it) }
+            for (next in curr.walk().filter { it !in V }) {
+                U += next
+                val (prevCost, cost) = D[next] to D.getValue(curr) + 1
+                when {
+                    prevCost == null || cost < prevCost -> {
+                        D[next] = cost
+                        P[next] = setOf(curr)
+                    }
 
-        for (next in curr.walk().filter { it !in V }) {
-            U += next
-            val (prevCost, cost) = D[next] to D.getValue(curr) + 1
-            when {
-                prevCost == null || cost < prevCost -> {
-                    D[next] = cost
-                    P[next] = setOf(curr)
+                    cost == prevCost -> P[next] = P.getValue(next) + curr
                 }
-
-                cost == prevCost -> P[next] = P.getValue(next) + curr
             }
+
+            V += curr
+            U -= curr
+        } while (U.isNotEmpty())
+        return D
+    }
+
+    val D = solve(grid.filterValues { it == 'S' }.keys.single())
+
+    val path = grid.filter { it.key in D.keys && it.value in "S.E" }.keys
+
+    fun Point.surrounding(d: Int) = (-d..d).flatMap { dy ->
+        (-d..d).mapNotNull { dx -> Point(x + dx, y + dy).takeIf { it != this && abs(it.x - x) + abs(it.y - y) <= d } }
+    }
+
+    fun savings(maxD: Int, min: Int) = path.flatMap { s ->
+        s.surrounding(maxD).filter(path::contains).mapNotNull { e ->
+            val d = abs(e.x - s.x) + abs(e.y - s.y)
+            (D.getValue(e) - D.getValue(s) - d).takeIf { it >= min }
         }
+    }
 
-        V += curr
-        U -= curr
-    } while (U.isNotEmpty())
-
-    val cheatSavings = grid.filter { it.key in D.keys && it.value != '#' }.keys.flatMap { s ->
-        s.next().filter { grid[it] == '#' }.flatMap { o ->
-            o.next().filter { it in D.keys && grid[it] != '#' && D.getValue(it) > D.getValue(s) }.map { e ->
-                D.getValue(e) - D.getValue(s) - 2
-            }
-        }
-    }.filter { it > 0 }
-
-    println(cheatSavings.count { it >= 100 })
+    println(savings(2, 100).count())
+    println(savings(20, 100).count())
 }
