5135de885df2f5ce2ca3c3231d15d524ffab79c6
Michael Böiers <michael.boiers2@ingka.com>
2025-10-14 13:57:27 +0200

    refactor

--- src/main/kotlin/aoc2024/day23.kt
+++ src/main/kotlin/aoc2024/day23.kt
@@ -4,16 +4,16 @@ fun main() {
     val links = generateSequence(::readlnOrNull).map { it.split("-").toSet() }.toSet()
     fun Set<String>.areAllConnectedTo(other: String) = all { setOf(it, other) in links }
 
-    val computers = links.flatten().toSet()
+    val nodes = links.flatten().toSet()
 
     links.filter { link -> link.any { it.startsWith("t") } }
-        .flatMap { link -> computers.filter { link.areAllConnectedTo(it) }.map { link + it } }
+        .flatMap { link -> nodes.filter { link.areAllConnectedTo(it) }.map { link + it } }
         .distinct().count()
         .also(::println)
 
-    val parties = computers.map { mutableSetOf(it) }.onEach {
-        it.addAll(generateSequence { computers.firstOrNull(it::areAllConnectedTo) })
-    }
-
-    println(parties.maxBy { it.size }.sorted().joinToString(","))
+    nodes.map { mutableSetOf(it) }
+        .onEach { it.addAll(generateSequence { nodes.firstOrNull(it::areAllConnectedTo) }) }
+        .maxBy { it.size }
+        .sorted().joinToString(",")
+        .also(::println)
 }
