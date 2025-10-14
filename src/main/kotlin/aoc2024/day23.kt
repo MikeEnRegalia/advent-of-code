42710fdd3151e414786fdce27eabfb716978eb0f
Michael Böiers <michael.boiers2@ingka.com>
2025-10-14 11:45:31 +0200

    refactor

--- src/main/kotlin/aoc2024/day23.kt
+++ src/main/kotlin/aoc2024/day23.kt
@@ -2,22 +2,20 @@ package aoc2024
 
 fun main() {
     val links = generateSequence { readlnOrNull()?.split("-")?.toSet() }.toSet()
-
     val nodes = links.flatten().toSet()
 
-    links.asSequence()
+    links
         .filter { it.any { n -> n.startsWith("t") } }
         .flatMap { link -> nodes.filter { c -> link.all { n -> setOf(n, c) in links } }.map { link + it } }
-        .toSet()
+        .distinct()
         .count()
         .also(::println)
 
-    val bigParties = buildSet {
-        for (computer in nodes) add(buildSet {
-            add(computer)
-            generateSequence { nodes.firstOrNull { node -> all { setOf(it, node) in links } } }
+    val bigParties = nodes.map {
+        mutableSetOf(it).apply {
+            generateSequence { nodes.firstOrNull { new -> all { n -> setOf(n, new) in links } } }
                 .forEach(::add)
-        })
+        }
     }
 
     println(bigParties.maxBy { it.size }.sorted().joinToString(","))
