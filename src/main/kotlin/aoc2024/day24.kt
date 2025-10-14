920933ec1986405b7f7739efbc4221c0b3d0e1af
Michael Böiers <michael.boiers2@ingka.com>
2025-10-14 15:48:53 +0200

    refactor

--- src/main/kotlin/aoc2024/day24.kt
+++ src/main/kotlin/aoc2024/day24.kt
@@ -4,13 +4,14 @@ fun main() {
     val lines = generateSequence(::readLine).toList()
 
     val initialWires = lines.filter { ':' in it }.associate { it.split(": ").let { it[0] to (it[1].toInt() == 1) } }
-    val rules = lines.filter { "->" in it }.map { it.split(" ").filter { it != "->" } }
+    val rules = lines.filter { "->" in it }.map { it.split(" ") }
 
     val targets = rules.map { it.last() }.filter { it.startsWith("z") }.sortedDescending()
+
     val wires = initialWires.toMutableMap()
 
     while (targets.any { it !in wires }) {
-        for ((aWire, op, bWire, resultWire) in rules) {
+        for ((aWire, op, bWire, _, resultWire) in rules) {
             val a = wires[aWire] ?: continue
             val b = wires[bWire] ?: continue
 
