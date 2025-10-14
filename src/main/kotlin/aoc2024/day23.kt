d935981d2e1f34e94b4505c28fcd03470d0a04b0
Michael Böiers <michael.boiers2@ingka.com>
2025-10-14 11:33:09 +0200

    refactor

--- src/main/kotlin/aoc2024/day23.kt
+++ src/main/kotlin/aoc2024/day23.kt
@@ -3,35 +3,25 @@ package aoc2024
 fun main() {
     val links = generateSequence { readlnOrNull()?.split("-")?.toSet() }.toSet()
 
-    val computers = links.flatten().toSet()
+    val nodes = links.flatten().toSet()
 
-    val tLinks = links.filter { link -> link.any { it.startsWith("t") } }
-
-    val parties = buildSet {
-        for (link in tLinks) for (c in computers) {
-            if (link.all { setOf(it, c) in links })
-                add(link + c)
-        }
-    }
-
-    println(parties.count())
+    links.asSequence()
+        .filter { it.any { n -> n.startsWith("t") } }
+        .flatMap { link -> nodes.filter { c -> link.all { n -> setOf(n, c) in links } }.map { link + it } }
+        .toSet()
+        .count()
+        .also(::println)
 
     val bigParties = mutableSetOf<Set<String>>()
 
-    for (computer in computers) {
+    for (computer in nodes) {
         if (bigParties.any { computer in it }) continue
-
-        val party = buildSet {
+        bigParties.add(buildSet {
             add(computer)
             do {
-                computers
-                    .filter { it !in this }
-                    .firstOrNull { node -> all { setOf(it, node) in links } }
-                    ?.let { add(it) } ?: break
-
+                nodes.firstOrNull { node -> all { setOf(it, node) in links } }?.let { add(it) } ?: break
             } while (true)
-        }
-        bigParties.add(party)
+        })
     }
 
     println(bigParties.maxBy { it.size }.sorted().joinToString(","))
