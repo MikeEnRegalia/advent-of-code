33e2a5c7b0701204773133688dfb90726e6e3a7e
Michael Böiers <michael.boiers2@ingka.com>
2025-10-15 11:27:45 +0200

    work on 2024 day 24

--- src/main/kotlin/aoc2024/day24.kt
+++ src/main/kotlin/aoc2024/day24.kt
@@ -3,15 +3,15 @@ package aoc2024
 fun main() {
     val lines = generateSequence(::readLine).toList()
 
-    val initialWires = lines.filter { ':' in it }.associate { it.split(": ").let { it[0] to (it[1].toInt() == 1) } }
-    val rules = lines.filter { "->" in it }.map { it.split(" ") }
+    val init = lines.filter { ':' in it }.associate { it.split(": ").let { it[0] to (it[1].toInt() == 1) } }
+    val gates = lines.filter { "->" in it }.map { it.split(" ") }
 
-    val targets = rules.map { it.last() }.filter { it.startsWith("z") }.sortedDescending()
+    val targetWires = gates.map { it.last() }.filter { it.startsWith("z") }.sortedDescending()
 
-    val wires = initialWires.toMutableMap()
+    val wires = init.toMutableMap()
 
-    while (targets.any { it !in wires }) {
-        for ((aWire, op, bWire, _, resultWire) in rules) {
+    while (targetWires.any { it !in wires }) {
+        for ((aWire, op, bWire, _, resultWire) in gates) {
             val a = wires[aWire] ?: continue
             val b = wires[bWire] ?: continue
 
@@ -23,5 +23,23 @@ fun main() {
         }
     }
 
-    println(targets.joinToString("") { if (wires[it] == true) "1" else "0" }.toLong(2))
+    println(targetWires.joinToString("") { if (wires[it] == true) "1" else "0" }.toLong(2))
+
+    val lastWire = "z${targetWires.lastIndex}"
+
+    fun String.connectedOperations() = gates.mapNotNull { (a, op, b) ->
+        if (listOf(a, b).any { it == this }) op else null
+    }
+
+    val bogus = gates.filterNot { (_, op, _, _, wire) ->
+        when (op) {
+            "OR" -> wire == lastWire || "XOR" in wire.connectedOperations()
+            "AND" -> "OR" in wire.connectedOperations()
+            "XOR" -> wire in targetWires || "XOR" in wire.connectedOperations()
+            else -> true
+        }
+    }.onEach(::println)
+
+    // not dgr,dtv,mtj,njb,vvm,z12,z29,z37
+    println(bogus.map { it.last() }.sorted().joinToString(","))
 }
