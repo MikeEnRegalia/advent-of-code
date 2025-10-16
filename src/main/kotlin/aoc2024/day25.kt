1d4c6c5690d97099da490f801fd9b25275d7a459
Michael Böiers <michael.boiers2@ingka.com>
2025-10-16 23:00:22 +0200

    refactor

--- src/main/kotlin/aoc2024/day25.kt
+++ src/main/kotlin/aoc2024/day25.kt
@@ -1,7 +1,6 @@
 package aoc2024
 
-fun main() = generateSequence(::readLine).filter(String::isNotBlank)
-    .chunked(7)
+fun main() = generateSequence(::readLine).chunked(8)
     .partition { "#" in it.first() }.toList()
     .map { grids -> grids.map { lines -> lines.subList(1, 6).run { indices.map { i -> count { it[i] == '#' } } } } }
     .run { first().sumOf { lock -> last().count { key -> lock.zip(key).none { (a, b) -> a + b > 5 } } } }
