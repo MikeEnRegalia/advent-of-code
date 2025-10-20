500fccbaf24c129742ba4b9b8a71d49d080ba2a3
Michael Böiers <michael.boiers2@ingka.com>
2025-10-20 09:12:27 +0200

    solve ec2024 q3 part 1 and 2

---
+++ src/main/kotlin/ec2024/q3p1.kt
@@ -0,0 +1,33 @@
+package ec2024
+
+import kotlin.math.abs
+
+fun main() {
+    data class Point(val x: Int, val y: Int) {
+        fun neighbors() = sequenceOf(copy(x = x + 1), copy(x = x - 1), copy(y = y + 1), copy(y = y - 1))
+    }
+
+    val grid = generateSequence(::readLine)
+        .flatMapIndexed { y, row -> row.mapIndexedNotNull { x, value -> if (value == '.') null else Point(x, y) to 0 } }
+        .toMap(mutableMapOf())
+
+    var removed = 0
+
+    fun Point.hasLevelNeighbors() = neighbors().all { neighbor ->
+        abs(grid.getOrDefault(neighbor, 0) - grid.getOrDefault(this, 0)) == 0
+    }
+
+    do {
+        var removedThisTurn = 0
+
+        val remove = grid.keys.filter { it.hasLevelNeighbors() }
+        for (point in remove) {
+            grid[point] = grid.getValue(point) + 1
+            removedThisTurn++
+        }
+
+        removed += removedThisTurn
+    } while (removedThisTurn > 0)
+
+    println(removed)
+}
