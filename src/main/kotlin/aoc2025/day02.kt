95be8f9e6b105adb94c706a635a04129e990e6b0
Michael Böiers <michael.boiers2@ingka.com>
2025-12-02 15:14:27 +0100

    refactor (thanks to ephemient)

--- src/main/kotlin/aoc2025/day02.kt
+++ src/main/kotlin/aoc2025/day02.kt
@@ -1,20 +1,17 @@
 package aoc2025
 
 fun main() {
-    val ranges = readln().split(",")
-        .map { s -> s.split("-").map(String::toLong).let { it[0]..it[1] } }
+    val ranges = readln().split(",").map { s -> s.split("-").map(String::toLong) }
 
     var part1 = 0L
     var part2 = 0L
 
-    for (range in ranges) for (id in range) {
+    for (range in ranges) for (id in range[0]..range[1]) {
         val s = id.toString()
-        for (l in (1..s.length / 2).reversed()) {
+        for (l in s.length / 2 downTo 1) {
             val chunks = s.chunked(l)
             if (chunks.distinct().size == 1) {
-                if (chunks.size == 2) {
-                    part1 += id
-                }
+                if (chunks.size == 2) part1 += id
                 part2 += id
                 break
             }
