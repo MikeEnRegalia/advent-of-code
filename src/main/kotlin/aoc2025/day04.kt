97875e5618ca9bfd393e48976233a39458221d6f
Michael Böiers <michael.boiers2@ingka.com>
2025-12-04 10:13:49 +0100

    refactor

--- src/main/kotlin/aoc2025/day04.kt
+++ src/main/kotlin/aoc2025/day04.kt
@@ -6,7 +6,7 @@ fun main() = generateSequence(::readLine)
         generateSequence {
             grid.filter { (px, py) ->
                 (-1..1).flatMap { y -> (-1..1).map { px + it to py + y } }.count { it in grid } <= 4
-            }.toSet().also { grid -= it }.size.takeIf { it > 0 }
+            }.also { grid -= it }.size.takeIf { it > 0 }
         }.toList().run {
             println(first())
             println(sum())
