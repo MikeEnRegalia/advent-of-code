bf75fee49322ab7608619c7b915a62b7c11c7a23
Michael Böiers <michael.boiers2@ingka.com>
2025-12-04 10:13:32 +0100

    refactor

--- src/main/kotlin/aoc2025/day04.kt
+++ src/main/kotlin/aoc2025/day04.kt
@@ -1,23 +1,14 @@
 package aoc2025
 
-fun main() {
-    val grid = generateSequence(::readLine).flatMapIndexed { y, line ->
-        line.mapIndexedNotNull { x, char -> if (char == '@') x to y else null }
-    }.toMutableSet()
-
-    fun liftable() = grid.filter { (px, py) ->
-        (-1..1).flatMap { y -> (-1..1).map { px + it to py + y } }.count { it in grid } <= 4
-    }.toSet()
-
-    var lifted = 0
-    while (true) {
-        val liftable = liftable()
-        if (lifted == 0) println(liftable.size)
-
-        if (liftable.isEmpty()) break
-        grid -= liftable
-        lifted += liftable.size
+fun main() = generateSequence(::readLine)
+    .flatMapIndexed { y, line -> line.mapIndexedNotNull { x, char -> if (char == '@') x to y else null } }
+    .toMutableSet().let { grid ->
+        generateSequence {
+            grid.filter { (px, py) ->
+                (-1..1).flatMap { y -> (-1..1).map { px + it to py + y } }.count { it in grid } <= 4
+            }.toSet().also { grid -= it }.size.takeIf { it > 0 }
+        }.toList().run {
+            println(first())
+            println(sum())
+        }
     }
-
-    println(lifted)
-}
