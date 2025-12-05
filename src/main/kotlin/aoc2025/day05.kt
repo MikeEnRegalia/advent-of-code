c8b793334e791b8d8f1c9cfbe643f71af44fb4b4
Michael Böiers <michael.boiers2@ingka.com>
2025-12-05 09:27:14 +0100

    refactor

--- src/main/kotlin/aoc2025/day05.kt
+++ src/main/kotlin/aoc2025/day05.kt
@@ -5,8 +5,7 @@ import kotlin.math.min
 
 fun main() {
     val lines = generateSequence(::readLine).toList()
-    val ranges = lines.filter { "-" in it }
-        .map { it.split("-").map(String::toLong).let { it[0]..it[1] } }
+    val ranges = lines.filter { "-" in it }.map { it.split("-").map(String::toLong).let { it[0]..it[1] } }
     val ingredients = lines.filter { it.isNotEmpty() && "-" !in it }.map(String::toLong)
 
     println(ingredients.count { i -> ranges.any { i in it } })
@@ -15,25 +14,18 @@ fun main() {
     fun LongRange.isIncludedIn(other: LongRange) = first in other && last in other
 
     with(ranges.toMutableSet()) {
-        var sum = 0L
-        while (isNotEmpty()) {
-            val redundant = firstOrNull { r -> any { it != r && r.isIncludedIn(it) } }
-            if (redundant != null){
+        while (any { r -> none { it != r && r.overlapsWith(it) } }) {
+            val redundant = filter { r -> any { it != r && r.isIncludedIn(it) } }
+            if (redundant.isNotEmpty()) {
                 this -= redundant
                 continue
             }
 
-            val done = filter { r -> none { it != r && r.overlapsWith(it) } }
-            this -= done.toSet()
-            sum += done.sumOf { it.last + 1 - it.first }
-
-            val r = filter { r -> any { it != r && r.overlapsWith(it) } }.randomOrNull() ?: continue
-            val r2 = filter { it != r && r.overlapsWith(it) }.random()
-
-            this -= r
-            this -= r2
+            val r = firstOrNull { r -> any { it != r && r.overlapsWith(it) } } ?: break
+            val r2 = first { it != r && r.overlapsWith(it) }
             this += min(r.first, r2.first)..max(r.last, r2.last)
         }
-        println(sum)
+        println(sumOf { it.last + 1 - it.first })
     }
 }
+
