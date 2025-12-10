0ad3022d5c31437a1c67ae5fa822d817436c6954
Michael Böiers <michael.boiers2@ingka.com>
2025-12-10 15:20:09 +0100

    solve aoc 2025 day 6 part 2

--- src/main/kotlin/aoc2025/day06.kt
+++ src/main/kotlin/aoc2025/day06.kt
@@ -3,15 +3,26 @@ package aoc2025
 fun main() {
     val lines = generateSequence(::readLine).toList()
 
-    lines.map { it.split(" ").map(String::trim).filter { it.isNotBlank() } }.let { data ->
+    fun String.operation(): (Long, Long) -> Long = if (this == "+") Long::plus else Long::times
+
+    lines.map { it.split(" ").map(String::trim).filter(String::isNotBlank) }.let { data ->
         data.first().indices.sumOf { i ->
-            data.dropLast(1).map { it[i].toLong() }.reduce(
-                when (data.last()[i]) {
-                    "+" -> Long::plus
-                    else -> Long::times
-                }
-            )
+            data.dropLast(1).map { it[i].toLong() }.reduce(data.last()[i].operation())
         }
     }.also(::println)
 
+    val columns = lines.first().indices
+
+    var sum = 0L
+    var from = 0
+
+    while (from in columns) {
+        val till = columns.drop(from).firstOrNull { i -> lines.all { it[i] == ' ' } } ?: (columns.last + 1)
+        val numbers = (from..<till)
+            .map { i -> lines.dropLast(1).map { it[i] }.joinToString("").trim().toLong() }
+        sum += numbers.reduce(lines.last().drop(from).take(till - from).trim().operation())
+        from = till + 1
+    }
+
+    println(sum)
 }
