022937cadddd1be33f486972ce452d0cb8aad761
Michael Böiers <michael.boiers2@ingka.com>
2025-12-10 08:26:39 +0100

    refactor

--- src/main/kotlin/aoc2025/day05.kt
+++ src/main/kotlin/aoc2025/day05.kt
@@ -10,15 +10,15 @@ fun main() {
     ingredientsData.mapNotNull(String::toLongOrNull).count { i -> ranges.any { i in it } }
         .also(::println)
 
-    fun LongRange.overlapsWith(other: LongRange) = first in other || last in other
-    fun LongRange.isIncludedIn(other: LongRange) = first in other && last in other
+    fun LongRange.overlapsWith(other: LongRange) = this != other && (first in other || last in other)
+    fun LongRange.isIncludedIn(other: LongRange) = this != other && first in other && last in other
 
     with(ranges.toMutableSet()) {
-        while (any { r -> none { it != r && r.overlapsWith(it) } }) {
-            filter { r -> any { it != r && r.isIncludedIn(it) } }.forEach(::remove)
+        while (any { r -> none { r.overlapsWith(it) } }) {
+            filter { r -> any { r.isIncludedIn(it) } }.forEach(::remove)
 
-            val r = firstOrNull { r -> any { it != r && r.overlapsWith(it) } } ?: break
-            val r2 = first { it != r && r.overlapsWith(it) }
+            val r = firstOrNull { r -> any { r.overlapsWith(it) } } ?: break
+            val r2 = first { r.overlapsWith(it) }
             this += min(r.first, r2.first)..max(r.last, r2.last)
         }
         println(sumOf { it.last + 1 - it.first })
