9078e5d90e212fddd1c928bbeab669032348851c
Michael Böiers <michael.boiers2@ingka.com>
2025-12-11 06:50:26 +0100

    solve aoc 2025 day 08

---
+++ src/main/kotlin/aoc2025/day08.kt
@@ -0,0 +1,39 @@
+package aoc2025
+
+import kotlin.math.sqrt
+
+fun main() {
+    data class Point(val x: Long, val y: Long, val z: Long)
+
+    fun Point.distanceTo(o: Point): Double =
+        sqrt(sequenceOf(o.x - x, o.y - y, o.z - z).sumOf { it * it }.toDouble())
+
+    val points = generateSequence(::readLine).map { line ->
+        line.split(",").map { it.toLong() }.let { Point(it[0], it[1], it[2]) }
+    }.toSet()
+
+    val connections = points.flatMap { a -> points.filter { it != a }.map { b -> setOf(a, b) } }
+        .distinct().sortedBy { it.first().distanceTo(it.last()) }
+
+    val circuits = points.map { mutableSetOf(it) }.toMutableSet()
+
+    for ((n, c) in connections.withIndex()) {
+        val (a, b) = c.first() to c.last()
+
+        val ca = circuits.single { a in it }
+        val cb = circuits.single { b in it }
+
+        if (ca != cb) {
+            ca.addAll(cb)
+            circuits.removeIf { it == cb }
+        }
+        if (n == 999) {
+            println(circuits.map { it.size }.sortedDescending().take(3).reduce { a, b -> a * b })
+        }
+        if (circuits.size == 1) {
+            println(a.x * b.x)
+            break
+        }
+    }
+
+}
